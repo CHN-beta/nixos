3899d41b0f46a2af2debd36c160d2a2fa391082f
chn <chn@chn.moe>
2023-06-09 16:47:52 +0800

    加几个空格

--- modules/fonts/basic.nix
+++ modules/fonts/basic.nix
@@ -9,8 +9,8 @@
 		{
 			emoji = [ "Noto Color Emoji" ];
 			monospace = [ "Noto Sans Mono CJK SC" "Sarasa Mono SC" "DejaVu Sans Mono"];
-			sansSerif = ["Noto Sans CJK SC" "Source Han Sans SC" "DejaVu Sans"];
-			serif = ["Noto Serif CJK SC" "Source Han Serif SC" "DejaVu Serif"];
+			sansSerif = [ "Noto Sans CJK SC" "Source Han Sans SC" "DejaVu Sans" ];
+			serif = [ "Noto Serif CJK SC" "Source Han Serif SC" "DejaVu Serif" ];
 		};
 	};
 }
