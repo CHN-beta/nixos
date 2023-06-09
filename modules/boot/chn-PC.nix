1c8c78602481fe6833b87e9493bb0420975e8b9b
chn <chn@chn.moe>
2023-06-10 01:21:45 +0800

    remove cpupower

--- modules/boot/chn-PC.nix
+++ modules/boot/chn-PC.nix
@@ -10,7 +10,6 @@ inputs:
 				"usb_storage" "virtio_blk" "virtio_pci" "xhci_pci"
 			];
 			kernelModules = [ "kvm-intel" ];
-			extraModulePackages = with inputs.config.boot.kernelPackages; [ cpupower ];
 			extraModprobeConfig = "options kvm_intel nested=1";
 			kernelParams = [ "delayacct" "acpi_osi=Linux" "resume_offset=19145984" ];
 			resumeDevice = "/dev/mapper/root";
