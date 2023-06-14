15b9c69a5311c7bcc44a50270f96ccf66623e074
chn <chn@chn.moe>
2023-06-14 09:16:14 +0800

    尝试只替换mono

--- modules/packages/gaming.nix
+++ modules/packages/gaming.nix
@@ -2,45 +2,22 @@
 {
 	config =
 	{
+		nixpkgs.overlays =
+		[(
+			final: prev: let generic-pkgs = (inputs.inputs.nixpkgs.lib.nixosSystem
+			{
+				system = "x86_64-linux";
+				specialArgs = { inputs = inputs.inputs; };
+				modules = [{ config.nixpkgs.config.allowUnfree = true; }];
+			}).pkgs;
+				in { mono = generic-pkgs.mono; }
+		)];
 		environment.systemPackages = [ inputs.config.nur.repos.ataraxiasjel.proton-ge ];
-		programs = let generic-pkgs = (inputs.inputs.nixpkgs.lib.nixosSystem
+		programs =
 		{
-			system = "x86_64-linux";
-			specialArgs = { inputs = inputs.inputs; };
-			modules =
-			[
-				inputs.inputs.aagl.nixosModules.default
-				({ pkgs, ...}@inputs:
-				{
-					config.nixpkgs =
-					{
-						config.allowUnfree = true;
-						overlays =
-						[( final: prev:
-						{
-							anime-game-launcher = inputs.inputs.aagl.packages.x86_64-linux.anime-game-launcher;
-							honkers-railway-launcher = inputs.inputs.aagl.packages.x86_64-linux.honkers-railway-launcher;
-						} )];
-					};
-				})
-			];
-		}).pkgs;
-		in {
-			anime-game-launcher =
-			{
-				enable = true;
-				package = generic-pkgs.anime-game-launcher;
-			};
-			honkers-railway-launcher =
-			{
-				enable = true;
-				package = generic-pkgs.honkers-railway-launcher;
-			};
-			steam =
-			{
-				enable = true;
-				package = generic-pkgs.steam;
-			};
+			anime-game-launcher.enable = true;
+			honkers-railway-launcher.enable = true;
+			steam.enable = true;
 		};
 	};
 }
