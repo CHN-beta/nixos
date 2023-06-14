eb5e1c82fe2a5c958d2be81a97bad7aa9783d837
chn <chn@chn.moe>
2023-06-14 19:52:21 +0800

    move generic-pkgs from gaming to hardware

--- modules/hardware/chn-PC.nix
+++ modules/hardware/chn-PC.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }@inputs:
 {
 	config =
 	{
@@ -6,6 +7,16 @@
 		{
 			hostPlatform = { system = "x86_64-linux"; gcc = { arch = "alderlake"; tune = "alderlake"; }; };
 			config.allowUnfree = true;
+			overlays =
+			[(
+				final: prev: let generic-pkgs = (inputs.inputs.nixpkgs.lib.nixosSystem
+				{
+					system = "x86_64-linux";
+					specialArgs = { inputs = inputs.inputs; };
+					modules = [{ config.nixpkgs.config.allowUnfree = true; }];
+				}).pkgs;
+					in { mono = generic-pkgs.mono; }
+			)];
 		};
 	};
 }
--- modules/packages/gaming.nix
+++ modules/packages/gaming.nix
@@ -2,16 +2,6 @@
 {
 	config =
 	{
-		nixpkgs.overlays =
-		[(
-			final: prev: let generic-pkgs = (inputs.inputs.nixpkgs.lib.nixosSystem
-			{
-				system = "x86_64-linux";
-				specialArgs = { inputs = inputs.inputs; };
-				modules = [{ config.nixpkgs.config.allowUnfree = true; }];
-			}).pkgs;
-				in { mono = generic-pkgs.mono; }
-		)];
 		environment.systemPackages = [ inputs.config.nur.repos.ataraxiasjel.proton-ge ];
 		programs =
 		{
