d0e7a70f78ee4371a2526d340344a4d168388f56
chn <chn@chn.moe>
2023-06-14 18:29:25 +0800

    disable alsa

--- modules/hardware/sound.nix
+++ modules/hardware/sound.nix
@@ -7,7 +7,7 @@
 		services.pipewire =
 		{
 			enable = true;
-			alsa = { enable = true; support32Bit = true; };
+			# alsa = { enable = true; support32Bit = true; };
 			pulse.enable = true;
 		};
 		systemd.user.services.pipewire.serviceConfig.CPUSchedulingPolicy = -11;
