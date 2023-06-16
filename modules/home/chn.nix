78594e2cf1e754c6dcbce02907d663b8a6e90a2d
chn <chn@chn.moe>
2023-06-16 13:13:00 +0800

    try to enable plasma manager

--- modules/home/chn.nix
+++ modules/home/chn.nix
@@ -6,8 +6,11 @@
 		{
 			imports = [ inputs.inputs.plasma-manager.homeManagerModules.plasma-manager ];
 			home.stateVersion = "22.11";
-			programs.zsh = import ./zsh.nix { inherit pkgs; };
+			programs =
+			{
+				zsh = import ./zsh.nix { inherit pkgs; };
+				plasma = import ./kde.nix;
+			};
 		};
-
 	};
 }
