dbde3eb2d1154b23ada0472744998db05843d2f6
chn <chn@chn.moe>
2023-06-17 22:30:59 +0800

    auto modprobe br_netfilter

--- modules/boot/chn-PC.nix
+++ modules/boot/chn-PC.nix
@@ -5,13 +5,26 @@
 		boot =
 		{
 			kernelPackages = inputs.pkgs.linuxPackages_xanmod_latest;
+			
+			# initrd 里有的模块
 			initrd.availableKernelModules =
 			[
 				"ahci" "i915" "intel_cstate" "nvidia" "nvidia_drm" "nvidia_modeset" "nvidia_uvm" "nvme" "sr_mod"
 				"usb_storage" "virtio_blk" "virtio_pci" "xhci_pci"
 			];
-			kernelModules = [ "kvm-intel" ];
-			extraModprobeConfig = "options kvm_intel nested=1";
+
+			# stage2 中自动加载的模块
+			kernelModules = [ "kvm-intel" "br_netfilter" ];
+
+			# 只安装，不需要自动加载的模块
+			# extraModulePackages = [ yourmodulename ];
+
+			extraModprobeConfig =
+			''
+				options kvm_intel nested=1
+				options iwlmvm power_scheme=1
+				options iwlwifi uapsd_disable=1
+			'';
 			kernelParams = [ "delayacct" "acpi_osi=Linux" "resume_offset=19145984" ];
 			resumeDevice = "/dev/mapper/root";
 		};
