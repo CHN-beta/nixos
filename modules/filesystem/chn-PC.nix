06d94e2f7f954606bbb0a085af0eb5d1943c5c9e
chn <chn@chn.moe>
2023-06-17 12:03:53 +0800

    enable snapper

--- modules/filesystem/chn-PC.nix
+++ modules/filesystem/chn-PC.nix
@@ -50,5 +50,17 @@
 		};
 		systemd.services.nix-daemon =
 			{ environment = { TMPDIR = "/var/cache/nix"; }; serviceConfig = { CacheDirectory = "nix"; }; };
+		services.snapper.configs.impermanence =
+		{
+			SUBVOLUME = "/nix/impermanence";	
+			TIMELINE_CREATE = true;
+			TIMELINE_CLEANUP = true;
+			TIMELINE_MIN_AGE = 1800;
+			TIMELINE_LIMIT_HOURLY = "10";
+			TIMELINE_LIMIT_DAILY = "7";
+			TIMELINE_LIMIT_WEEKLY = "1";
+			TIMELINE_LIMIT_MONTHLY = "0";
+			TIMELINE_LIMIT_YEARLY = "0";
+		};
 	};
 }
