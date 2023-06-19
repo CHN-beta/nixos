a71766f7713c05e31d1d5e03ff395a9e6f211ead
chn <chn@chn.moe>
2023-06-19 21:51:59 +0800

    add smartmontools

--- modules/packages/terminal.nix
+++ modules/packages/terminal.nix
@@ -5,7 +5,7 @@
 		environment.systemPackages = with inputs.pkgs;
 		[
 			beep neofetch screen dos2unix tldr gnugrep pv
-			pciutils usbutils lshw powertop compsize iotop iftop
+			pciutils usbutils lshw powertop compsize iotop iftop smartmontools
 			ksh
 			vim nano
 			wget aria2 curl yt-dlp
