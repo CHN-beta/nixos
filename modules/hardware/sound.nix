54aec417ffee2e82dc92abb9625ab571483b7bd4
chn <chn@chn.moe>
2023-06-24 08:45:05 +0800

    switch to zen kernel adjust rtkit params

--- modules/hardware/sound.nix
+++ modules/hardware/sound.nix
@@ -15,5 +15,7 @@
 			alsa = { enable = true; support32Bit = true; };
 			pulse.enable = true;
 		};
+		systemd.services.rtkit-daemon.serviceConfig.ExecStart =
+			[ "" "${pkgs.rtkit}/libexec/rtkit-daemon --our-realtime-priority=95 --max-realtime-priority=90" ];
 	};
 }
