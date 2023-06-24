eff5a4a9cdffe9073fa7602b0cef412c5de57429
chn <chn@chn.moe>
2023-06-24 09:54:59 +0800

    Revert "switch to zen kernel"
    This reverts commit 54aec417ffee2e82dc92abb9625ab571483b7bd4.

--- modules/hardware/sound.nix
+++ modules/hardware/sound.nix
@@ -15,7 +15,5 @@
 			alsa = { enable = true; support32Bit = true; };
 			pulse.enable = true;
 		};
-		systemd.services.rtkit-daemon.serviceConfig.ExecStart =
-			[ "" "${pkgs.rtkit}/libexec/rtkit-daemon --our-realtime-priority=95 --max-realtime-priority=90" ];
 	};
 }
