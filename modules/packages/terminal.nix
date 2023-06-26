237e28a4514a2f9e98bede513cdc55933060b1a3
chn <chn@chn.moe>
2023-06-26 11:17:09 +0800

    add btop

--- modules/packages/terminal.nix
+++ modules/packages/terminal.nix
@@ -5,7 +5,7 @@ inputs:
 		environment.systemPackages = with inputs.pkgs;
 		[
 			beep neofetch screen dos2unix tldr gnugrep pv
-			pciutils usbutils lshw powertop compsize iotop iftop smartmontools htop intel-gpu-tools
+			pciutils usbutils lshw powertop compsize iotop iftop smartmontools htop intel-gpu-tools btop
 			ksh
 			vim nano
 			wget aria2 curl yt-dlp
