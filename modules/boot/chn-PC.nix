63b881ae0ea0c2cd6d9c8cc5a276e187b578b052
chn <chn@chn.moe>
2023-06-27 12:50:45 +0800

    try to use fido2

--- modules/boot/chn-PC.nix
+++ modules/boot/chn-PC.nix
@@ -93,6 +93,7 @@ inputs:
 		# initrd, luks
 		boot.initrd =
 		{
+			systemd.enable = true;
 			# modules in initrd
 			# modprobe --show-depends
 			availableKernelModules =
@@ -104,47 +105,12 @@ inputs:
 			++ [ "aesni_intel" "cryptd" "crypto_simd" "libaes" ];
 			luks =
 			{
-				# setup accroding to https://github.com/sgillespie/nixos-yubikey-luks
-				# nix-shell https://github.com/sgillespie/nixos-yubikey-luks/archive/master.tar.gz
-				# ykpersonalize -2 -ochal-resp -ochal-hmac
-				# SALT_LENGTH=16
-				# SALT="$(dd if=/dev/random bs=1 count=$SALT_LENGTH 2>/dev/null | rbtohex)"
-				# read -s USER_PASSPHRASE
-				# CHALLENGE="$(echo -n $SALT | openssl dgst -binary -sha512 | rbtohex)"
-				# RESPONSE=$(ykchalresp -2 -x $CHALLENGE 2>/dev/null)
-				# KEY_LENGTH=512
-				# ITERATIONS=1000000
-				# LUKS_KEY="$(echo -n $USER_PASSPHRASE | pbkdf2-sha512 $(($KEY_LENGTH / 8)) $ITERATIONS $RESPONSE | rbtohex)"
-				# CIPHER=aes-xts-plain64
-				# HASH=sha512
-				# echo -n "$LUKS_KEY" | hextorb | cryptsetup luksFormat --cipher="$CIPHER" \
-				#	--key-size="$KEY_LENGTH" --hash="$HASH" --key-file=- /dev/sdb5
-				# mkdir -p /boot/crypt-storage
-				# echo -ne "$SALT\n$ITERATIONS" > /boot/crypt-storage/default
-				# echo -n "$LUKS_KEY" | hextorb | cryptsetup open /dev/sdb5 encrypted --key-file=-
-				# cryptsetup luksOpen --test-passphrase --key-slot 0 /dev/sda2
-				# cryptsetup luksKillSlot /dev/sdb1 2
-				yubikeySupport = true;
-				reusePassphrases = false;
 				devices.root =
 				{
 					device = "/dev/disk/by-uuid/55fdd19f-0f1d-4c37-bd4e-6df44fc31f26";
 					allowDiscards = true;
-					yubikey =
-					{
-						slot = 2;
-						twoFactor = true;
-						gracePeriod = 120;
-						keyLength = 64;
-						saltLength = 16;
-						storage =
-						{
-							device = "/dev/disk/by-uuid/631e5af2-af11-464a-9fbc-52f5215ae401";
-							fsType = "btrfs";
-							path = "/crypt-storage/default";
-						};
-					};
 					bypassWorkqueues = true;
+					crypttabExtraOpts = [ "fido2-device=auto" ];
 				};
 			};
 		};
