318e185217ace7576ce6bc36297c6a3c4d5a16d9
chn <chn@chn.moe>
2023-06-27 23:14:33 +0800

    set root password in initrd

--- modules/users/root.nix
+++ modules/users/root.nix
@@ -9,5 +9,9 @@
 					else { passwordFile = inputs.config.sops.secrets."password/root".path; });
 			mutableUsers = false;
 		};
+		# root password in initrd: 0000
+		# currently not working, might work in the future
+		# boot.initrd.secrets.${builtins.toString inputs.config.sops.secrets."password/root".path}
+		# 	= builtins.toFile "root-password" "$y$j9T$EHgd1EmvM54fIkuDnrAM41$WNhog3VSAdrQXljA4I7Coy8W6iRQFQ3CLOKEH6IZzJ/";
 	} // (if !bootstrape then { sops.secrets."password/root".neededForUsers = true; } else {});
 }
