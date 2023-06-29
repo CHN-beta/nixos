6df144384a988a385bc07b888c051b51f67fdaef
chn <chn@chn.moe>
2023-06-29 18:30:20 +0800

    fix upho

--- localPackages/upho/default.nix
+++ localPackages/upho/default.nix
@@ -6,8 +6,8 @@
 	{
 		owner = "CHN-beta";
 		repo = "upho";
-		rev = "1468521477f2a6d112abd7a3e182c6a0ccb6f6c0";
-		sha256 = "sha256-ZtGUGpxesiQL/76zSTQjt3UK+JoRNs/C2g+3PC3eADE=";
+		rev = "45d4821bf5ef1d26ddf98e84bd497b1ddec9e057";
+		sha256 = "sha256-J/DlCeGk70BTTTCcoN2ztruCnaA40kFiHDzJw3yAC24=";
 	};
 	doCheck = false;
 	propagatedBuildInputs = [ numpy h5py phonopy ];
