3ab06acd097d0e9a4c0b300611f21208fff58234
chn <chn@chn.moe>
2023-06-29 19:03:26 +0800

    fix upho

--- localPackages/upho/default.nix
+++ localPackages/upho/default.nix
@@ -6,8 +6,8 @@
 	{
 		owner = "CHN-beta";
 		repo = "upho";
-		rev = "45d4821bf5ef1d26ddf98e84bd497b1ddec9e057";
-		sha256 = "sha256-J/DlCeGk70BTTTCcoN2ztruCnaA40kFiHDzJw3yAC24=";
+		rev = "0f27ac6918e8972c70692816438e4ac37ec6b348";
+		sha256 = "sha256-NvoV+AUH9MmGT4ohrLAAvpLs8APP2DOKYlZVliHrVRM=";
 	};
 	doCheck = false;
 	propagatedBuildInputs = [ numpy h5py phonopy ];
