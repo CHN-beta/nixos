48762a7b019bc9574d1200119762c82cea9ff825
chn <chn@chn.moe>
2023-07-03 12:43:13 +0800

    add virt-viewer

--- modules/packages/gui.nix
+++ modules/packages/gui.nix
@@ -84,7 +84,7 @@ inputs:
 			yubikey-manager yubikey-manager-qt yubikey-personalization yubikey-personalization-gui
 			appflowy
 			nomacs
-			putty
+			putty virt-viewer
 		]
 		++ (with inputs.lib; filter isDerivation (attrValues inputs.pkgs.plasma5Packages.kdeGear));
 		programs.wireshark = { enable = true; package = inputs.pkgs.wireshark; };
