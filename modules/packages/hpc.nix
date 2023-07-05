f2a73928bee4afee9b062d7f9c67b58e969b9e18
chn <chn@chn.moe>
2023-07-05 10:33:28 +0800

    add tensorflow keras

--- modules/packages/hpc.nix
+++ modules/packages/hpc.nix
@@ -5,7 +5,7 @@ inputs:
 		ovito paraview localPackages.vesta # vsim
 		(python3.withPackages (ps: with ps;
 		[
-			phonopy inquirerpy requests tqdm
+			phonopy inquirerpy requests tqdm tensorflow keras
 			localPackages.upho localPackages.spectral
 		]))
 		mathematica octave root cling gfortran
