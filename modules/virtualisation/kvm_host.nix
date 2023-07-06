401f4210dc4e21a1898b45e947e9ade61bb615a0
chn <chn@chn.moe>
2023-07-06 23:08:38 +0800

    休眠和睡眠时自动休眠虚拟机

--- modules/virtualisation/kvm_host.nix
+++ modules/virtualisation/kvm_host.nix
@@ -9,5 +9,79 @@ inputs:
 			spiceUSBRedirection.enable = true;
 		};
 		environment.systemPackages = with inputs.pkgs; [ qemu_full virt-manager win-spice ];
+		systemd.services =
+			let
+				virsh = "${inputs.pkgs.libvirt}/bin/virsh";
+				hibernate = inputs.pkgs.writeShellScript "libvirt-hibernate"
+				''
+					if [ "$(LANG=C ${virsh} domstate "$1")" = 'running' ]
+					then
+						if ${virsh} dompmsuspend "$1" disk
+						then
+							echo "Waiting for $1 to suspend"
+							while ! [ "$(LANG=C ${virsh} domstate "$1")" = 'shut off' ]
+							do
+								sleep 1
+							done
+							echo "$1 suspended"
+							touch "/tmp/libvirt.$1.suspended"
+						else
+							echo "Failed to suspend $1"
+						fi
+					fi
+				'';
+				resume = inputs.pkgs.writeShellScript "libvirt-resume"
+				''
+					if [ "$(LANG=C ${virsh} domstate "$1")" = 'shut off' ] && [ -f "/tmp/libvirt.$1.suspended" ]
+					then
+						if virsh start "$1"
+						then
+							echo "Waiting for $1 to resume"
+							while ! [ "$(LANG=C ${virsh} domstate "$1")" = 'running' ]
+							do
+								sleep 1
+							done
+							echo "$1 resumed"
+							rm "/tmp/libvirt.$1.suspended"
+						else
+							echo "Failed to resume $1"
+						fi
+					fi
+				'';
+			in
+			{
+				"libvirt-hibernate@" =
+				{
+					description = "libvirt hibernate";
+					before = [ "systemd-hibernate.service" "systemd-suspend.service" ];
+					serviceConfig = { Type = "oneshot"; ExecStart = "${hibernate} %i"; };
+				};
+				"libvirt-resume@" =
+				{
+					description = "libvirt resume";
+					after = [ "systemd-hibernate.service" "systemd-suspend.service" ];
+					serviceConfig = { Type = "oneshot"; ExecStart = "${resume} %i"; };
+				};
+				"libvirt-hibernate@win10" =
+				{
+					wantedBy = [ "systemd-hibernate.service" "systemd-suspend.service" ];
+					overrideStrategy = "asDropin";
+				};
+				"libvirt-resume@win10" =
+				{
+					wantedBy = [ "systemd-hibernate.service" "systemd-suspend.service" ];
+					overrideStrategy = "asDropin";
+				};
+				"libvirt-hibernate@hardconnect" =
+				{
+					wantedBy = [ "systemd-hibernate.service" "systemd-suspend.service" ];
+					overrideStrategy = "asDropin";
+				};
+				"libvirt-resume@hardconnect" =
+				{
+					wantedBy = [ "systemd-hibernate.service" "systemd-suspend.service" ];
+					overrideStrategy = "asDropin";
+				};
+			};
 	};
 }
