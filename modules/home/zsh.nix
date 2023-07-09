53c94c46ee9d6847cd7154b321541a8becab55df
chn <chn@chn.moe>
2023-07-09 22:44:59 +0800

    enable br

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -10,6 +10,21 @@
 		HYPHEN_INSENSITIVE="true"
 
 		export PATH=~/bin:$PATH
+
+		function br
+		{
+			local cmd cmd_file code
+			cmd_file=$(mktemp)
+			if broot --outcmd "$cmd_file" "$@"; then
+				cmd=$(<"$cmd_file")
+				command rm -f "$cmd_file"
+				eval "$cmd"
+			else
+				code=$?
+				command rm -f "$cmd_file"
+				return "$code"
+			fi
+		}
 	'';
 	plugins =
 	[
