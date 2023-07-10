dc34a98c7d77a9fd7367d1bf8dd0e73284310785
chn <chn@chn.moe>
2023-07-10 20:11:17 +0800

    enable nix-direnv

--- modules/home/chn.nix
+++ modules/home/chn.nix
@@ -6,6 +6,8 @@ inputs:
 		{
 			home.stateVersion = "22.11";
 			programs.zsh = import ./zsh.nix { inherit pkgs; };
+			programs.direnv.enable = true;
+			programs.direnv.nix-direnv.enable = true;
 		};
 	};
 }
--- modules/home/root.nix
+++ modules/home/root.nix
@@ -7,6 +7,8 @@
 		{
 			home.stateVersion = "22.11";
 			programs.zsh = import ./zsh.nix { inherit pkgs; };
+			programs.direnv.enable = true;
+			programs.direnv.nix-direnv.enable = true;
 		};
 	};
 }
