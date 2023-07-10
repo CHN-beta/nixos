dc707d3902f742fa91353c5ad7b9d18d2085d328
chn <chn@chn.moe>
2023-07-10 15:41:44 +0800

    enable nil

--- modules/packages/terminal.nix
+++ modules/packages/terminal.nix
@@ -25,6 +25,7 @@ inputs:
 			trash-cli tmux adb-sync pdfchain wgetpaste httplib clang magic-enum xtensor
 			go rustc boost cereal cxxopts valgrind
 			lsd zellij broot
+			nil
 		];
 		programs =
 		{
