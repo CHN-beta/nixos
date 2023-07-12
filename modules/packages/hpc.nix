3ff3b24e682e60c9d0cc9cd313ca18163c2d1ea3
chn <chn@chn.moe>
2023-07-12 23:44:37 +0800

    currently disable distcc

--- modules/packages/hpc.nix
+++ modules/packages/hpc.nix
@@ -18,8 +18,9 @@ inputs:
 	++ ( with inputs.pkgs.pkgsCross.mingwW64.buildPackages; [ gcc ] );
 	config.programs.ccache.enable = true;
 	config.nix.settings.extra-sandbox-paths = [ inputs.config.programs.ccache.cacheDir ];
-	config.nixpkgs.config.replaceStdenv = { pkgs }: pkgs.ccacheStdenv;
-	# config.programs.ccache.packageNames = [ "webkitgtk" ];
+	# config.nixpkgs.config.replaceStdenv = { pkgs }: pkgs.ccacheStdenv;
+	# only replace stdenv for large and tested packages
+	# config.programs.ccache.packageNames = [ "webkitgtk" "libreoffice" "tensorflow" "linux" "chromium" ];
 }
 
 # cross-x86_64-pc-linux-musl/gcc
