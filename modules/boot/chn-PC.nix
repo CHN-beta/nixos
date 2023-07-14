f1200943bbac4446d471330e5644926d664be445
chn <chn@chn.moe>
2023-07-14 15:59:14 +0800

    fix cjytty hash

--- modules/boot/chn-PC.nix
+++ modules/boot/chn-PC.nix
@@ -42,7 +42,7 @@ inputs:
 				patch = inputs.pkgs.fetchurl
 				{
 					url = "https://raw.githubusercontent.com/zhmars/cjktty-patches/master/v6.x/cjktty-6.4.patch";
-					sha256 = "sha256-QnsWruzhtiZnqzTUXkPk9Hb19Iddr4VTWXyV4r+iLvE=";
+					sha256 = "sha256-oGZxvg6ldpPAn5+W+r/e/WkVO92iv0XVFoJfFF5rdc8=";
 				};
 				extraStructuredConfig = { FONT_CJK_16x16 = inputs.lib.kernel.yes; FONT_CJK_32x32 = inputs.lib.kernel.yes; };
 			}
