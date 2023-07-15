10fecc5ab9bd1ccbf11d89830d6dd43d514201c3
chn <chn@chn.moe>
2023-07-15 09:30:18 +0800

    sddm use wayland

--- modules/kde.nix
+++ modules/kde.nix
@@ -5,13 +5,22 @@ inputs:
 		services.xserver =
 		{
 			enable = true;
-			displayManager.sddm.enable = true;
+			displayManager.sddm =
+			{
+				enable = true;
+				settings.General =
+				{
+					DisplayServer = "wayland";
+					GreeterEnvironment = "QT_WAYLAND_SHELL_INTEGRATION=layer-shell";
+					CompositorCommand = "weston";
+				};
+			};
 			desktopManager.plasma5.enable = true;
 		};
 		environment =
 		{
 			sessionVariables."GTK_USE_PORTAL" = "1";
-			systemPackages = [ inputs.pkgs.libsForQt5.qtstyleplugin-kvantum ];
+			systemPackages = [ inputs.pkgs.libsForQt5.qtstyleplugin-kvantum inputs.pkgs.weston ];
 		};
 		xdg.portal.extraPortals = with inputs.pkgs; [ xdg-desktop-portal-gtk xdg-desktop-portal-wlr ];
 		programs.xwayland.enable = true;
