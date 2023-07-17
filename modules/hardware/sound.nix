02491d124642d9576de1d76dd431ec81f1ff506f
chn <chn@chn.moe>
2023-07-17 13:47:12 +0800

    fix wireplump config

--- modules/hardware/sound.nix
+++ modules/hardware/sound.nix
@@ -2,11 +2,7 @@ inputs:
 {
 	config =
 	{
-		sound =
-		{
-			enable = true;
-			extraConfig = "session.suspend-timeout-seconds 0";
-		};
+		sound.enable = true;
 		hardware.pulseaudio.enable = false;
 		security.rtkit.enable = true;
 		services.pipewire =
@@ -15,5 +11,15 @@ inputs:
 			alsa = { enable = true; support32Bit = true; };
 			pulse.enable = true;
 		};
+		environment.etc."wireplumber/main.lua.d/50-alsa-config.lua".text =
+			let
+				content = builtins.readFile
+					("/." + inputs.pkgs.wireplumber + "/share/wireplumber/main.lua.d/50-alsa-config.lua");
+				matched = builtins.match ".*\n([[:space:]]*)(--\\[\"session\\.suspend-timeout-seconds\"][^\n]*)[\n].*" content;
+				spaces = builtins.elemAt matched 0;
+				comment = builtins.elemAt matched 1;
+				config = "[\"session.suspend-timeout-seconds\"] = 0";
+			in
+				builtins.replaceStrings [(spaces + comment)] [(spaces + config)] content;
 	};
 }
