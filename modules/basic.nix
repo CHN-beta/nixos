421274f9a242d4bdd618f53c532f98531ba76f19
chn <chn@chn.moe>
2023-07-19 18:40:46 +0800

    add ubuntu nspawn config

--- modules/basic.nix
+++ modules/basic.nix
@@ -59,11 +59,15 @@
 			services = builtins.listToAttrs (builtins.map (name: { inherit name; value = { u2fAuth = true; }; })
 				[ "login" "sudo" "su" "kde" "polkit-1" ]);
 		};
-		systemd.nspawn.arch =
-		{
-			execConfig.PrivateUsers = false;
-			networkConfig.VirtualEthernet = false;
-		};
+		systemd.nspawn =
+			let
+				f = name: { inherit name; value =
+				{
+					execConfig.PrivateUsers = false;
+					networkConfig.VirtualEthernet = false;
+				}; };
+			in
+				builtins.listToAttrs (builtins.map f [ "arch" "ubuntu-22.04" ]);
 		environment.etc."channels/nixpkgs".source = inputs.topInputs.nixpkgs.outPath;
 		# environment.pathsToLink = [ "/include" ];
 		# environment.variables.CPATH = "/run/current-system/sw/include";
