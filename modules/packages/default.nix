2f9987cc784971d81b0fe5b044d9d2803401bbec
chn <chn@chn.moe>
2023-07-26 14:17:17 +0800

    remove unused extensions

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -166,8 +166,8 @@ inputs:
 										james-yu.latex-workshop gimly81.matlab affenwiesel.matlab-formatter ckolkman.vscode-postgres
 										yzhang.markdown-all-in-one pkief.material-icon-theme bbenoist.nix ms-ossdata.vscode-postgresql
 										redhat.vscode-xml dotjoshjohnson.xml jnoortheen.nix-ide xdebug.php-debug hbenl.vscode-test-explorer
-										jeff-hykin.better-cpp-syntax josetr.cmake-language-support-vscode fredericbonnet.cmake-test-adapter
-										hirse.vscode-ungit fortran-lang.linter-gfortran ms-dotnettools.vscode-dotnet-runtime
+										jeff-hykin.better-cpp-syntax fredericbonnet.cmake-test-adapter
+										hirse.vscode-ungit fortran-lang.linter-gfortran
 									];
 							}
 						)
