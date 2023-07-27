3422a545f4e660968fce02e03877df1bed057258
chn <chn@chn.moe>
2023-07-27 23:36:30 +0800

    start dnsmasq after network-interfaces

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -334,19 +334,23 @@ inputs:
 						secrets = listToAttrs
 							(map (name: { name = "xray-client/${name}"; value = {}; }) [ "server" "serverName" "uuid" ]);
 					};
-					systemd.services.xray =
+					systemd.services =
 					{
-						serviceConfig =
+						xray =
 						{
-							DynamicUser = inputs.lib.mkForce false;
-							User = "v2ray";
-							Group = "v2ray";
-							CapabilityBoundingSet = "CAP_NET_ADMIN CAP_NET_BIND_SERVICE";
-							AmbientCapabilities = "CAP_NET_ADMIN CAP_NET_BIND_SERVICE";
-							LimitNPROC = 10000;
-							LimitNOFILE = 1000000;
+							serviceConfig =
+							{
+								DynamicUser = inputs.lib.mkForce false;
+								User = "v2ray";
+								Group = "v2ray";
+								CapabilityBoundingSet = "CAP_NET_ADMIN CAP_NET_BIND_SERVICE";
+								AmbientCapabilities = "CAP_NET_ADMIN CAP_NET_BIND_SERVICE";
+								LimitNPROC = 10000;
+								LimitNOFILE = 1000000;
+							};
+							restartTriggers = [ inputs.config.sops.templates."xray-client.json".file ];
 						};
-						restartTriggers = [ inputs.config.sops.templates."xray-client.json".file ];
+						dnsmasq.after = [ "network-interfaces.target" ];
 					};
 					users = { users.v2ray = { isSystemUser = true; group = "v2ray"; }; groups.v2ray = {}; };
 					environment.etc."resolv.conf".text = "nameserver 127.0.0.1";
