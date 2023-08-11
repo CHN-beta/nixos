97496005cc8a44fc91951d1d69dd182747abf76b
chn <chn@chn.moe>
2023-08-11 12:25:09 +0800

    add cinny

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -116,7 +116,7 @@ inputs:
 						# instant messager
 						element-desktop telegram-desktop discord qq nur-xddxdd.wechat-uos # jail
 						inputs.config.nur.repos.linyinfeng.wemeet # native # nur-xddxdd.wine-wechat thunder
-						zoom-us signal-desktop
+						zoom-us signal-desktop cinny-desktop
 						# browser
 						google-chrome
 						# networking
