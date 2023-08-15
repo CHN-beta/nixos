0282024b5564c0fe57cca52b0992d14848867ccd
chn <chn@chn.moe>
2023-08-15 16:26:46 +0800

    currently disable wallabag mailer

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -1120,15 +1120,16 @@ inputs:
 								SYMFONY__ENV__DATABASE_NAME=wallabag
 								SYMFONY__ENV__DATABASE_USER=wallabag
 								SYMFONY__ENV__DATABASE_PASSWORD=${placeholder."postgresql/wallabag"}
-								SYMFONY__ENV__MAILER_DSN=smtp://bot%%40chn.moe@${placeholder."mail/bot-encoded"}:mail.chn.moe
-								SYMFONY__ENV__FROM_EMAIL=bot@chn.moe
-								SYMFONY__ENV__TWOFACTOR_SENDER=bot@chn.moe
-								SYMFONY__ENV__DOMAIN_NAME=https://wallabag.chn.moe
 								SYMFONY__ENV__REDIS_HOST=host.docker.internal
 								SYMFONY__ENV__REDIS_PORT=8790
 								SYMFONY__ENV__REDIS_PASSWORD=${placeholder."redis/wallabag"}
 								SYMFONY__ENV__SERVER_NAME=wallabag.chn.moe
+								SYMFONY__ENV__TWOFACTOR_AUTH=false
 							'';
+							# SYMFONY__ENV__MAILER_DSN=smtp://bot%%40chn.moe@${placeholder."mail/bot-encoded"}:mail.chn.moe
+							# SYMFONY__ENV__FROM_EMAIL=bot@chn.moe
+							# SYMFONY__ENV__TWOFACTOR_SENDER=bot@chn.moe
+							# SYMFONY__ENV__DOMAIN_NAME=https://wallabag.chn.moe
 						secrets =
 						{
 							"redis/wallabag".owner = inputs.config.users.users.redis-wallabag.name;
