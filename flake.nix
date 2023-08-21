249b5983c96c28635110324c19e075da763bb6d3
chn <chn@chn.moe>
2023-08-21 12:15:20 +0800

    fix yoga boot

--- flake.nix
+++ flake.nix
@@ -626,7 +626,7 @@
 						};})
 					];
 				};
-				# sudo HTTPS_PROXY=socks5://127.0.0.1:10884 nixos-install --flake .#bootstrap --option substituters http://127.0.0.1:5000 --option require-sigs false
+				# sudo HTTPS_PROXY=socks5://127.0.0.1:10884 nixos-install --flake .#bootstrap --option substituters http://127.0.0.1:5000 --option require-sigs false --option system-features gccarch-silvermont
 			};
 			deploy =
 			{
