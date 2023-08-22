ff056eee8b9538be94038a3e8dca435edc4ef96a
chn <chn@chn.moe>
2023-08-22 21:10:53 +0800

    整理rsshub（还不能用）

--- local/pkgs/rsshub/default.nix
+++ local/pkgs/rsshub/default.nix
@@ -1,8 +1,6 @@
-{ lib, mkYarnPackage, fetchFromGitHub }:
-mkYarnPackage
+{ lib, mkPnpmPackage, fetchFromGitHub }:
+mkPnpmPackage
 {
-  pname = "rsshub";
-  version = "20230814";
   src = fetchFromGitHub
 	{
 		owner = "DIYgod";
@@ -10,15 +8,6 @@ mkYarnPackage
 		rev = "ecab0c0882a17b9b70431aca0c155a2da9d2c4fa";
 		hash = "sha256-VAIUQCQcKYaav4Ch73Cn7poO0/VCGtWkWJkPJ3Qp31A=";
 	};
-	# configurePhase =
-	# ''
-	# 	cp -r $node_modules node_modules
-	# '';
-	# buildPhase =
-	# ''
-	# 	runHook preBuild
-	# 	# yarn install --frozen-lockfile --offline
-	# 	NODE_ENV=production yarn build --offline
-	# 	runHook postBuild
-	# '';
+	nodeModulesPreBuild = "export PUPPETEER_SKIP_DOWNLOAD=true";
+	script = "build:all";
 }
