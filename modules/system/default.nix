289ed2fb27f74da9034b1e0f0f19b0d3a6a1355d
chn <chn@chn.moe>
2023-09-02 21:39:01 +0800

    整理 systemd.security

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -11,6 +11,7 @@ inputs:
     ./gui.nix
     ./nixpkgs.nix
     ./networking.nix
+    ./systemd.nix
   ];
   config =
     let
@@ -30,7 +31,6 @@ inputs:
               ACTION=="add|change", KERNEL=="nvme[0-9]n[0-9]", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="bfq"
             '';
             dbus.implementation = "broker";
-            journald.extraConfig = "MaxRetentionSec=7d";
           };
           time.timeZone = "Asia/Shanghai";
           boot =
@@ -47,36 +47,21 @@ inputs:
             consoleLogLevel = 7;
           };
           hardware.enableAllFirmware = true;
-          systemd =
+          environment.sessionVariables = rec
           {
-            extraConfig =
-            ''
-              DefaultTimeoutStopSec=10s
-              DefaultLimitNOFILE=1048576:1048576
-            '';
-            user.extraConfig = "DefaultTimeoutStopSec=10s";
-            services.systemd-tmpfiles-setup = { environment = { SYSTEMD_TMPFILES_FORCE_SUBVOL = "0"; }; };
-            timers.systemd-tmpfiles-clean.enable = false;
-            coredump.enable = false;
-          };
-          environment =
-          {
-            sessionVariables = rec
-            {
-              XDG_CACHE_HOME = "$HOME/.cache";
-              XDG_CONFIG_HOME = "$HOME/.config";
-              XDG_DATA_HOME = "$HOME/.local/share";
-              XDG_STATE_HOME = "$HOME/.local/state";
-              # ANDROID_HOME = "${XDG_DATA_HOME}/android";
-              HISTFILE= "${XDG_STATE_HOME}/bash/history";
-              CUDA_CACHE_PATH = "${XDG_CACHE_HOME}/nv";
-              DOCKER_CONFIG = "${XDG_CONFIG_HOME}/docker";
-              GNUPGHOME = "${XDG_DATA_HOME}/gnupg";
-              GTK2_RC_FILES = "${XDG_CONFIG_HOME}/gtk-2.0/gtkrc";
-              XCOMPOSECACHE = "${XDG_CACHE_HOME}/X11/xcompose";
-              MATHEMATICA_USERBASE = "${XDG_CONFIG_HOME}/mathematica";
-              _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=${XDG_CONFIG_HOME}/java";
-            };
+            XDG_CACHE_HOME = "$HOME/.cache";
+            XDG_CONFIG_HOME = "$HOME/.config";
+            XDG_DATA_HOME = "$HOME/.local/share";
+            XDG_STATE_HOME = "$HOME/.local/state";
+            # ANDROID_HOME = "${XDG_DATA_HOME}/android";
+            HISTFILE= "${XDG_STATE_HOME}/bash/history";
+            CUDA_CACHE_PATH = "${XDG_CACHE_HOME}/nv";
+            DOCKER_CONFIG = "${XDG_CONFIG_HOME}/docker";
+            GNUPGHOME = "${XDG_DATA_HOME}/gnupg";
+            GTK2_RC_FILES = "${XDG_CONFIG_HOME}/gtk-2.0/gtkrc";
+            XCOMPOSECACHE = "${XDG_CACHE_HOME}/X11/xcompose";
+            MATHEMATICA_USERBASE = "${XDG_CONFIG_HOME}/mathematica";
+            _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=${XDG_CONFIG_HOME}/java";
           };
           i18n =
           {
