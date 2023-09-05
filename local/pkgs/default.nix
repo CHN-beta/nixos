d5cd5d256eea1fba6433598bd565fa5e4177bc21
chn <chn@chn.moe>
2023-09-06 02:14:56 +0800

    vasp: use openmpi without cuda support

--- local/pkgs/default.nix
+++ local/pkgs/default.nix
@@ -16,7 +16,11 @@
   #   intel-mpi = pkgs.lmix-pkgs.intel-oneapi-mpi_2021_9_0;
   #   ifort = pkgs.lmix-pkgs.intel-oneapi-ifort_2021_9_0;
   # };
-  vasp = callPackage ./vasp { openmp = llvmPackages.openmp; };
+  vasp = callPackage ./vasp
+  {
+    openmp = llvmPackages.openmp;
+    openmpi = pkgs.openmpi.override { cudaSupport = false; };
+  };
   vaspkit = callPackage ./vaspkit { attrsToList = (import ../lib lib).attrsToList; };
   # "12to11" = callPackage ./12to11 {};
 }
