3dc8a2d73aa7af76dcce23d777378d0109047338
chn <chn@chn.moe>
2023-10-01 23:12:34 +0800

    bugs: add nvme

--- modules/bugs/default.nix
+++ modules/bugs/default.nix
@@ -75,6 +75,7 @@ inputs:
       firefox.programs.firefox.enable = inputs.lib.mkForce false;
       embree.nixpkgs.overlays =
         [(final: prev: { embree = prev.embree.override { stdenv = final.genericPackages.stdenv; }; })];
+      nvme.boot.kernelParams = [ "nvme_core.default_ps_max_latency_us=0" ];
     };
   in
     {
