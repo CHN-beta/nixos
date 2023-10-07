f6a5022aca03bc5c91a248f0bac3b4f6826e9be8
chn <chn@chn.moe>
2023-10-07 20:41:14 +0800

    merge next

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs =
   {
-    nixpkgs.url = "github:CHN-beta/nixpkgs/nixos-23.05-next";
-    nixpkgs-unstable.url = "github:CHN-beta/nixpkgs/nixos-unstable-next";
+    nixpkgs.url = "github:CHN-beta/nixpkgs/nixos-23.05";
+    nixpkgs-unstable.url = "github:CHN-beta/nixpkgs/nixos-unstable";
     home-manager = { url = "github:nix-community/home-manager/release-23.05"; inputs.nixpkgs.follows = "nixpkgs"; };
     sops-nix =
     {
