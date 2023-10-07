22ef0c27f5864dc134fb18cbc32564232043b749
chn <chn@chn.moe>
2023-10-07 22:11:22 +0800

    local.pkgs.matplotplusplus: enable opengl

--- local/pkgs/matplotplusplus/default.nix
+++ local/pkgs/matplotplusplus/default.nix
@@ -1,7 +1,17 @@
 {
-  stdenv, fetchFromGitHub, cmake, pkg-config,
-  gnuplot, libjpeg, libtiff, zlib, libpng, lapack, blas, fftw, opencv, nodesoup, cimg
-}: stdenv.mkDerivation rec
+  stdenv, fetchFromGitHub, cmake, pkg-config, substituteAll,
+  gnuplot, libjpeg, libtiff, zlib, libpng, lapack, blas, fftw, opencv, nodesoup, cimg, glfw, libGL, python3
+}:
+let
+  glad = fetchFromGitHub
+  {
+    owner = "Dav1dde";
+    repo = "glad";
+    rev = "v0.1.36";
+    sha256 = "FtkPz0xchwmqE+QgS+nSJVYaAfJSTUmZsObV/IPypVQ=";
+  };
+  python = python3.withPackages (pythonPackages: with pythonPackages; [ glad ]);
+in stdenv.mkDerivation rec
 {
   pname = "matplotplusplus";
   version = "1.2.0";
@@ -9,14 +19,16 @@
   {
     owner = "alandefreitas";
     repo = "matplotplusplus";
-    rev = "v${version}";
-    sha256 = "mYXAB1AbCtcd2rEuluJN6hDKE9+AowodjJt2pdyntes=";
+    rev = "a40344efa9dc5ea0c312e6e9ef4eb7238d98dc12";
+    sha256 = "6/dH/Rl2aAb8b+Ji5LwzkC+GWPOCBnYCrjy0qk8u/+I=";
   };
   cmakeFlags =
   [
     "-DBUILD_SHARED_LIBS=ON" "-DMATPLOTPP_BUILD_SHARED_LIBS=ON" "-DMATPLOTPP_BUILD_EXAMPLES=OFF"
     "-DMATPLOTPP_WITH_SYSTEM_NODESOUP=ON" "-DMATPLOTPP_WITH_SYSTEM_CIMG=ON"
+    "-DMATPLOTPP_BUILD_EXPERIMENTAL_OPENGL_BACKEND=ON" "-DGLAD_REPRODUCIBLE=ON"
   ];
-  buildInputs = [ gnuplot libjpeg libtiff zlib libpng lapack blas fftw opencv nodesoup cimg ];
-  nativeBuildInputs = [ cmake pkg-config ];
+  patches = [(substituteAll { src = ./add-glad.patch; inherit glad; })];
+  buildInputs = [ gnuplot libjpeg libtiff zlib libpng lapack blas fftw opencv nodesoup cimg glfw libGL python ];
+  nativeBuildInputs = [ cmake pkg-config python ];
 }
