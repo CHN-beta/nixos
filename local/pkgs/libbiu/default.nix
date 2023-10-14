4bb77b3351a005c92a9e0fff34a230fe974b98c9
chn <chn@chn.moe>
2023-10-14 22:46:17 +0800

    update libbiu

--- local/pkgs/libbiu/default.nix
+++ local/pkgs/libbiu/default.nix
@@ -1,6 +1,6 @@
 {
   stdenv, fetchFromGitHub, cmake, pkg-config, ninja,
-  fmt, boost, magic-enum, libbacktrace, concurrencpp, tgbot-cpp, nameof
+  fmt, boost, magic-enum, libbacktrace, concurrencpp, tgbot-cpp, nameof, eigen, range-v3
 }: stdenv.mkDerivation rec
 {
   name = "libbiu";
@@ -8,10 +8,10 @@
   {
     owner = "CHN-beta";
     repo = "libbiu";
-    rev = "46a23a7ee377f870a16b4591acf6d691a8e217cf";
-    sha256 = "c9wRTzb9EVVOsRvrtm+zj17b5hmorVeJ9L+iBbPA+cw=";
+    rev = "b048dd269e44a62c5220742ce697664088348e51";
+    sha256 = "SxxLGj1Kqj4oUvWQvkpNAA6YnWt4sF5Gzclox9wl0uU=";
   };
   nativeBuildInputs = [ cmake pkg-config ninja ];
-  buildInputs = [ fmt boost magic-enum libbacktrace concurrencpp tgbot-cpp nameof ];
+  buildInputs = [ fmt boost magic-enum libbacktrace concurrencpp tgbot-cpp nameof eigen range-v3 ];
   propagatedBuildInputs = buildInputs;
 }
