ea4b2cbeb844376e20b4a02164f5a20faa7a6acb
chn <chn@chn.moe>
2023-10-22 20:27:57 +0800

    system.fstrim: set interval to hourly

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -25,7 +25,7 @@ inputs:
         ACTION=="add|change", KERNEL=="nvme[0-9]n[0-9]", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="bfq"
       '';
       dbus.implementation = "broker";
-      fstrim.enable = true;
+      fstrim = { enable = true; interval = "daily"; };
     };
     time.timeZone = "Asia/Shanghai";
     boot =
