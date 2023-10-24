183caffbd8e519d31d16de2650e3a98a5bc3524f
chn <chn@chn.moe>
2023-10-24 22:34:10 +0800

    packages: exa to eza

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -54,7 +54,7 @@ inputs:
               # downloader
               wget aria2 curl
               # file manager
-              tree exa trash-cli lsd broot file xdg-ninja mlocate
+              tree eza trash-cli lsd broot file xdg-ninja mlocate
               # compress
               pigz rar upx unzip zip lzip p7zip
               # file system management
