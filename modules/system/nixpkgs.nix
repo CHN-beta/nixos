73ddbd00a9352a0595f5ec8455a972e87f777173
chn <chn@chn.moe>
2023-11-30 20:14:49 +0800

    fix chromium build with ccache

--- modules/system/nixpkgs.nix
+++ modules/system/nixpkgs.nix
@@ -44,7 +44,7 @@ inputs:
                 localSystem = { system = "x86_64-linux"; gcc = { arch = nixpkgs.march; tune = nixpkgs.march; }; };
                 config.allowUnfree = true;
               };
-              chromium = prev.chromium.override { ccache = true; };
+              chromium = builtins.trace "enable ccache for chromium" (prev.chromium.override { enableCcache = true; });
             })];
           };
           boot.kernelPatches =
