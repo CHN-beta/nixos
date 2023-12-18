02a2d399d68edf1a78c83865b44dda8c09b70e2a
chn <chn@chn.moe>
2023-12-18 20:08:24 +0800

    hardware: gpu drivers should not be in initrd

--- modules/hardware/default.nix
+++ modules/hardware/default.nix
@@ -85,16 +85,6 @@ inputs:
       (
         mkIf (hardware.gpus != [])
         {
-          boot.initrd.availableKernelModules =
-            let
-              modules =
-              {
-                intel = [ "i915" ];
-                nvidia = []; # [ "nvidia" "nvidia_drm" "nvidia_modeset" "nvidia_uvm" ];
-                amd = [ "amdgpu" ];
-              };
-            in
-              concatLists (map (gpu: modules.${gpu}) hardware.gpus);
           hardware =
           {
             opengl =
