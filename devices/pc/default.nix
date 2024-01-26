7a881491bd5198d715f0779bfd672014b9fad8bd
chn <chn@chn.moe>
2024-01-26 15:56:49 +0800

    pc: do not autoOptimiseStore

--- devices/pc/default.nix
+++ devices/pc/default.nix
@@ -51,6 +51,7 @@ inputs:
             "alderlake"
           ];
           keepOutputs = true;
+          autoOptimiseStore = false;
         };
         nixpkgs =
           { march = "znver4"; cuda = { enable = true; capabilities = [ "8.9" ]; forwardCompat = false; }; };
