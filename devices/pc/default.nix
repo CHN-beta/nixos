c29eb53ee00438a6e782aa6c86827908f06581a6
chn <chn@chn.moe>
2024-01-30 13:34:54 +0800

    Revert "Revert "pc: include all build dependencies""
    This reverts commit fbc6d5bee12bff67bedcc2ab8364ee2c49a52107.

--- devices/pc/default.nix
+++ devices/pc/default.nix
@@ -147,5 +147,6 @@ inputs:
     };
     virtualisation.virtualbox.host = { enable = true; enableExtensionPack = true; };
     hardware.nvidia.forceFullCompositionPipeline = true;
+    system.includeBuildDependencies = true;
   };
 }
