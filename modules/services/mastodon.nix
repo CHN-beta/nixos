9b41ebd15408a3d218f338c1d0981c4c2fe0bb6d
chn <chn@chn.moe>
2024-02-03 20:51:55 +0800

    Revert "services.mastodon: security update to 4.2.5"
    This reverts commit 3a60acebb512aefc921a40f8ada758409a472952.

--- modules/services/mastodon.nix
+++ modules/services/mastodon.nix
@@ -12,16 +12,6 @@ inputs:
       inherit (builtins) toString;
     in mkIf mastodon.enable
     {
-      # TODO: remove in next release
-      nixpkgs.overlays = [(final: prev: { mastodon = prev.mastodon.override
-      {
-        version = "4.2.5";
-        patches = prev.patches or [] ++ [(final.fetchpatch
-        {
-          url = "https://github.com/mastodon/mastodon/compare/v4.2.4...v4.2.5.patch";
-          hash = "sha256-CtzYV1i34s33lV/1jeNcr9p/x4Es1zRaf4l1sNWVKYk=";
-        })];
-      };})];
       services.mastodon =
       {
         enable = true;
