ef30e0f6dce237402830d63fdc10b48e1a04b71f
chn <chn@chn.moe>
2024-02-11 11:32:22 +0800

    packages.desktop.firefox: force set search engine

--- modules/packages/desktop/firefox.nix
+++ modules/packages/desktop/firefox.nix
@@ -30,7 +30,7 @@ inputs:
               }
             )
           ];
-          search.default = "Google";
+          search = { default = "Google"; force = true; };
           userChrome = builtins.concatStringsSep "\n" (builtins.map
             (file: builtins.readFile "${inputs.topInputs.cascade}/chrome/includes/cascade-${file}.css")
             [ "config-mouse" "colours" "layout" "responsive" "floating-panel" "nav-bar" "tabs" ]);
