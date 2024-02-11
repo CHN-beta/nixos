fd7fe508f54f2e164def61791cdfd946f7d862ee
chn <chn@chn.moe>
2024-02-11 22:57:59 +0800

    localPackages.blurred-wallpaper: fix

--- local/pkgs/blurred-wallpaper/default.nix
+++ local/pkgs/blurred-wallpaper/default.nix
@@ -1,11 +1,11 @@
 { stdenv, src }: stdenv.mkDerivation
 {
-  name = "blurred-wallpaper ";
+  name = "blurred-wallpaper";
   inherit src;
   phases = [ "installPhase" ];
   installPhase =
   ''
     mkdir -p $out/share/plasma/wallpapers/a2n.blur
-    cp -r * $out/share/plasma/wallpapers/a2n.blur
+    cp -r $src/* $out/share/plasma/wallpapers/a2n.blur
   '';
 }
