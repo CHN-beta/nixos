40f4d35b621aed5ff574c48b1d31fa11891db1c6
chn <chn@chn.moe>
2024-02-12 11:09:26 +0800

    users.chn.plasma.autostart: add element

--- modules/users/chn/plasma/autostart/default.nix
+++ modules/users/chn/plasma/autostart/default.nix
@@ -4,13 +4,30 @@ inputs:
   {
     home-manager.users.chn.config.home.file =
       let
+        programs =
+        {
+          nheko = "${inputs.pkgs.nheko}/share/applications/nheko.desktop";
+          kclockd = "${inputs.pkgs.plasma5Packages.kdeGear.kclock}/etc/xdg/autostart/org.kde.kclockd-autostart.desktop";
+          yakuake = "${inputs.pkgs.yakuake}/share/applications/org.kde.yakuake.desktop";
+          telegram = ./org.telegram.desktop.desktop;
+          element =
+            let
+              drv = inputs.pkgs.writeTextDir "element-desktop.desktop" (builtins.replaceStrings
+                [ "Exec=element-desktop %u" ] [ "Exec=element-desktop --hide" ]
+                (builtins.readFile "${inputs.pkgs.element-desktop.desktopItem}/share/applications/element-desktop.desktop"));
+            in builtins.trace "${drv}" "${drv}/element-desktop.desktop";
+        };
         devices =
         {
-          pc = [ "nheko" "kclockd" "yakuake" "telegram" ];
-          surface = [ "kclockd" "yakuake" "telegram" ];
+          pc = [ "nheko" "kclockd" "yakuake" "telegram" "element" ];
+          surface = [ "kclockd" "yakuake" "telegram" "element" ];
         };
       in builtins.listToAttrs (builtins.map
-        (file: { name = ".config/autostart/${file}.desktop"; value.source = ./. + "/${file}.desktop"; })
+        (file:
+        {
+          name = ".config/autostart/${builtins.baseNameOf "programs.${file}"}";
+          value.source = programs.${file};
+        })
         (devices.${inputs.config.nixos.system.networking.hostname}));
     environment.persistence =
       let impermanence = inputs.config.nixos.system.impermanence;
