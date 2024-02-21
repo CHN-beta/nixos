88609fafe484db1186eaa253643888f1c089ac95
chn <chn@chn.moe>
2024-02-21 15:27:39 +0800

    localPackages.vasp: fix

--- local/pkgs/vasp-gpu/default.nix
+++ local/pkgs/vasp-gpu/default.nix
@@ -1,39 +1,42 @@
-{
-  stdenvNoCC, requireFile,
-  nvhpc, rsync, mkl, lmod, bash, which,
-  glibc_multi
-}:
-stdenvNoCC.mkDerivation rec
-{
-  pname = "vasp";
-  version = "6.4.0";
-  # nix-store --query --hash $(nix store add-path ./vasp-6.4.0)
-  src = requireFile
+{ buildFHSEnv, writeScript, stdenvNoCC, requireFile, nvhpc, lmod }:
+let
+  env = buildFHSEnv
   {
-    name = "${pname}-${version}";
-    sha256 = "189i1l5q33ynmps93p2mwqf5fx7p4l50sls1krqlv8ls14s3m71f";
-    hashMode = "recursive";
-    message = "Source file not found.";
+    name = "env";
+    targetPkgs = pkgs: with pkgs; [ nvhpc gfortran zlib which rsync mkl lmod ];
   };
-  makeFlags = "DEPS=1";
-  enableParallelBuilding = true;
-  buildInputs = [ nvhpc mkl glibc_multi ];
-  nativeBuildInputs = [ rsync bash which ];
-  MKLROOT = mkl;
-  configurePhase =
+  buildScript = writeScript "build"
   ''
-    cp ${./makefile.include} makefile.include
-    . ${lmod}/lmod/lmod/init/bash
-    module use ${nvhpc}/share/nvhpc/modulefiles
+    . /usr/share/lmod/lmod/init/bash
+    module use /usr/share/nvhpc/modulefiles
     module load nvhpc
-    # chmod +w makefile.include
-    # echo "CPP_OPTIONS += -Duse_shmem -Dshmem_bcast_buffer -Dshmem_rproj" >> makefile.include
-    # echo "OBJECTS_LIB += getshmem.o" >> makefile.include
     mkdir -p bin
+    export MKLROOT=/usr
+    make DEPS=1 -j$NIX_BUILD_CORES
   '';
-  installPhase =
-  ''
-    mkdir -p $out
-    cp -r bin $out
-  '';
+  vasp = stdenvNoCC.mkDerivation rec
+  {
+    pname = "vasp";
+    version = "6.4.0";
+    # nix-store --query --hash $(nix store add-path ./vasp-6.4.0)
+    src = requireFile
+    {
+      name = "${pname}-${version}";
+      sha256 = "189i1l5q33ynmps93p2mwqf5fx7p4l50sls1krqlv8ls14s3m71f";
+      hashMode = "recursive";
+      message = "Source file not found.";
+    };
+    configurePhase = "cp ${./makefile.include} makefile.include";
+    enableParallelBuilding = true;
+    buildPhase = "${env}/bin/env ${buildScript}";
+    installPhase =
+    ''
+      mkdir -p $out
+      cp -r bin $out
+    '';
+  };
+in buildFHSEnv
+{
+  name = "vasp-gpu";
+  targetPkgs = pkgs: with pkgs; [ nvhpc gfortran zlib mkl lmod vasp ];
 }
