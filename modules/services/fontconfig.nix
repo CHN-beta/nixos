8cf5fa8635025bb24162053cc2df5231d106de85
chn <chn@chn.moe>
2024-02-26 12:48:49 +0800

    services.fontconfig: fix

--- modules/services/fontconfig.nix
+++ modules/services/fontconfig.nix
@@ -14,13 +14,16 @@ inputs:
       {
         fontDir.enable = true;
         packages = with inputs.pkgs;
-          [ noto-fonts source-han-sans source-han-serif source-code-pro hack-font jetbrains-mono nerdfonts ];
+        [
+          noto-fonts source-han-sans source-han-serif source-code-pro hack-font jetbrains-mono nerdfonts hack-font inter
+          noto-fonts-color-emoji roboto sarasa-gothic source-han-mono wqy_microhei wqy_zenhei
+        ];
         fontconfig.defaultFonts =
         {
           emoji = [ "Noto Color Emoji" ];
-          monospace = [ "Noto Sans Mono CJK SC" "Sarasa Mono SC" "DejaVu Sans Mono"];
-          sansSerif = [ "Noto Sans CJK SC" "Source Han Sans SC" "DejaVu Sans" ];
-          serif = [ "Noto Serif CJK SC" "Source Han Serif SC" "DejaVu Serif" ];
+          monospace = [ "Hack" "Source Han Mono SC" ];
+          sansSerif = [ "Inter" "Liberation Sans" "Source Han Sans SC" ];
+          serif = [ "Liberation Serif" "Source Han Serif SC" ];
         };
       };
       nixos.users.sharedModules = [{ config.xdg.configFile."fontconfig/conf.d/10-hm-fonts.conf".force = true; }];
