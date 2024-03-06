2c472400c8f8bf7bee1295e4b5cd3148b6599974
chn <chn@chn.moe>
2024-03-06 17:49:29 +0800

    devices.pc: fix gpu

--- devices/pc/default.nix
+++ devices/pc/default.nix
@@ -60,7 +60,7 @@ inputs:
       hardware =
       {
         cpus = [ "amd" ];
-        gpu.type = "nvidia";
+        gpu = { type = "nvidia"; dynamicBoost = true; };
         bluetooth.enable = true;
         joystick.enable = true;
         printer.enable = true;
