53a2a6ff0c1f5f3ccde1f6b01dd2f4164b70685d
chn <chn@chn.moe>
2024-03-27 18:46:45 +0800

    system.systemd: 整理

--- modules/system/systemd.nix
+++ modules/system/systemd.nix
@@ -1,19 +1,22 @@
-inputs: { config =
+inputs:
 {
-  # only preserve the last 7 days of logs
-  services.journald.extraConfig = "MaxRetentionSec=7d";
-  systemd =
+  config =
   {
-    extraConfig =
-    ''
-      DefaultTimeoutStopSec=10s
-      DefaultLimitNOFILE=1048576:1048576
-    '';
-    user.extraConfig = "DefaultTimeoutStopSec=10s";
-    # do not create /var/lib/machines and /var/lib/portables as subvolumes
-    services.systemd-tmpfiles-setup.environment.SYSTEMD_TMPFILES_FORCE_SUBVOL = "0";
-    # do not clean /tmp
-    timers.systemd-tmpfiles-clean.enable = false;
-    coredump = { enable = true; extraConfig = "Storage=none"; };
+    # only preserve the last 7 days of logs
+    services.journald.extraConfig = "MaxRetentionSec=7d";
+    systemd =
+    {
+      extraConfig =
+      ''
+        DefaultTimeoutStopSec=10s
+        DefaultLimitNOFILE=1048576:1048576
+      '';
+      user.extraConfig = "DefaultTimeoutStopSec=10s";
+      # do not create /var/lib/machines and /var/lib/portables as subvolumes
+      services.systemd-tmpfiles-setup.environment.SYSTEMD_TMPFILES_FORCE_SUBVOL = "0";
+      # do not clean /tmp
+      timers.systemd-tmpfiles-clean.enable = false;
+      coredump = { enable = true; extraConfig = "Storage=none"; };
+    };
   };
-};}
+}
