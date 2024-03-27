b04b1375d03a91529daaf48ac8598668290b5a6f
chn <chn@chn.moe>
2024-03-27 18:47:56 +0800

    system.sysctl: 整理

--- modules/system/sysctl.nix
+++ modules/system/sysctl.nix
@@ -4,21 +4,17 @@ inputs:
   {
     laptop-mode = mkOption { type = types.nullOr types.int; default = null; };
   };
-  config =
-    let
-      inherit (inputs.lib) mkIf mkMerge;
-      inherit (inputs.config.nixos.system) sysctl;
-    in mkMerge
-    [
+  config = let inherit (inputs.config.nixos.system) sysctl; in inputs.lib.mkMerge
+  [
+    {
+      boot.kernel.sysctl =
       {
-        boot.kernel.sysctl =
-        {
-          "vm.oom_kill_allocating_task" = true;
-          "vm.oom_dump_tasks" = false;
-          "vm.overcommit_memory" = 1;
-          "kernel.sysrq" = 438;
-        };
-      }
-      (mkIf (sysctl.laptop-mode != null) { boot.kernel.sysctl."vm.laptop_mode" = sysctl.laptop-mode; })
-    ];
+        "vm.oom_kill_allocating_task" = true;
+        "vm.oom_dump_tasks" = false;
+        "vm.overcommit_memory" = 1;
+        "kernel.sysrq" = 438;
+      };
+    }
+    (inputs.lib.mkIf (sysctl.laptop-mode != null) { boot.kernel.sysctl."vm.laptop_mode" = sysctl.laptop-mode; })
+  ];
 }
