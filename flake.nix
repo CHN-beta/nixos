562e51a86634f79d8b8e08e74cbbf06d104ffe69
chn <chn@chn.moe>
2024-04-01 18:55:33 +0800

    fix overlay

--- flake.nix
+++ flake.nix
@@ -172,7 +172,7 @@
       };
       checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks inputs.self.deploy) inputs.deploy-rs.lib;
       overlays.default = final: prev:
-        { localPackages = (import ./local/pkgs { inherit (inputs) lib; pkgs = final; }); };
+        { localPackages = (import ./local/pkgs { inherit (inputs) lib; pkgs = final; topInputs = inputs; }); };
       config.archive = false;
     };
 }
