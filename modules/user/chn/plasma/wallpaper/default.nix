6ab9fe88ca8f6f619000fa4d8c20841e9a6d5b23
chn <chn@chn.moe>
2024-04-07 23:50:18 +0800

    users.chn.plasma.wallpaper: set different wallpapers

--- modules/user/chn/plasma/wallpaper/default.nix
+++ modules/user/chn/plasma/wallpaper/default.nix
@@ -1,13 +1,20 @@
 inputs:
 {
   config.home-manager.users.chn.config.programs.plasma.configFile =
-    let wallpaper = "${./pixiv-96734339-x2.png}"; in
+    let wallpaper =
+    {
+      pc = ./pixiv-117612023.png;
+      surface = ./pixiv-116547323.jpg;
+    }.${inputs.config.nixos.system.networking.hostname} or ./pixiv-96734339-x2.png;
+    in
     {
       "plasma-org.kde.plasma.desktop-appletsrc" =
       {
         "Containments.1".wallpaperplugin = "a2n.blur";
-        "Containments.1.Wallpaper.a2n\\.blur.General".Image = wallpaper;
+        "Containments.1.Wallpaper.a2n\\.blur.General".Image = "${wallpaper}";
       };
-      kscreenlockerrc."Greeter.Wallpaper.org\\.kde\\.image.General" = { Image = wallpaper; PreviewImage = wallpaper; };
+      kscreenlockerrc."Greeter.Wallpaper.org\\.kde\\.image.General" =
+        { Image = "${wallpaper}"; PreviewImage = "${wallpaper}"; };
+      kdeglobals.General.accentColorFromWallpaper = true;
     };
 }
