25e317841d47b537e852260fae202f678364fb5b
chn <chn@chn.moe>
2024-04-12 15:43:38 +0800

    topInputs.chaotic: do not fix rev

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
     nix-flatpak.url = "github:gmodena/nix-flatpak";
     chaotic =
     {
-      url = "github:chaotic-cx/nyx?rev=03b2bea544688068025df1912ff1e9a1ad4a642a";
+      url = "github:chaotic-cx/nyx";
       inputs = { nixpkgs.follows = "nixpkgs"; home-manager.follows = "home-manager"; };
     };
     gricad = { url = "github:Gricad/nur-packages"; flake = false; };
