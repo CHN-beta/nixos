d8d82114935319a188350a47beff96912b2341f8
chn <chn@chn.moe>
2024-04-14 11:53:16 +0800

    packages.vasp: remove unused vasp installation

--- modules/packages/vasp.nix
+++ modules/packages/vasp.nix
@@ -4,8 +4,8 @@ inputs:
   {
     nixos.packages._packages =
       (builtins.concatLists (builtins.map
-        (compiler: builtins.map (version: inputs.pkgs.localPackages.vasp.${compiler}.${version}) [ "6.3.1" "6.4.0" ])
-        [ "amd" "gnu" "gnu-mkl" "intel" "nvidia" ]))
+        (compiler: builtins.map (version: inputs.pkgs.localPackages.vasp.${compiler}.${version}) [ "6.4.0" ])
+        [ "intel" "nvidia" ]))
       ++ [ inputs.pkgs.localPackages.vasp.vtstscripts ];
   };
 }
