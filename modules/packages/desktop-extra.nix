83e54aa09d0101ae7225948274997ac77d5432f8
chn <chn@chn.moe>
2024-05-04 10:43:53 +0800

    packages: remove nextcloud-client

--- modules/packages/desktop-extra.nix
+++ modules/packages/desktop-extra.nix
@@ -35,7 +35,6 @@ inputs:
           # office
           crow-translate zotero pandoc ydict libreoffice-qt texstudio poppler_utils pdftk gnuplot pdfchain hdfview
           (texlive.combine { inherit (texlive) scheme-full; inherit (localPackages) citation-style-language; })
-          nextcloud-client
           # math, physics and chemistry
           octaveFull root ovito localPackages.vesta localPackages.vaspkit localPackages.v-sim
         ] ++ (with inputs.lib; filter isDerivation (attrValues plasma5Packages.kdeGear));
