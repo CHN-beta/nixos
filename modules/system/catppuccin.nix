2d1bfb84ebbf2e6d0f6fc27880daa2de37acd27c
chn <chn@chn.moe>
2024-05-04 16:15:50 +0800

    system.catppuccin: force set btop config

--- modules/system/catppuccin.nix
+++ modules/system/catppuccin.nix
@@ -14,7 +14,7 @@ inputs:
           bat = { enable = true; catppuccin.enable = true; };
           btop = { enable = true; catppuccin.enable = true; };
         };
-        xdg.enable = true;
+        xdg = { enable = true; configFile."btop/btop.conf".force = true; };
       };
     }];
   };
