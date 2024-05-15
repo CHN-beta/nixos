10495ee0cd363e61be2150c8958c2109906f880e
chn <chn@chn.moe>
2024-05-15 13:55:01 +0800

    devices.nas: add disk

--- devices/nas/default.nix
+++ devices/nas/default.nix
@@ -32,12 +32,14 @@ inputs:
             {
               "/dev/disk/by-uuid/5cf1d19d-b4a5-4e67-8e10-f63f0d5bb649".mapper = "root1";
               "/dev/disk/by-uuid/aa684baf-fd8a-459c-99ba-11eb7636cb0d".mapper = "root2";
+              "/dev/disk/by-uuid/a47f06e1-dc90-40a4-89ea-7c74226a5449".mapper = "root3";
+              "/dev/disk/by-uuid/b3408fb5-68de-405b-9587-5e6fbd459ea2".mapper = "root4";
               "/dev/disk/by-uuid/a779198f-cce9-4c3d-a64a-9ec45f6f5495" = { mapper = "nix"; ssd = true; };
             };
             delayedMount = [ "/" "/nix" ];
           };
           swap = [ "/nix/swap/swap" ];
-          rollingRootfs.waitDevices = [ "/dev/mapper/root2" ];
+          rollingRootfs.waitDevices = [ "/dev/mapper/root2" "/dev/mapper/root3" "/dev/mapper/root4" ];
         };
         initrd.sshd.enable = true;
         grub.installDevice = "efi";
