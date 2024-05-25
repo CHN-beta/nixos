22464c272a656b54e301dda89b48331a84e893a1
chn <chn@chn.moe>
2024-05-25 19:01:12 +0800

    try to fix plasma6

--- modules/system/gui.nix
+++ modules/system/gui.nix
@@ -6,31 +6,26 @@ inputs:
     preferred = mkOption { type = types.bool; default = inputs.config.nixos.system.gui.enable; };
     autoStart = mkOption { type = types.bool; default = inputs.config.nixos.system.gui.preferred; };
   };
-  config =
-    let
-      inherit (builtins) map;
-      inherit (inputs.lib) mkIf;
-      inherit (inputs.config.nixos.system) gui;
-    in mkIf gui.enable
+  config = let inherit (inputs.config.nixos.system) gui; in inputs.lib.mkIf gui.enable
+  {
+    services =
     {
-      services =
-      {
-        displayManager = { sddm.enable = true; defaultSession = "plasma"; };
-        desktopManager.plasma6.enable = true;
-        xserver.enable = true;
-      };
-      systemd.services.display-manager.enable = gui.autoStart;
-      environment =
-      {
-        sessionVariables."GTK_USE_PORTAL" = "1";
-        plasma6.excludePackages = inputs.lib.mkIf (!gui.preferred) [ inputs.pkgs.kdePackages.plasma-nm ];
-      };
-      xdg.portal.extraPortals = map (p: inputs.pkgs."xdg-desktop-portal-${p}") [ "gtk" "kde" "wlr" ];
-      i18n.inputMethod =
-      {
-        enabled = "fcitx5";
-        fcitx5.addons = map (p: inputs.pkgs."fcitx5-${p}") [ "rime" "chinese-addons" "mozc" "nord" "material-color" ];
-      };
-      programs = { dconf.enable = true; xwayland.enable = true; };
+      displayManager = { sddm = { enable = true; wayland.enable = true; }; defaultSession = "plasma"; };
+      desktopManager.plasma6.enable = true;
+      xserver.enable = true;
     };
+    systemd.services.display-manager.enable = gui.autoStart;
+    environment =
+    {
+      sessionVariables."GTK_USE_PORTAL" = "1";
+      plasma6.excludePackages = inputs.lib.mkIf (!gui.preferred) [ inputs.pkgs.kdePackages.plasma-nm ];
+    };
+    xdg.portal.extraPortals = map (p: inputs.pkgs."xdg-desktop-portal-${p}") [ "gtk" "kde" "wlr" ];
+    i18n.inputMethod =
+    {
+      enabled = "fcitx5";
+      fcitx5.addons = map (p: inputs.pkgs."fcitx5-${p}") [ "rime" "chinese-addons" "mozc" "nord" "material-color" ];
+    };
+    programs = { dconf.enable = true; xwayland.enable = true; };
+  };
 }
