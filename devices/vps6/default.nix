0ab4353016e8cb8f7bbdaa15184318bcd1c5a650
chn <chn@chn.moe>
2024-05-25 17:01:39 +0800

    devices.vps6.xray: add user

--- devices/vps6/default.nix
+++ devices/vps6/default.nix
@@ -35,7 +35,7 @@ inputs:
       {
         snapper.enable = true;
         sshd = {};
-        xray.server = { serverName = "vps6.xserver.chn.moe"; userNumber = 17; };
+        xray.server = { serverName = "vps6.xserver.chn.moe"; userNumber = 18; };
         frpServer = { enable = true; serverName = "frp.chn.moe"; };
         nginx =
         {
