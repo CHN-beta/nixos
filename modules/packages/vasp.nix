6e736c80932d55c8cdc022e2dc44cc239570935d
chn <chn@chn.moe>
2024-05-29 14:37:04 +0800

    Revert "packages.vasp: add gnu"
    This reverts commit eb97197d0ddd6df5763b3445c5ef911154eb7b98.

--- modules/packages/vasp.nix
+++ modules/packages/vasp.nix
@@ -2,6 +2,6 @@ inputs:
 {
   config = inputs.lib.mkIf (builtins.elem "workstation" inputs.config.nixos.packages._packageSets)
   {
-    nixos.packages._packages = with inputs.pkgs.localPackages.vasp; [ intel nvidia gnu vtstscripts ];
+    nixos.packages._packages = with inputs.pkgs.localPackages.vasp; [ intel nvidia vtstscripts ];
   };
 }
