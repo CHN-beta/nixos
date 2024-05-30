cf00062fab2aba216db573cae7e9eab749700729
chn <chn@chn.moe>
2024-05-30 14:10:13 +0800

    packages.emacs: remove

--- modules/packages/emacs/default.nix
+++
@@ -1,7 +0,0 @@
-inputs:
-{
-  config = inputs.lib.mkIf (builtins.elem "workstation" inputs.config.nixos.packages._packageSets)
-  {
-    home-manager.users.chn.config.programs.doom-emacs = { enable = true; doomPrivateDir = ./doom.d; };
-  };
-}
