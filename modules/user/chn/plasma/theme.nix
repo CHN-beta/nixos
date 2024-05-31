b7ffc2cbe27c45e09421c929089ea00b244ca30d
chn <chn@chn.moe>
2024-05-31 13:24:34 +0800

    user.chn: restore some plasma settings

--- modules/user/chn/plasma/theme.nix
+++ modules/user/chn/plasma/theme.nix
@@ -4,35 +4,35 @@ inputs:
   {
     home-manager.users.chn.config.programs.plasma =
     {
-      # workspace =
-      # {
-      #   theme = "Fluent-round-light";
-      #   colorScheme = "FluentLight";
-      #   cursorTheme = "Breeze_Light";
-      #   lookAndFeel = "com.github.vinceliuice.Fluent-round-light";
-      #   # ~/.config/kdeglobals [Icons]
-      #   iconTheme = "Tela-circle-light";
-      # };
-      # configFile =
-      # {
-      #   kdeglobals.KDE.widgetStyle.value = "kvantum";
-      #   "Kvantum/kvantum.kvconfig".General.theme.value = "Fluent-round";
-      #   kwinrc =
-      #   {
-      #     Effect-blur.BlurStrength.value = 10;
-      #     Effect-kwin4_effect_translucency.MoveResize.value = 75;
-      #     Effect-wobblywindows =
-      #       { AdvancedMode.value = true; Drag.value = 85; Stiffness.value = 10; WobblynessLevel.value = 1; };
-      #     Plugins =
-      #     {
-      #       blurEnabled.value = true;
-      #       kwin4_effect_dimscreenEnabled.value = true;
-      #       kwin4_effect_translucencyEnabled.value = true;
-      #       padding.value = 4;
-      #       wobblywindowsEnabled.value = true;
-      #     };
-      #   };
-      # };
+      workspace =
+      {
+        theme = "Fluent-round-light";
+        colorScheme = "FluentLight";
+        cursorTheme = "Breeze_Light";
+        lookAndFeel = "com.github.vinceliuice.Fluent-round-light";
+        # ~/.config/kdeglobals [Icons]
+        iconTheme = "Tela-circle-light";
+      };
+      configFile =
+      {
+        kdeglobals.KDE.widgetStyle.value = "kvantum";
+        "Kvantum/kvantum.kvconfig".General.theme.value = "Fluent-round";
+        kwinrc =
+        {
+          Effect-blur.BlurStrength.value = 10;
+          Effect-kwin4_effect_translucency.MoveResize.value = 75;
+          Effect-wobblywindows =
+            { AdvancedMode.value = true; Drag.value = 85; Stiffness.value = 10; WobblynessLevel.value = 1; };
+          Plugins =
+          {
+            blurEnabled.value = true;
+            kwin4_effect_dimscreenEnabled.value = true;
+            kwin4_effect_translucencyEnabled.value = true;
+            padding.value = 4;
+            wobblywindowsEnabled.value = true;
+          };
+        };
+      };
     };
   };
 }
