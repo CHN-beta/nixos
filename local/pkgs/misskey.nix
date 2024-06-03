4a2c0778e3f3cb57a1c7dd870fb98e77f515ea58
chn <chn@chn.moe>
2024-06-03 17:03:39 +0800

    update everything

--- local/pkgs/misskey.nix
+++ local/pkgs/misskey.nix
@@ -4,7 +4,7 @@
 }: (mkPnpmPackage.override { inherit nodejs; })
   {
     inherit src;
-    extraIntegritySha256."https://github.com/aiscript-dev/aiscript-languageserver/releases/download/0.1.5/aiscript-dev-aiscript-languageserver-0.1.5.tgz" = "1mhnwa8h48bc21f0zv8q93aphiqz9i70r7m4xsa4sd1mlncfgyl7";
+    extraIntegritySha256."https://github.com/aiscript-dev/aiscript-languageserver/releases/download/0.1.6/aiscript-dev-aiscript-languageserver-0.1.6.tgz" = "0092d5r67bhf4xkvrdn4a2rm1drjzy7b5sw8mi7hp4pqvpc20ylr";
     extraNativeBuildInputs = [ bash nodejs.pkgs.typescript nodejs.pkgs.gulp ];
     extraAttrs =
     {
