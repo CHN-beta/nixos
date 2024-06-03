7afafdaac51bf7c5e7a6f0eb80c59d8704d0c42d
chn <chn@chn.moe>
2024-06-03 12:49:03 +0800

    enable plasma-manager

--- modules/user/chn/plasma/theme.nix
+++ modules/user/chn/plasma/theme.nix
@@ -5,35 +5,27 @@ inputs:
     home-manager.users.chn.config.programs.plasma =
     {
       # TODO: do not setup theme before clean these configs
-      # workspace =
-      # {
-      #   theme = "Fluent-round-light";
-      #   colorScheme = "FluentLight";
-      #   cursorTheme = "Breeze_Light";
-      #   lookAndFeel = "com.github.vinceliuice.Fluent-round-light";
-      #   # ~/.config/kdeglobals [Icons]
-      #   iconTheme = "Fluent";
-      # };
-      # configFile =
-      # {
-      #   # kdeglobals.KDE.widgetStyle.value = "kvantum";
-      #   # "Kvantum/kvantum.kvconfig".General.theme.value = "Fluent-round";
-      #   kwinrc =
-      #   {
-      #     Effect-blur.BlurStrength.value = 10;
-      #     Effect-kwin4_effect_translucency.MoveResize.value = 75;
-      #     Effect-wobblywindows =
-      #       { AdvancedMode.value = true; Drag.value = 85; Stiffness.value = 10; WobblynessLevel.value = 1; };
-      #     Plugins =
-      #     {
-      #       blurEnabled.value = true;
-      #       kwin4_effect_dimscreenEnabled.value = true;
-      #       kwin4_effect_translucencyEnabled.value = true;
-      #       padding.value = 4;
-      #       wobblywindowsEnabled.value = true;
-      #     };
-      #   };
-      # };
+      workspace = { theme = "Utterly-Round"; };
+      configFile =
+      {
+        kdeglobals.KDE.widgetStyle.value = "kvantum";
+        "Kvantum/kvantum.kvconfig".General.theme.value = "Utterly-Nord-Light";
+        kwinrc =
+        {
+          Effect-blur.BlurStrength.value = 10;
+          Effect-kwin4_effect_translucency.MoveResize.value = 75;
+          Effect-wobblywindows =
+            { AdvancedMode.value = true; Drag.value = 85; Stiffness.value = 10; WobblynessLevel.value = 1; };
+          Plugins =
+          {
+            blurEnabled.value = true;
+            kwin4_effect_dimscreenEnabled.value = true;
+            kwin4_effect_translucencyEnabled.value = true;
+            padding.value = 4;
+            wobblywindowsEnabled.value = true;
+          };
+        };
+      };
     };
   };
 }
