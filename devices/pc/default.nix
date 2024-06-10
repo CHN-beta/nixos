de68b75268cf9b6cda0c87320f7851647fa01809
chn <chn@chn.moe>
2024-06-10 20:26:55 +0800

    add BPD-paper-old

--- devices/pc/default.nix
+++ devices/pc/default.nix
@@ -150,7 +150,7 @@ inputs:
       hideMounts = true;
       users.chn.directories = builtins.map
         (dir: { directory = "repo/${dir}"; user = "chn"; group = "chn"; mode = "0755"; })
-        [ "lammps-SiC" "BPD-paper" "kurumi-asmr" "linwei-stuff" ];
+        [ "lammps-SiC" "BPD-paper" "kurumi-asmr" "linwei-stuff" "BPD-paper-old" ];
     };
     specialisation =
     {
