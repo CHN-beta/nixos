bc7561ed10a0fda6a90f554ff81d106cd81017d0
chn <chn@chn.moe>
2024-07-05 15:17:07 +0800

    localPackages.misskey: fix

--- local/pkgs/misskey.nix
+++ local/pkgs/misskey.nix
@@ -1,11 +1,11 @@
 {
-  lib, mkPnpmPackage, nodejs, writeShellScript,
-  bash, cypress, vips, src
+  lib, mkPnpmPackage, nodejs, writeShellScript, src,
+  bash, cypress, vips, python3
 }: (mkPnpmPackage.override { inherit nodejs; })
   {
     inherit src;
     extraIntegritySha256."https://github.com/aiscript-dev/aiscript-languageserver/releases/download/0.1.6/aiscript-dev-aiscript-languageserver-0.1.6.tgz" = "0092d5r67bhf4xkvrdn4a2rm1drjzy7b5sw8mi7hp4pqvpc20ylr";
-    extraNativeBuildInputs = [ bash nodejs.pkgs.typescript nodejs.pkgs.gulp ];
+    extraNativeBuildInputs = [ bash nodejs.pkgs.typescript nodejs.pkgs.gulp python3 ];
     extraAttrs =
     {
       CYPRESS_INSTALL_BINARY = "0";
