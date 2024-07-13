3156219ae0a35de07c209af109b6942b40bba992
chn <chn@chn.moe>
2024-07-13 14:11:05 +0800

    packages.git: workaround for lfs ssh multiplexing

--- modules/packages/git.nix
+++ modules/packages/git.nix
@@ -11,6 +11,7 @@ inputs:
       {
         init.defaultBranch = "main";
         core.quotepath = false;
+        lfs.ssh.automultiplex = false; # 避免 lfs 一直要求触摸 yubikey
       };
     };
     nixos.packages._packages = [ inputs.pkgs.localPackages.git-lfs-transfer ]; # make pure ssh lfs work
