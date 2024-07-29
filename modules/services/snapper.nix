6e16e52064650318e1a19b1acc1dbb3aead73e35
chn <chn@chn.moe>
2024-07-29 22:44:40 +0800

    services.snapper: fix

--- modules/services/snapper.nix
+++ modules/services/snapper.nix
@@ -5,33 +5,25 @@ inputs:
     enable = mkOption { type = types.bool; default = false; };
     configs = mkOption { type = types.attrsOf types.nonEmptyStr; default.persistent = "/nix/persistent"; };
   };
-  config =
-    let
-      inherit (inputs.lib) mkMerge mkIf;
-      inherit (inputs.localLib) stripeTabs attrsToList;
-      inherit (inputs.config.nixos) services;
-      inherit (builtins) map listToAttrs toString;
-    in mkIf services.snapper.enable
-    {
-      services.snapper.configs =
-        let
-          f = (config:
-          {
-            inherit (config) name;
-            value =
-            {
-              SUBVOLUME = config.value;
-              TIMELINE_CREATE = true;
-              TIMELINE_CLEANUP = true;
-              TIMELINE_MIN_AGE = 1800;
-              TIMELINE_LIMIT_HOURLY = "10";
-              TIMELINE_LIMIT_DAILY = "7";
-              TIMELINE_LIMIT_WEEKLY = "1";
-              TIMELINE_LIMIT_MONTHLY = "0";
-              TIMELINE_LIMIT_YEARLY = "0";
-            };
-          });
-        in
-          listToAttrs (map f (attrsToList services.snapper.configs));
-    };
+  config = let inherit (inputs.config.nixos.services) snapper; in inputs.lib.mkIf snapper.enable
+  {
+    services.snapper.configs = builtins.listToAttrs (builtins.map
+      (config:
+      {
+        inherit (config) name;
+        value =
+        {
+          SUBVOLUME = config.value;
+          TIMELINE_CREATE = true;
+          TIMELINE_CLEANUP = true;
+          TIMELINE_MIN_AGE = 1800;
+          TIMELINE_LIMIT_HOURLY = 10;
+          TIMELINE_LIMIT_DAILY = 7;
+          TIMELINE_LIMIT_WEEKLY = 1;
+          TIMELINE_LIMIT_MONTHLY = 0;
+          TIMELINE_LIMIT_YEARLY = 0;
+        };
+      })
+      (inputs.localLib.attrsToList snapper.configs));
+  };
 }
