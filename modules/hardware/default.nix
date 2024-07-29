167ed596d8d4c7e20b40f6bf4a14c140bc2b5141
chn <chn@chn.moe>
2024-07-29 14:43:59 +0800

    hardware.sound: fix

--- modules/hardware/default.nix
+++ modules/hardware/default.nix
@@ -39,7 +39,6 @@ inputs:
       {
         hardware.pulseaudio.enable = false;
         services.pipewire = { enable = true; alsa = { enable = true; support32Bit = true; }; pulse.enable = true; };
-        sound.enable = true;
         security.rtkit.enable = true;
       }
     )
