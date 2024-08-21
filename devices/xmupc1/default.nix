5f373767f3c6861538e61e8740af45882d1fd5fa
chn <chn@chn.moe>
2024-08-21 17:57:07 +0800

    users.zqq: init

--- devices/xmupc1/default.nix
+++ devices/xmupc1/default.nix
@@ -91,7 +91,7 @@ inputs:
         hpcstat = {};
       };
       bugs = [ "xmunet" "amdpstate" ];
-      user.users = [ "chn" "xll" "zem" "yjq" "gb" ];
+      user.users = [ "chn" "xll" "zem" "yjq" "gb" "zqq" ];
     };
     services.hardware.bolt.enable = true;
   };
