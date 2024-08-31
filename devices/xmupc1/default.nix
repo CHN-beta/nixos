b0ed4921178f1e88b2e1d0b3d9ba1e45aea55f36
chn <chn@chn.moe>
2024-08-31 23:57:56 +0800

    devices.xmupc1: add zzn

--- devices/xmupc1/default.nix
+++ devices/xmupc1/default.nix
@@ -91,7 +91,7 @@ inputs:
         hpcstat = {};
       };
       bugs = [ "xmunet" "amdpstate" ];
-      user.users = [ "chn" "xll" "zem" "yjq" "gb" "wp" "hjp" ];
+      user.users = [ "chn" "xll" "zem" "yjq" "gb" "wp" "hjp" "zzn" ];
     };
     services.hardware.bolt.enable = true;
   };
