651b1870affc289e8b5b1c521d40ec5572b6ff33
chn <chn@chn.moe>
2024-09-02 13:18:50 +0800

    user.chn: use zsh

--- modules/user/chn/default.nix
+++ modules/user/chn/default.nix
@@ -10,7 +10,6 @@ inputs:
         (builtins.attrNames inputs.config.users.groups);
       autoSubUidGidRange = true;
       hashedPassword = "$y$j9T$xJwVBoGENJEDSesJ0LfkU1$VEExaw7UZtFyB4VY1yirJvl7qS7oiF49KbEBrV0.hhC";
-      shell = inputs.lib.mkForce inputs.pkgs.nushell;
     };
     home-manager.users.chn =
     {
