301df4f58c5106096bebfac58ef0740089a5c5b1
chn <chn@chn.moe>
2024-09-04 16:26:39 +0800

    modules.packages.vscode: remove some extensions

--- modules/packages/vscode.nix
+++ modules/packages/vscode.nix
@@ -26,53 +26,34 @@ inputs:
                 ++ (builtins.attrNames vscode-extensions)
               )));
             in with extensions;
-              (with equinusocio; [ vsc-material-theme vsc-material-theme-icons ])
-              ++ (with github; [ copilot copilot-chat github-vscode-theme ])
-              ++ (with intellsmi; [ comment-translate deepl-translate ])
-              ++ (with ms-python; [ isort python vscode-pylance ])
-              ++ (with ms-toolsai;
-              [
-                jupyter jupyter-keymap jupyter-renderers vscode-jupyter-cell-tags vscode-jupyter-slideshow
-              ])
-              ++ (with ms-vscode;
-              [
-                (cmake-tools.overrideAttrs { sourceRoot = "extension"; }) cpptools cpptools-extension-pack cpptools-themes hexeditor remote-explorer
-                test-adapter-converter
-              ])
-              ++ (with ms-vscode-remote; [ remote-ssh remote-containers remote-ssh-edit ])
+              (with github; [ copilot github-vscode-theme ])
+              ++ (with intellsmi; [ comment-translate ])
+              ++ (with ms-vscode; [ cmake-tools cpptools cpptools-extension-pack hexeditor remote-explorer ])
+              ++ (with ms-vscode-remote; [ remote-ssh ])
               ++ [
-                donjayamanne.githistory genieai.chatgpt-vscode fabiospampinato.vscode-diff cschlosser.doxdocgen
+                donjayamanne.githistory fabiospampinato.vscode-diff
                 llvm-vs-code-extensions.vscode-clangd ms-ceintl.vscode-language-pack-zh-hans
                 oderwat.indent-rainbow
                 twxs.cmake guyutongxue.cpp-reference thfriedrich.lammps leetcode.vscode-leetcode # znck.grammarly
-                james-yu.latex-workshop gimly81.matlab affenwiesel.matlab-formatter ckolkman.vscode-postgres
-                yzhang.markdown-all-in-one pkief.material-icon-theme bbenoist.nix ms-ossdata.vscode-postgresql
-                redhat.vscode-xml dotjoshjohnson.xml jnoortheen.nix-ide xdebug.php-debug
-                hbenl.vscode-test-explorer
-                jeff-hykin.better-cpp-syntax fredericbonnet.cmake-test-adapter mesonbuild.mesonbuild
-                hirse.vscode-ungit fortran-lang.linter-gfortran tboox.xmake-vscode ccls-project.ccls
-                feiskyer.chatgpt-copilot yukiuuh2936.vscode-modern-fortran-formatter wolframresearch.wolfram
-                njpipeorgan.wolfram-language-notebook brettm12345.nixfmt-vscode webfreak.debug
+                james-yu.latex-workshop bbenoist.nix jnoortheen.nix-ide ccls-project.ccls
+                brettm12345.nixfmt-vscode
                 gruntfuggly.todo-tree
                 # restrctured text
-                lextudio.restructuredtext trond-snekvik.simple-rst
+                lextudio.restructuredtext trond-snekvik.simple-rst swyddfa.esbonio chrisjsewell.myst-tml-syntax
                 # markdown
-                shd101wyy.markdown-preview-enhanced
+                yzhang.markdown-all-in-one shd101wyy.markdown-preview-enhanced
                 # vasp
                 mystery.vasp-support
                 yutengjing.open-in-external-app
-                # ChatGPT-like plugin
-                codeium.codeium
                 # git graph
                 mhutchie.git-graph
+                # python
+                ms-python.python
+                # theme
+                pkief.material-icon-theme
               ];
         }
       )];
-      _pythonPackages = [(pythonPackages: with pythonPackages;
-      [
-        # required by vscode extensions restrucuredtext
-        localPackages.esbonio
-      ])];
     };
   };
 }
---
+++ packages/ufo/example/3/1/build.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+cp ../../1/super_cell_relax/CONTCAR POSCAR
+phonopy ./phonopy.conf
