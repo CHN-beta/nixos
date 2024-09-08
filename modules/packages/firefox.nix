b47a1d2a14d04e557855ed631cd7d0592b30a475
chn <chn@chn.moe>
2024-09-08 19:19:07 +0800

    modules.packages.firefox: use addons from rycee

--- modules/packages/firefox.nix
+++ modules/packages/firefox.nix
@@ -33,34 +33,7 @@ inputs:
               [
                 tampermonkey bitwarden cookies-txt dualsub firefox-color i-dont-care-about-cookies
                 metamask pakkujs switchyomega rsshub-radar rsspreview tabliss tree-style-tab ublock-origin wallabagger
-                wappalyzer grammarly plasma-integration zotero-connector 
-                (buildFirefoxXpiAddon
-                {
-                  pname = "pwas-for-firefox";
-                  version = "2.12.1";
-                  addonId = "firefoxpwa@filips.si";
-                  url = "https://addons.mozilla.org/firefox/downloads/file/4293028/pwas_for_firefox-2.12.1.xpi";
-                  sha256 = "m8BCAlQt37RxVnWw+2hIPnmofTicNa5OWkwXp/IXdWY=";
-                  meta = {};
-                })
-                (buildFirefoxXpiAddon
-                {
-                  pname = "smartproxy";
-                  version = "1.5";
-                  addonId = "smartproxy@salarcode.com";
-                  url = "https://addons.mozilla.org/firefox/downloads/file/4323346/smartproxy-1.5.xpi";
-                  sha256 = "1k0rk002iys9n0i4lvzcrllhw2zdrh4j2fshnxci50c7vwn6qfnm";
-                  meta = {};
-                })
-                (buildFirefoxXpiAddon
-                {
-                  pname = "kiss-translator";
-                  version = "1.8.11";
-                  addonId = "{fb25c100-22ce-4d5a-be7e-75f3d6f0fc13}";
-                  url = "https://addons.mozilla.org/firefox/downloads/file/4291806/kiss_translator-1.8.11.xpi";
-                  sha256 = "09mnd89ssd4cvbb092m4yza6brnq4nmzx765vgcdyffcc1drv5c9";
-                  meta = {};
-                })
+                wappalyzer grammarly plasma-integration zotero-connector pwas-for-firefox smartproxy kiss-translator
               ];
               search = { default = "Google"; force = true; };
               userChrome = builtins.readFile "${inputs.topInputs.lepton}/userChrome.css";
