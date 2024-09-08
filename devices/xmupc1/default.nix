43e9cd48d762d949466561ce5f72c09626c7188c
chn <chn@chn.moe>
2024-09-08 19:20:53 +0800

    devices.xmupc1: remove zzn

--- devices/xmupc1/default.nix
+++ devices/xmupc1/default.nix
@@ -90,7 +90,7 @@ inputs:
         hpcstat = {};
       };
       bugs = [ "xmunet" "amdpstate" ];
-      user.users = [ "chn" "xll" "zem" "yjq" "gb" "wp" "hjp" "zzn" "wm" ];
+      user.users = [ "chn" "xll" "zem" "yjq" "gb" "wp" "hjp" "wm" ];
     };
     services.hardware.bolt.enable = true;
   };
