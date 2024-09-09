73572b0964501e41f0641fd3e3c079a9e4d834e9
chn <chn@chn.moe>
2024-09-09 23:12:00 +0800

    modules.system.envfs: use module from nixpkgs

--- modules/system/envfs.nix
+++ modules/system/envfs.nix
@@ -2,9 +2,9 @@ inputs:
 {
   options.nixos.system.envfs = let inherit (inputs.lib) mkOption types; in mkOption
     { type = types.nullOr (types.submodule {}); default = {}; };
-  config = let inherit (inputs.config.nixos.system) envfs; in inputs.lib.mkIf (envfs != null) (inputs.lib.mkMerge
-  [
-    (builtins.elemAt inputs.topInputs.envfs.nixosModules.envfs.imports 0 inputs)
-    { environment.variables.ENVFS_RESOLVE_ALWAYS = "1"; }
-  ]);
+  config = let inherit (inputs.config.nixos.system) envfs; in inputs.lib.mkIf (envfs != null)
+  {
+    services.envfs.enable = true;
+    environment.variables.ENVFS_RESOLVE_ALWAYS = "1";
+  };
 }
