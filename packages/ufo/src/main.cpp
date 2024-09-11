1fcb16562275674d9781f20c01062a332a02b472
chn <chn@chn.moe>
2024-09-12 05:44:37 +0800

    packages.ufo: fix

--- packages/ufo/src/main.cpp
+++ packages/ufo/src/main.cpp
@@ -8,8 +8,8 @@ int main(int argc, const char** argv)
     ufo::fold(argv[2]);
   else if (argv[1] == std::string("unfold"))
     ufo::unfold(argv[2]);
-  // else if (argv[1] == std::string("plot"))
-  //   ufo::PlotSolver{argv[2]}();
+  else if (argv[1] == std::string("plot"))
+    ufo::plot(argv[2]);
   else
     throw std::runtime_error(fmt::format("Unknown task: {}", argv[1]));
 }
