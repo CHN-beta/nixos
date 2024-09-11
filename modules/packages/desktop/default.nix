1aee212aeff5df1935e2aa34bd2f4735d671d77f
chn <chn@chn.moe>
2024-09-12 07:30:09 +0800

    modules.packages.desktop: add ufo

--- modules/packages/desktop/default.nix
+++ modules/packages/desktop/default.nix
@@ -73,7 +73,7 @@ inputs:
           # math, physics and chemistry
           octaveFull root ovito localPackages.vesta localPackages.v-sim
           (mathematica.overrideAttrs (prev: { postInstall = prev.postInstall or "" + "ln -s ${src} $out/src"; }))
-          (quantum-espresso.override { stdenv = gcc14Stdenv; gfortran = gfortran14; }) jmol mpi
+          (quantum-espresso.override { stdenv = gcc14Stdenv; gfortran = gfortran14; }) jmol mpi localPackages.ufo
           # virtualization
           wineWowPackages.stagingFull virt-viewer bottles genymotion playonlinux
           # media
