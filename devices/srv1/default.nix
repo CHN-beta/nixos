a722dc17c4521cae639a6ec1fd1d613301a86eb9
chn <chn@chn.moe>
2024-09-15 11:50:38 +0800

    devices.srv1.node0: install

--- devices/srv1/default.nix
+++ devices/srv1/default.nix
@@ -6,7 +6,17 @@ inputs:
     {
       system =
       {
-        fileSystems = { swap = [ "/dev/mapper/swap" ]; rollingRootfs = {}; };
+        fileSystems =
+        {
+          mount = let inherit (inputs.config.nixos.system.cluster) clusterName nodeName; in
+          {
+            vfat."/dev/disk/by-partlabel/${clusterName}-${nodeName}-boot" = "/boot";
+            btrfs."/dev/disk/by-partlabel/${clusterName}-${nodeName}-root" =
+              { "/nix" = "/nix"; "/nix/rootfs/current" = "/"; };
+          };
+          swap = [ "/nix/swap/swap" ];
+          rollingRootfs = {};
+        };
         kernel.variant = "xanmod-lts";
         gui.enable = true;
       };
@@ -16,12 +26,6 @@ inputs:
         snapper.enable = true;
         sshd = {};
         smartd.enable = true;
-        slurm =
-        {
-          enable = true;
-          cpu = { cores = 16; threads = 2; mpiThreads = 2; openmpThreads = 4; };
-          memoryMB = 90112;
-        };
       };
       user.users = [ "chn" ];
     };
--- devices/srv1/node0/default.nix
+++ devices/srv1/node0/default.nix
@@ -4,26 +4,18 @@ inputs:
   {
     nixos =
     {
-      system =
-      {
-        fileSystems.mount =
-        {
-          vfat."/dev/disk/by-uuid/7A60-4232" = "/boot";
-          btrfs."/dev/mapper/root1" = { "/nix" = "/nix"; "/nix/rootfs/current" = "/"; };
-        };
-        nixpkgs.march = "cascadelake";
-      };
+      system.nixpkgs.march = "cascadelake";
       packages.vasp = null;
       services =
       {
         xray.client.enable = true;
-        beesd.instances.root = { device = "/"; hashTableSizeMB = 4096; threads = 4; };
+        beesd.instances.root = { device = "/"; hashTableSizeMB = 512; threads = 4; };
         wireguard =
         {
           enable = true;
           peers = [ "vps6" ];
-          publicKey = "l1gFSDCeBxyf/BipXNvoEvVvLqPgdil84nmr5q6+EEw=";
-          wireguardIp = "192.168.83.3";
+          publicKey = "Br+ou+t9M9kMrnNnhTvaZi2oNFRygzebA1NqcHWADWM=";
+          wireguardIp = "192.168.83.9";
         };
       };
     };
