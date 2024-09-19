f4344b8b319bc25dcc7ee39c421281db3af7507d
chn <chn@chn.moe>
2024-09-19 22:25:09 +0800

    Reapply "devices.srv1.node1: enable vasp"
    This reverts commit e9639a44c8a90a2553223cb5df421216fad04838.

--- devices/srv1/node1/default.nix
+++ devices/srv1/node1/default.nix
@@ -17,16 +17,12 @@ inputs:
         nix.remote.slave.enable = true;
       };
       services.beesd.instances.root = { device = "/"; hashTableSizeMB = 256; threads = 4; };
-      packages =
-      {
-        vasp = null;
-        packages._packages = [(inputs.pkgs.runCommand "master-system" {}
-        ''
-          mkdir -p $out/share
-          ln -s ${inputs.topInputs.self.nixosConfigurations.srv1-node0.config.system.build.toplevel} \
-            $out/share/master-system
-        '')];
-      };
+      packages.packages._packages = [(inputs.pkgs.runCommand "master-system" {}
+      ''
+        mkdir -p $out/share
+        ln -s ${inputs.topInputs.self.nixosConfigurations.srv1-node0.config.system.build.toplevel} \
+          $out/share/master-system
+      '')];
     };
     specialisation =
     {
