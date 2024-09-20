aad3164e112ff04446e39beefb77f95ab96ece1f
chn <chn@chn.moe>
2024-09-20 13:33:14 +0800

    devices.srv1.node2: init

---
+++ devices/srv1/node2/default.nix
@@ -0,0 +1,55 @@
+inputs:
+{
+  config =
+  {
+    nixos =
+    {
+      system =
+      {
+        nixpkgs.march = "broadwell";
+        networking.networkd.static.eno2 =
+          { ip = "192.168.178.3"; mask = 24; gateway = "192.168.178.1"; dns = "192.168.178.1"; };
+        cluster.nodeType = "worker";
+        initrd.sshd.enable = true;
+        nix.remote.slave.enable = true;
+      };
+      services.beesd.instances.root = { device = "/"; hashTableSizeMB = 256; threads = 4; };
+      packages.packages._packages = [(inputs.pkgs.runCommand "master-system" {}
+      ''
+        mkdir -p $out/share
+        ln -s ${inputs.topInputs.self.nixosConfigurations.srv1-node0.config.system.build.toplevel} \
+          $out/share/master-system
+      '')];
+    };
+    specialisation =
+    {
+      no-share-home.configuration =
+      {
+        nixos =
+        {
+          services.slurm.enable = inputs.lib.mkForce false;
+          system.cluster.nodeType = inputs.lib.mkForce "master";
+        };
+        system.nixos.tags = [ "no-share-home" ];
+      };
+    };
+    fileSystems = inputs.lib.mkIf (inputs.config.nixos.system.cluster.nodeType == "worker")
+    {
+      "/home" =
+      {
+        device = "192.168.178.1:/home";
+        fsType = "nfs";
+        neededForBoot = true;
+      };
+    };
+    boot.initrd.network.enable = true; 
+    boot.initrd.systemd.network.networks."10-eno2" = inputs.config.systemd.network.networks."10-eno2";
+    boot.initrd.systemd.extraBin =
+    {
+      "ifconfig" = "${inputs.pkgs.nettools}/bin/ifconfig";
+      "mount.nfs" = "${inputs.pkgs.nfs-utils}/bin/mount.nfs";
+      "mount.nfs4" = "${inputs.pkgs.nfs-utils}/bin/mount.nfs4";
+    };
+    services.rpcbind.enable = true;
+  };
+}
