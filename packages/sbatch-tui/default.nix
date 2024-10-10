fda4d2e86472dae624ff33a423d8f1b2a3f89fa4
chn <chn@chn.moe>
2024-10-10 16:17:18 +0800

    packages.sbatch-tui: fix

--- packages/sbatch-tui/default.nix
+++ packages/sbatch-tui/default.nix
@@ -1,13 +1,7 @@
-{
-  stdenv, lib, sbatchConfig ? null, substituteAll, runCommand,
-  cmake, pkg-config, ftxui, biu
-}:
-stdenv.mkDerivation
+{ stdenv, cmake, pkg-config, ftxui, biu }: stdenv.mkDerivation
 {
   name = "sbatch-tui";
   src = ./.;
-  preConfigure = lib.optionalString (sbatchConfig != null)
-    "cp ${substituteAll ({ src = ./src/device.cpp.template; } // sbatchConfig)} src/device.cpp";
   buildInputs = [ ftxui biu ];
   nativeBuildInputs = [ cmake pkg-config ];
 }
