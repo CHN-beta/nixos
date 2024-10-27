24e3993be6f434e34fbd0877e7588472abcc36f1
chn <chn@chn.moe>
2024-10-27 18:09:23 +0800

    modules.system.fontconfig: cleanup

--- modules/system/font.nix
+++ modules/system/font.nix
@@ -1,11 +1,6 @@
 inputs:
 {
-  options.nixos.services.fontconfig = let inherit (inputs.lib) mkOption types; in mkOption
-  {
-    type = types.nullOr (types.submodule {});
-    default = if builtins.elem inputs.config.nixos.model.type [ "desktop" "server" ] then {} else null;
-  };
-  config = let inherit (inputs.config.nixos.services) fontconfig; in inputs.lib.mkIf (fontconfig != null)
+  config = inputs.lib.mkIf (builtins.elem inputs.config.nixos.model.type [ "desktop" "server" ])
   {
     fonts =
     {
