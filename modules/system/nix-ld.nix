fa2f9c9bfd3d172ef12cee0cbd73701c28c6d602
chn <chn@chn.moe>
2024-11-06 08:46:22 +0800

    modules.syste.nix-ld: fix

--- modules/system/nix-ld.nix
+++ modules/system/nix-ld.nix
@@ -2,6 +2,11 @@ inputs:
 {
   config =
   {
-    programs.nix-ld = { enable = true; libraries = [ inputs.pkgs.steam-run.fhsenv ]; };
+    programs.nix-ld =
+    {
+      enable = true;
+      libraries = [(inputs.pkgs.runCommand "steamrun-lib" {}
+        "mkdir $out; ln -s ${inputs.pkgs.steam-run.fhsenv}/usr/lib64 $out/lib")];
+    };
   };
 }
