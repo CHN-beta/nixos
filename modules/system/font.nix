4b77c1436b6df538e5543567434d7622abe1e86a
chn <chn@chn.moe>
2024-11-08 00:00:44 +0800

    modules.system.font: fix

--- modules/system/font.nix
+++ modules/system/font.nix
@@ -8,7 +8,7 @@ inputs:
       packages = with inputs.pkgs;
       [
         noto-fonts source-han-sans source-han-serif source-code-pro hack-font jetbrains-mono nerdfonts hack-font inter
-        noto-fonts-color-emoji roboto sarasa-gothic source-han-mono wqy_microhei wqy_zenhei noto-fonts-cjk
+        noto-fonts-color-emoji roboto sarasa-gothic source-han-mono wqy_microhei wqy_zenhei noto-fonts-cjk-sans
         noto-fonts-emoji corefonts vistafonts vistafonts-chs
       ];
       fontconfig.defaultFonts =
