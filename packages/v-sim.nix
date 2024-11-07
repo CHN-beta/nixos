2c48026189f69f3a9aa5b0944fd6ada05e519d9f
chn <chn@chn.moe>
2024-11-07 23:59:07 +0800

    packages.v-sim: fix

--- packages/v-sim.nix
+++ packages/v-sim.nix
@@ -1,13 +1,13 @@
 {
   stdenv, lib, src,
   wrapGAppsHook, autoreconfHook, autoconf, libtool, intltool, gettext, automake, gtk-doc, pkg-config, gfortran, libxslt,
-  glib, gtk3, epoxy, libyaml
+  glib, gtk3, libepoxy, libyaml
 }:
 stdenv.mkDerivation
 {
   name = "v-sim";
   inherit src;
-  buildInputs = [ glib gtk3 epoxy libyaml ];
+  buildInputs = [ glib gtk3 libepoxy libyaml ];
   nativeBuildInputs =
   [
     autoreconfHook wrapGAppsHook autoconf libtool intltool gettext automake pkg-config
