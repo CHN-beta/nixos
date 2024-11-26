6bcc7875a8a9c5dabd60739a2babc90437724626
chn <chn@chn.moe>
2024-11-26 17:03:18 +0800

    modules.packages.server: add yq

--- modules/packages/server.nix
+++ modules/packages/server.nix
@@ -9,7 +9,7 @@ inputs:
       _packages = with inputs.pkgs;
       [
         # basic tools
-        beep dos2unix gnugrep pv tmux screen parallel tldr cowsay jq zellij ipfetch localPackages.pslist
+        beep dos2unix gnugrep pv tmux screen parallel tldr cowsay jq yq zellij ipfetch localPackages.pslist
         fastfetch reptyr duc ncdu progress libva-utils ksh neofetch
         # lsxx
         pciutils usbutils lshw util-linux lsof dmidecode lm_sensors hwloc acpica-tools
