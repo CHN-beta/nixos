88db84c3cf39b95bc5da6a4344c14a935322f0e0
chn <chn@chn.moe>
2024-12-09 16:05:59 +0800

    modules.system: disable hddtemp

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -17,12 +17,7 @@ inputs:
       supportedFilesystems = [ "ntfs" "nfs" "nfsv4" ];
       # consoleLogLevel = 7;
     };
-    hardware =
-    {
-      enableAllFirmware = true;
-      bluetooth.enable = true;
-      sensor = { hddtemp.enable = true; iio.enable = true; };
-    };
+    hardware = { enableAllFirmware = true; bluetooth.enable = true; sensor.iio.enable = true; };
     environment =
     {
       sessionVariables = rec
