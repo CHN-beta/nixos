d5bb5ecc2fb5ac25b6315612b1871c348e8f1986
chn <chn@chn.moe>
2024-12-12 11:45:41 +0800

    modules.packages: remove unused options

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -3,12 +3,6 @@ inputs:
   imports = inputs.localLib.findModules ./.;
   options.nixos.packages.packages = let inherit (inputs.lib) mkOption types; in
   {
-    extraPackages = mkOption { type = types.listOf types.unspecified; default = []; };
-    excludePackages = mkOption { type = types.listOf types.unspecified; default = []; };
-    extraPythonPackages = mkOption { type = types.listOf types.unspecified; default = []; };
-    excludePythonPackages = mkOption { type = types.listOf types.unspecified; default = []; };
-    extraPrebuildPackages = mkOption { type = types.listOf types.unspecified; default = []; };
-    excludePrebuildPackages = mkOption { type = types.listOf types.unspecified; default = []; };
     _packages = mkOption { type = types.listOf types.unspecified; default = []; };
     _pythonPackages = mkOption { type = types.listOf types.unspecified; default = []; };
     _prebuildPackages = mkOption { type = types.listOf types.unspecified; default = []; };
@@ -18,19 +12,14 @@ inputs:
   config =
   {
     environment.systemPackages = with inputs.config.nixos.packages.packages;
-      (inputs.lib.lists.subtractLists excludePackages (_packages ++ extraPackages))
+      _packages
       ++ [
         (
           (inputs.pkgs.python3.withPackages (pythonPackages:
-            inputs.lib.lists.subtractLists
-              (builtins.concatLists (builtins.map (packageFunction: packageFunction pythonPackages)
-                excludePythonPackages))
-              (builtins.concatLists (builtins.map (packageFunction: packageFunction pythonPackages)
-                (_pythonPackages ++ extraPythonPackages)))))
+            builtins.concatLists (builtins.map (packageFunction: packageFunction pythonPackages) _pythonPackages)))
           .override (prev: { makeWrapperArgs = prev.makeWrapperArgs or [] ++ _pythonEnvFlags; }))
         (inputs.pkgs.writeTextDir "share/prebuild-packages"
-          (builtins.concatStringsSep "\n" (builtins.map builtins.toString
-            (inputs.lib.lists.subtractLists excludePrebuildPackages (_prebuildPackages ++ extraPrebuildPackages)))))
+          (builtins.concatStringsSep "\n" (builtins.map builtins.toString _prebuildPackages)))
       ];
   };
 }
