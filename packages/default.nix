e5a31d911317d76ea74ece6bd934d8d06e325c5c
chn <chn@chn.moe>
2024-12-22 14:49:41 +0800

    packages.vasp.nvidia: fix

--- packages/default.nix
+++ packages/default.nix
@@ -63,15 +63,7 @@ inputs: rec
       };
       wannier90 = inputs.pkgs.wannier90.overrideAttrs { buildFlags = [ "dynlib" ]; };
     };
-    hdf5-nvhpc = (inputs.pkgs.hdf5.override
-    {
-      stdenv = nvhpcStdenv;
-      cppSupport = false;
-      fortranSupport = true;
-      enableShared = false;
-      enableStatic = true;
-    # }).overrideAttrs (prev: { nativeBuildInputs = prev.nativeBuildInputs or [] ++ [ inputs.pkgs.cudaPackages.cudatoolkit ]; });
-    }).overrideAttrs (prev: { nativeBuildInputs = prev.nativeBuildInputs or [] ++ []; });
+    hdf5-nvhpc = inputs.pkgs.callPackage ./vasp/hdf5-nvhpc { inherit lmod nvhpc; inherit (inputs.pkgs.hdf5) src; };
     vtst = (inputs.pkgs.callPackage ./vasp/vtst.nix {});
     vtstscripts = inputs.pkgs.callPackage ./vasp/vtstscripts.nix {};
   };
