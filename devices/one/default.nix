f78c1b8ce793cc6f6c011566f618d20985e99773
chn <chn@chn.moe>
2024-12-24 18:24:15 +0800

    devices.one: fix swap

--- devices/one/default.nix
+++ devices/one/default.nix
@@ -16,7 +16,7 @@ inputs:
           };
           luks.auto."/dev/disk/by-partlabel/one-root" = { mapper = "root"; ssd = true; };
           swap = [ "/nix/swap/swap" ];
-          resume = { device = "/dev/mapper/root"; offset = 728784; };
+          resume = { device = "/dev/mapper/root"; offset = 4728064; };
           rollingRootfs = {};
         };
         nixpkgs.march = "tigerlake";
