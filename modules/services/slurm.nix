a7be2d909bb4252bfa906195d2c1b4b520cb3877
chn <chn@chn.moe>
2025-01-12 14:42:17 +0800

    modules.services.slurm: un buffered io

--- modules/services/slurm.nix
+++ modules/services/slurm.nix
@@ -182,7 +182,7 @@ inputs:
       networking.firewall =
         let config = inputs.lib.mkIf slurm.setupFirewall [ 6818 ];
         in { allowedTCPPorts = config; allowedUDPPorts = config; };
-      environment.sessionVariables.SLURM_HINT = "nomultithread";
+      environment.sessionVariables = { SLURM_HINT = "nomultithread"; SLURM_UNBUFFEREDIO = "1"; };
     }
     # master 配置
     (inputs.lib.mkIf (slurm.master == inputs.config.nixos.model.hostname)
