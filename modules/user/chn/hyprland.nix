df1b014c4c039a5b235175cdba309569b3cad623
chn <chn@chn.moe>
2025-02-03 19:42:18 +0800

    modules.user.chn.hyprland: enable

---
+++ modules/user/chn/hyprland.nix
@@ -0,0 +1,25 @@
+inputs:
+{
+  config = inputs.lib.mkIf (inputs.config.nixos.packages.desktop != null)
+  {
+    home-manager.users.chn.config =
+    {
+      programs.hyprlock =
+      {
+        enable = true;
+        settings =
+        {
+          general = { disable_loading_bar = true; hide_cursor = true; };
+          background.path = "${inputs.topInputs.nixos-wallpaper}/twitter-1884592003595592025.jpg";
+        };
+      };
+      wayland.windowManager.hyprland =
+      {
+        enable = true;
+        # settings =
+        systemd.enable = false;
+        xwayland.enable = true;
+      };
+    };
+  };
+}
