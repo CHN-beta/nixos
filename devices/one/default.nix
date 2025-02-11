bd3e089791e4a7aba9b2b3e0fc975aa29b0a6847
chn <chn@chn.moe>
2025-02-11 18:25:03 +0800

    devices.one: use xanmod-latest kernel

--- devices/one/default.nix
+++ devices/one/default.nix
@@ -19,8 +19,9 @@ inputs:
           resume = { device = "/dev/mapper/root"; offset = 4728064; };
           rollingRootfs = {};
         };
-        nixpkgs.march = "tigerlake";
-        kernel.variant = "cachyos";
+        nixpkgs.march = "tigerlake";\
+        # somehow cachyos make touchscreen not work
+        kernel.variant = "xanmod-latest";
       };
       hardware = { cpus = [ "intel" ]; gpu.type = "intel"; };
       services =
