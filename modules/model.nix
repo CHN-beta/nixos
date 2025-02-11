0c95d54f437d459acac1c056e99908fd174c3ec6
chn <chn@chn.moe>
2025-02-11 18:53:18 +0800

    modules.model: type minimal -> vps

--- modules/model.nix
+++ modules/model.nix
@@ -3,7 +3,7 @@ inputs:
   options.nixos.model = let inherit (inputs.lib) mkOption types; in
   {
     hostname = mkOption { type = types.nonEmptyStr; };
-    type = mkOption { type = types.enum [ "minimal" "desktop" "server" ]; default = "minimal"; };
+    type = mkOption { type = types.enum [ "vps" "desktop" "server" ]; default = "vps"; };
     private = mkOption { type = types.bool; default = false; };
     cluster = mkOption
     {
