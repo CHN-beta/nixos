38f9c86cfd9757890d19560a215081d9ef034b54
chn <chn@chn.moe>
2025-02-25 18:55:02 +0800

    packages.hpcstat: fix export file name

--- packages/hpcstat/src/main.cpp
+++ packages/hpcstat/src/main.cpp
@@ -168,7 +168,7 @@ int main(int argc, const char** argv)
       auto begin = sys_seconds(sys_days(month(month_n) / 1 / year_n)).time_since_epoch().count();
       auto end = sys_seconds(sys_days(month(month_n) / 1 / year_n + months(1)))
         .time_since_epoch().count();
-      if (!sql::export_data(begin, end, "{}{}.xlsx"_f(year_n, month_n))) return 1;
+      if (!sql::export_data(begin, end, "{}{:0>2}.xlsx"_f(year_n, month_n))) return 1;
     }
     else if (args[1] == "push")
     {
