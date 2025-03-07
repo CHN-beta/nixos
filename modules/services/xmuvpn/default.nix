b1a3512ea81e2390db9ac56fded287c03bccdde3
chn <chn@chn.moe>
2025-03-07 11:06:06 +0800

    modules.services.xmuvpn: add ping service

--- modules/services/xmuvpn/default.nix
+++ modules/services/xmuvpn/default.nix
@@ -19,28 +19,42 @@ inputs:
       environment.PASSWORD = "xxxx";
     };
     nixos.services.docker = {};
-    systemd.services.xmuvpn-forwarder =
+    systemd.services =
     {
-      description = "xmuvpn forwarder daemon";
-      after = [ "network.target" "v2ray-forwarder.service" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig =
-        let ipset = "${inputs.pkgs.ipset}/bin/ipset";
-        in
+      xmuvpn-forwarder =
+      {
+        description = "xmuvpn forwarder daemon";
+        after = [ "network.target" "v2ray-forwarder.service" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig =
+          let ipset = "${inputs.pkgs.ipset}/bin/ipset";
+          in
+          {
+            Type = "simple";
+            RemainAfterExit = true;
+            ExecStart = inputs.pkgs.writeShellScript "xmuvpn-forwarder.start"
+              (builtins.concatStringsSep "\n" (builtins.map
+                (host: "${ipset} add xmu_net ${host}")
+                [
+                  # when add new ip, remember to also add it to router
+                  "218.193.58.125" "210.34.0.35" "121.192.191.10" "10.24.84.31" "59.77.0.143" "59.77.36.248"
+                  "172.27.124.24" "59.77.36.156" "59.77.36.223" "210.34.0.84" "218.193.50.157" "219.229.81.200"
+                  "210.34.16.60" "10.26.14.70" "10.26.14.56" "210.34.16.20" "59.77.36.250"
+                ]));
+            ExecStop = inputs.pkgs.writeShellScript "xmuvpn-forwarder.stop" "${ipset} flush xmu_net";
+          };
+      };
+      xmuvpn-ping =
+      {
+        description = "ping xmuvpn";
+        after = [ "xmuvpn-forwarder.service" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig =
         {
-          Type = "simple";
-          RemainAfterExit = true;
-          ExecStart = inputs.pkgs.writeShellScript "xmuvpn-forwarder.start"
-            (builtins.concatStringsSep "\n" (builtins.map
-              (host: "${ipset} add xmu_net ${host}")
-              [
-                # when add new ip, remember to also add it to router
-                "218.193.58.125" "210.34.0.35" "121.192.191.10" "10.24.84.31" "59.77.0.143" "59.77.36.248"
-                "172.27.124.24" "59.77.36.156" "59.77.36.223" "210.34.0.84" "218.193.50.157" "219.229.81.200"
-                "210.34.16.60" "10.26.14.70" "10.26.14.56" "210.34.16.20" "59.77.36.250"
-              ]));
-          ExecStop = inputs.pkgs.writeShellScript "xmuvpn-forwarder.stop" "${ipset} flush xmu_net";
+          ExecStart = "${inputs.pkgs.tcping-go}/bin/tcping office.chn.moe 22 -c 0 -I 1m -H";
+          Restart = "always";
         };
+      };
     };
   };
 }
