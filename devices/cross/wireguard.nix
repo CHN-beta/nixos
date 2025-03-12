9e65a7a8fb8a2623b60ade15c1803024c3e7d2b5
chn <chn@chn.moe>
2025-03-12 10:13:58 +0800

    modules.services.wireguard: allow multiple interfaces

--- devices/cross/wireguard.nix
+++ devices/cross/wireguard.nix
@@ -1,64 +1,66 @@
 inputs:
-let devices =
-{
-  vps6 =
+let
+  devices =
   {
-    peers = [ "pc" "nas" "one" "vps7" "srv2-node0" "srv1-node0" "vps8" ];
-    publicKey = "AVOsYUKQQCvo3ctst3vNi8XSVWo1Wh15066aHh+KpF4=";
-    wireguardIp = "192.168.83.1";
-    listenIp = "74.211.99.69";
-    lighthouse = true;
+    vps6 =
+    {
+      peers = [ "pc" "nas" "one" "vps7" "srv2-node0" "srv1-node0" "vps8" ];
+      publicKey = "AVOsYUKQQCvo3ctst3vNi8XSVWo1Wh15066aHh+KpF4=";
+      wireguardIp = 1;
+      listenIp = "74.211.99.69";
+      lighthouse = true;
+    };
+    vps7 =
+    {
+      peers = [ "vps6" ];
+      publicKey = "n056ppNxC9oECcW7wEbALnw8GeW7nrMImtexKWYVUBk=";
+      wireguardIp = 2;
+      listenIp = "144.126.144.62";
+    };
+    pc =
+    {
+      peers = [ "vps6" ];
+      behindNat = true;
+      publicKey = "l1gFSDCeBxyf/BipXNvoEvVvLqPgdil84nmr5q6+EEw=";
+      wireguardIp = 3;
+    };
+    nas =
+    {
+      peers = [ "vps6" ];
+      behindNat = true;
+      publicKey = "xCYRbZEaGloMk7Awr00UR3JcDJy4AzVp4QvGNoyEgFY=";
+      wireguardIp = 4;
+    };
+    one =
+    {
+      peers = [ "vps6" ];
+      behindNat = true;
+      publicKey = "Hey9V9lleafneEJwTLPaTV11wbzCQF34Cnhr0w2ihDQ=";
+      wireguardIp = 5;
+    };
+    srv2-node0 =
+    {
+      peers = [ "vps6" ];
+      behindNat = true;
+      publicKey = "lNTwQqaR0w/loeG3Fh5qzQevuAVXhKXgiPt6fZoBGFE=";
+      wireguardIp = 7;
+    };
+    srv1-node0 =
+    {
+      peers = [ "vps6" ];
+      behindNat = true;
+      publicKey = "Br+ou+t9M9kMrnNnhTvaZi2oNFRygzebA1NqcHWADWM=";
+      wireguardIp = 9;
+    };
+    vps8 =
+    {
+      peers = [ "vps8" ];
+      publicKey = "ifOlF2zBEygsqSX48ljT9CRKx/eiTFvI78HJtmLOpnU=";
+      wireguardIp = 6;
+      listenIp = "144.34.225.59";
+    };
   };
-  vps7 =
-  {
-    peers = [ "vps6" ];
-    publicKey = "n056ppNxC9oECcW7wEbALnw8GeW7nrMImtexKWYVUBk=";
-    wireguardIp = "192.168.83.2";
-    listenIp = "144.126.144.62";
-  };
-  pc =
-  {
-    peers = [ "vps6" ];
-    behindNat = true;
-    publicKey = "l1gFSDCeBxyf/BipXNvoEvVvLqPgdil84nmr5q6+EEw=";
-    wireguardIp = "192.168.83.3";
-  };
-  nas =
-  {
-    peers = [ "vps6" ];
-    behindNat = true;
-    publicKey = "xCYRbZEaGloMk7Awr00UR3JcDJy4AzVp4QvGNoyEgFY=";
-    wireguardIp = "192.168.83.4";
-  };
-  one =
-  {
-    peers = [ "vps6" ];
-    behindNat = true;
-    publicKey = "Hey9V9lleafneEJwTLPaTV11wbzCQF34Cnhr0w2ihDQ=";
-    wireguardIp = "192.168.83.5";
-  };
-  srv2-node0 =
-  {
-    peers = [ "vps6" ];
-    behindNat = true;
-    publicKey = "lNTwQqaR0w/loeG3Fh5qzQevuAVXhKXgiPt6fZoBGFE=";
-    wireguardIp = "192.168.83.7";
-  };
-  srv1-node0 =
-  {
-    peers = [ "vps6" ];
-    behindNat = true;
-    publicKey = "Br+ou+t9M9kMrnNnhTvaZi2oNFRygzebA1NqcHWADWM=";
-    wireguardIp = "192.168.83.9";
-  };
-  vps8 =
-  {
-    peers = [ "vps8" ];
-    publicKey = "ifOlF2zBEygsqSX48ljT9CRKx/eiTFvI78HJtmLOpnU=";
-    wireguardIp = "192.168.83.6";
-    listenIp = "144.34.225.59";
-  };
-};
+  port = 51820;
 in
 {
   config.nixos.services.wireguard = inputs.lib.mkIf (devices ? ${inputs.config.nixos.model.hostname})
@@ -68,10 +70,17 @@ in
       {
         inherit (cfg) publicKey wireguardIp;
         lighthouse = inputs.lib.mkIf (cfg ? lighthouse) cfg.lighthouse;
-        behindNat = inputs.lib.mkIf (cfg ? behindNat) cfg.behindNat;
         listenIp = inputs.lib.mkIf (cfg ? listenIp) cfg.listenIp;
       };
       this = devices.${inputs.config.nixos.model.hostname};
-    in (buildConfig this) // { peers = builtins.map (peer: buildConfig (devices.${peer})) this.peers; }
+    in
+    {
+      wireguard = (buildConfig this)
+        // {
+          listenPort = port;
+          net = 83;
+          peers = builtins.map (peer: buildConfig (devices.${peer})) this.peers;
+        };
+    }
   );
 }
--- modules/services/wireguard.nix
+++ modules/services/wireguard.nix
@@ -2,57 +2,66 @@ inputs:
 {
   options.nixos.services.wireguard = let inherit (inputs.lib) mkOption types; in mkOption
   {
-    type = types.nullOr (types.submodule (submoduleInputs: { options =
+    type = types.nullOr (types.attrsOf (types.submodule (submoduleInputs: { options =
       let generalOption =
       {
         publicKey = mkOption { type = types.nonEmptyStr; };
         lighthouse = mkOption { type = types.bool; default = false; };
-        behindNat = mkOption { type = types.bool; default = false; };
+        # if behind nat, set to null; if not, set to the public ip
         listenIp = mkOption { type = types.nullOr types.nonEmptyStr; default = null; };
-        wireguardIp = mkOption { type = types.nonEmptyStr; };
+        # if specify "3", then the subnet should be 192.168.83.3/24
+        wireguardIp = mkOption { type = types.int; };
       };
       in generalOption
-        // { peers = mkOption { type = types.nonEmptyListOf (types.submodule { options = generalOption; }); }; };
-    }));
+        // {
+          # if listenIp is set, actually listen on this port; otherwise, listen on this port + behindNatPortOffset
+          listenPort = mkOption { type = types.int; };
+          # if specify "83", then the subnet should be 192.168.83.0/24
+          net = mkOption { type = types.int; };
+          behindNatPortOffset = mkOption { type = types.int; default = 1; readOnly = true; };
+          peers = mkOption { type = types.nonEmptyListOf (types.submodule { options = generalOption; }); };
+        };
+    })));
     default = null;
   };
   config = let inherit (inputs.config.nixos.services) wireguard; in inputs.lib.mkIf (wireguard != null)
   {
-    assertions =
-    [
+    assertions = builtins.map
+      (wg:
       {
-        assertion = !wireguard.behindNat -> wireguard.listenIp != null;
-        message = "wireguard.listenIp should not be null when behindNat is false.";
-      }
+        assertion = inputs.config.nixos.services.xray.client.enable -> (wg.value.listenIp == null);
+        message = "Wireguard should behind NAT when xray client is enabled.";
+      })
+      (inputs.localLib.attrsToList wireguard);
+    networking = inputs.lib.mkMerge (builtins.map
+      (wg:
       {
-        assertion = inputs.config.nixos.services.xray.client.enable -> wireguard.behindNat;
-        message = "Wireguard is behind NAT when xray client is enabled.";
-      }
-    ];
-    networking =
-    {
-      firewall =
-      {
-        allowedUDPPorts = inputs.lib.mkIf (!wireguard.behindNat) [ 51820 ];
-        trustedInterfaces = [ "wireguard" ];
-      };
-      wireguard.interfaces.wireguard =
-      {
-        ips = [ "${wireguard.wireguardIp}/24" ];
-        # if the host is behind xray, it should listen on another port, to make xray succeffully listen on 51820
-        listenPort = inputs.localLib.mkConditional wireguard.behindNat 51821 51820;
-        privateKeyFile = inputs.config.sops.secrets."wireguard/privateKey".path;
-        peers = builtins.map
-          (peer:
-          {
-            inherit (peer) publicKey;
-            allowedIPs = [ (if peer.lighthouse then "192.168.83.0/24" else "${peer.wireguardIp}/32") ];
-            endpoint = inputs.lib.mkIf (!peer.behindNat) "${peer.listenIp}:51820";
-            persistentKeepalive = inputs.lib.mkIf peer.lighthouse 5;
-          })
-          wireguard.peers;
-      };
-    };
-    sops.secrets."wireguard/privateKey" = {};
+        firewall =
+        {
+          allowedUDPPorts = inputs.lib.mkIf (wg.value.listenIp != null) [ wg.value.listenPort ];
+          trustedInterfaces = [ wg.name ];
+        };
+        wireguard.interfaces.${wg.name} =
+        {
+          ips = [ "192.168.${builtins.toString wg.value.net}.${builtins.toString wg.value.wireguardIp}/24" ];
+          # if the host is behind xray, it should listen on another port, to make xray succeffully listen on 51820
+          listenPort = with wg.value; if listenIp == null then listenPort + behindNatPortOffset else listenPort;
+          privateKeyFile = inputs.config.sops.secrets."wireguard/${wg.name}".path;
+          peers = builtins.map
+            (peer:
+            {
+              inherit (peer) publicKey;
+              allowedIPs = let suffix = if peer.lighthouse then "0/24" else "${builtins.toString peer.wireguardIp}/32";
+                in [ "192.168.${builtins.toString wg.value.net}.${suffix}" ];
+              endpoint = inputs.lib.mkIf (peer.listenIp != null)
+                "${peer.listenIp}:${builtins.toString wg.value.listenPort}";
+              persistentKeepalive = inputs.lib.mkIf peer.lighthouse 1;
+            })
+            wg.value.peers;
+        };
+      })
+      (inputs.localLib.attrsToList wireguard));
+    sops.secrets = inputs.lib.mkMerge (builtins.map
+      (wg: { "wireguard/${wg.name}" = {}; }) (inputs.localLib.attrsToList wireguard));    
   };
 }
