7f09b9a868f96bcccb61d6077a9b654be9afe5bd
chn <chn@chn.moe>
2025-03-23 17:10:10 +0800

    modules.system.nixpkgs: fix

--- modules/system/nixpkgs/buildNixpkgsConfig.nix
+++ modules/system/nixpkgs/buildNixpkgsConfig.nix
@@ -68,7 +68,7 @@ in platformConfig //
                     .overrideAttrs (prev:
                       { cmakeFlags = prev.cmakeFlags or [] ++ [ "-DENABLE_CPU_DISPATCH=OFF" ]; });
                 }
-                // inputs.lib.optionalAttrs (builtins.elem nixpkgs.march [ "skylake" "silvermont" ])
+                // inputs.lib.optionalAttrs (builtins.elem nixpkgs.march [ "skylake" "silvermont" "broadwell" ])
                   { redis = prev.redis.overrideAttrs { doCheck = false; }; };
             };
           };
