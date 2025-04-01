6752a265a6efcf01f70cd5bdf0e76e8b040e1032
chn <chn@chn.moe>
2025-04-01 23:08:38 +0800

    modules.packages.desktop: add ocrmypdf

--- modules/packages/desktop/default.nix
+++ modules/packages/desktop/default.nix
@@ -65,7 +65,7 @@ inputs:
           google-chrome tor-browser microsoft-edge
           # office
           crow-translate zotero pandoc texliveFull poppler_utils pdftk pdfchain davinci-resolve
-          ydict texstudio panoply pspp paperwork libreoffice-qt6-fresh
+          ydict texstudio panoply pspp paperwork libreoffice-qt6-fresh ocrmypdf
           # matplot++ needs old gnuplot
           inputs.pkgs."pkgs-23.11".gnuplot
           # math, physics and chemistry
