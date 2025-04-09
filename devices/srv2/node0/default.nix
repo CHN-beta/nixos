27987f67ea0f428763b29f1251567945e2604c6f
chn <chn@chn.moe>
2025-04-09 13:00:41 +0800

    devices.srv2.node0: set hpc dns

--- devices/srv2/node0/default.nix
+++ devices/srv2/node0/default.nix
@@ -17,7 +17,8 @@ inputs:
       };
       services =
       {
-        xray.client = { enable = true; dnsmasq.extraInterfaces = [ "eno2" ]; };
+        xray.client =
+          { enable = true; dnsmasq = { extraInterfaces = [ "eno2" ]; hosts."hpc.xmu.edu.cn" = "121.192.191.11"; }; };
         beesd.instances.root = { device = "/"; hashTableSizeMB = 16 * 128; loadAverage = 8; };
         xrdp = { enable = true; hostname = [ "srv2.chn.moe" ]; };
         samba = { hostsAllowed = ""; shares = { home.path = "/home"; root.path = "/"; }; };
