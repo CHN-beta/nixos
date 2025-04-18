d806c19d4eb89c2ba2633150c21f85a96177e5a3
chn <chn@chn.moe>
2025-04-18 17:47:03 +0800

    devices.srv3: setup luks

--- devices/cross/luks-manual/default.nix
+++ devices/cross/luks-manual/default.nix
@@ -9,6 +9,7 @@ let devices =
   };
   vps6."/dev/disk/by-uuid/961d75f0-b4ad-4591-a225-37b385131060" = { mapper = "root"; ssd = true; };
   vps7."/dev/disk/by-uuid/db48c8de-bcf7-43ae-a977-60c4f390d5c4" = { mapper = "root"; ssd = true; };
+  srv3."/dev/disk/by-partlabel/srv3-root1" = { mapper = "root1"; ssd = true; };
 };
 in
 {
--- devices/srv3/default.nix
+++ devices/srv3/default.nix
@@ -14,12 +14,6 @@ inputs:
             vfat."/dev/disk/by-partlabel/srv3-boot" = "/boot";
             btrfs."/dev/mapper/root1" = { "/nix" = "/nix"; "/nix/rootfs/current" = "/"; };
           };
-          luks.auto =
-          {
-            "/dev/disk/by-partlabel/srv3-root1" = { mapper = "root1"; ssd = true; };
-            "/dev/disk/by-partlabel/srv3-root2" = { mapper = "root2"; ssd = true; };
-            "/dev/disk/by-partlabel/srv3-swap" = { mapper = "swap"; ssd = true; };
-          };
           swap = [ "/dev/mapper/swap" ];
           rollingRootfs = {};
         };
