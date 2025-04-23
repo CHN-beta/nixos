50ce96742361700543558fceea87979f1e57c0be
chn <chn@chn.moe>
2025-04-23 15:15:14 +0800

    modules.services.nix-serve: 整理

--- modules/services/nix-serve.nix
+++ modules/services/nix-serve.nix
@@ -1,26 +1,23 @@
 inputs:
 {
-  options.nixos.services.nix-serve = let inherit (inputs.lib) mkOption types; in
+  options.nixos.services.nix-serve = let inherit (inputs.lib) mkOption types; in mkOption
   {
-    enable = mkOption { type = types.bool; default = false; };
-    hostname = mkOption { type = types.nonEmptyStr; };
-  };
-  config =
-    let
-      inherit (inputs.lib) mkMerge mkIf;
-      inherit (inputs.localLib) stripeTabs attrsToList;
-      inherit (inputs.config.nixos.services) nix-serve;
-      inherit (builtins) map listToAttrs toString;
-    in mkIf nix-serve.enable
+    type = types.nullOr (types.submodule { options =
     {
-      services.nix-serve =
-      {
-        enable = true;
-        openFirewall = true;
-        secretKeyFile = inputs.config.sops.secrets."store/signingKey".path;
-      };
-      sops.secrets."store/signingKey" = {};
-      nixos.services.nginx =
-        { enable = true; https.${nix-serve.hostname}.location."/".proxy.upstream = "http://127.0.0.1:5000"; };
+      hostname = mkOption { type = types.nonEmptyStr; default = "nix-store.chn.moe"; };
+    };});
+    default = null;
+  };
+  config = let inherit (inputs.config.nixos.services) nix-serve; in inputs.lib.mkIf (nix-serve != null)
+  {
+    services.nix-serve =
+    {
+      enable = true;
+      openFirewall = true;
+      secretKeyFile = inputs.config.sops.secrets."store/signingKey".path;
     };
+    sops.secrets."store/signingKey" = {};
+    nixos.services.nginx =
+      { enable = true; https.${nix-serve.hostname}.location."/".proxy.upstream = "http://127.0.0.1:5000"; };
+  };
 }
