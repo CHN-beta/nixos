700b0586200026fb6b9852118011ddbb0b957636
chn <chn@chn.moe>
2025-04-29 10:10:58 +0800

    modules.system.nix: add want-mass-query=true

--- devices/nas/default.nix
+++ devices/nas/default.nix
@@ -19,7 +19,7 @@ inputs:
         };
         initrd.sshd = {};
         nixpkgs.march = "silvermont";
-        nix.substituters = [ "https://nix-store.pc.chn.moe" ];
+        nix.substituters = [ "https://nix-store.pc.chn.moe?want-mass-query=true" ];
         networking = {};
       };
       hardware = { cpus = [ "intel" ]; gpu.type = "intel"; };
