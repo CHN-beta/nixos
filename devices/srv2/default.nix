ff9b27bf2cb2952a0b495e75bb717139925a5f7f
chn <chn@chn.moe>
2025-04-30 19:45:43 +0800

    Revert "devices: remove nfs over wg1"
    This reverts commit e88b21fc00ea16b10be4209f94d8c511a4fcf5cb.

--- devices/srv2/default.nix
+++ devices/srv2/default.nix
@@ -14,6 +14,8 @@ inputs:
             vfat."/dev/disk/by-partlabel/${clusterName}-${nodeName}-boot" = "/boot";
             btrfs."/dev/disk/by-partlabel/${clusterName}-${nodeName}-root1" =
               { "/nix" = "/nix"; "/nix/rootfs/current" = "/"; };
+            nfs."${inputs.topInputs.self.config.dns."chn.moe".getAddress "wg1.pc"}:/" =
+              { mountPoint = "/nix/pc"; hard = false; };
           };
           swap = [ "/nix/swap/swap" ];
           rollingRootfs = {};
