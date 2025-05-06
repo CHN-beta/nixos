7175664f771452b1c0fdc42d35c962b63602516d
chn <chn@chn.moe>
2025-05-06 21:20:41 +0800

    devices.srv3: add pen

--- devices/srv3/default.nix
+++ devices/srv3/default.nix
@@ -36,9 +36,10 @@ inputs:
         nixvirt =
         {
           alikia = { memoryGB = 1; cpus = 1; address = 2; portForward.tcp = [{ host = 5689; guest = 22; }]; };
+          pen = { memoryGB = 1; cpus = 1; address = 3; portForward.tcp = [{ host = 5690; guest = 22; }]; };
         };
       };
-      user.users = [ "chn" "aleksana" "alikia" ];
+      user.users = [ "chn" "aleksana" "alikia" "pen" ];
     };
     # TODO: use a generic way
     boot.initrd.systemd.network.networks."10-eno1" = inputs.config.systemd.network.networks."10-eno1";
