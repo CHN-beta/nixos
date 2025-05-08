78150703bee5017e42ce5910e9746d67d3a428aa
chn <chn@chn.moe>
2025-05-08 11:48:48 +0800

    modules.services.snapper: lower snap

--- modules/services/snapper.nix
+++ modules/services/snapper.nix
@@ -18,9 +18,9 @@ inputs:
           TIMELINE_CLEANUP = true;
           TIMELINE_MIN_AGE = 1800;
           TIMELINE_LIMIT_HOURLY = 10;
-          TIMELINE_LIMIT_DAILY = 7;
-          TIMELINE_LIMIT_WEEKLY = 3;
-          TIMELINE_LIMIT_MONTHLY = 1;
+          TIMELINE_LIMIT_DAILY = 3;
+          TIMELINE_LIMIT_WEEKLY = 1;
+          TIMELINE_LIMIT_MONTHLY = 0;
           TIMELINE_LIMIT_YEARLY = 0;
         };
       })
