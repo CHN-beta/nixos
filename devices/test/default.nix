5fd6dbf79255b961517d8f2bc6d871b7ed2b2a81
chn <chn@chn.moe>
2025-05-10 11:54:51 +0800

    devices.test: init

---
+++ devices/test/default.nix
@@ -0,0 +1,28 @@
+inputs:
+{
+  config =
+  {
+    nixos =
+    {
+      system =
+      {
+        fileSystems =
+        {
+          mount =
+          {
+            vfat."/dev/disk/by-partlabel/test-boot" = "/boot";
+            btrfs."/dev/disk/by-partlabel/test-root1" = { "/nix" = "/nix"; "/nix/rootfs/current" = "/"; };
+          };
+          rollingRootfs = {};
+        };
+        nixpkgs.march = "haswell";
+        networking = {};
+      };
+      hardware.cpus = [ "intel" ];
+      services =
+      {
+        sshd = {};
+      };
+    };
+  };
+}
