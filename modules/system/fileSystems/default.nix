e0104154ea66d5bf508159e721ab577fc40c4546
chn <chn@chn.moe>
2025-05-16 12:03:27 +0800

    modules.system.fileSystems: fix

--- modules/system/fileSystems/default.nix
+++ modules/system/fileSystems/default.nix
@@ -123,7 +123,7 @@ inputs:
                 btrfs property set -ts /mnt/nix/rootfs/$timestamp-$subvolid ro true
               fi
               [ -d /mnt/nix/rootfs/current ] || btrfs subvolume create /mnt/nix/rootfs/current
-              mkdir /mnt/nix/rootfs/current/usr
+              mkdir -p /mnt/nix/rootfs/current/usr
               touch /mnt/nix/rootfs/current/usr/make-systemd-happy
               chattr +C /mnt/nix/rootfs/current
               echo $(date '+%Y%m%d%H%M%S') > /mnt/nix/rootfs/current/.timestamp
