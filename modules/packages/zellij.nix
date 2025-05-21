61e63931907046fdc3dc509e74d7c44ea781cd48
chn <chn@chn.moe>
2025-05-21 17:53:09 +0800

    modules.packages.zellij: init

---
+++ modules/packages/zellij.nix
@@ -0,0 +1,13 @@
+inputs:
+{
+  options.nixos.packages.zellij = let inherit (inputs.lib) mkOption types; in mkOption
+    { type = types.nullOr (types.submodule {}); default = {}; };
+  config = let inherit (inputs.config.nixos.packages) zellij; in inputs.lib.mkIf (zellij != null)
+  {
+    nixos =
+    {
+      packages.packages._packages = [ inputs.pkgs.zellij ];
+      user.sharedModules = [{ config.programs.zellij = { enable = true; settings.show_startup_tips = false; }; }];
+    };
+  };
+}
