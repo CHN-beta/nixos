919eb1247c41c4d6978460b5a7b721d3ba3b9e49
chn <chn@chn.moe>
2025-05-24 18:41:54 +0800

    modules.system.nix-ld: only enable for desktop/server

--- modules/system/nix-ld.nix
+++ modules/system/nix-ld.nix
@@ -1,6 +1,11 @@
 inputs:
 {
-  config =
+  options.nixos.system.nix-ld = let inherit (inputs.lib) mkOption types; in mkOption
+  {
+    type = types.nullOr (types.submodule {});
+    default = if builtins.elem inputs.config.nixos.model.type [ "desktop" "server" ] then {} else null;
+  };
+  config = let inherit (inputs.config.nixos.system) nix-ld; in inputs.lib.mkIf (nix-ld != null)
   {
     programs.nix-ld =
     {
