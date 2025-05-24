524285fa6791cd3a65dbef53a911edffe4bd1a67
chn <chn@chn.moe>
2025-05-24 18:34:21 +0800

    modules.system.sops: remove unused gnupg keys

--- modules/system/sops.nix
+++ modules/system/sops.nix
@@ -35,7 +35,6 @@ inputs:
         ];
       # sops start before impermanence, so we need to use the absolute path
       age.sshKeyPaths = [ "/nix/persistent/etc/ssh/ssh_host_ed25519_key" ];
-      gnupg.sshKeyPaths = [ "/nix/persistent/etc/ssh/ssh_host_rsa_key" ];
     };
   };
 }
