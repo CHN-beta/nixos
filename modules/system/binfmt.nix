ee86e9c24498ef75586527d4c897fb73fe6494ea
chn <chn@chn.moe>
2025-05-24 18:58:15 +0800

    modules.system.binfmt: disable as default

--- modules/system/binfmt.nix
+++ modules/system/binfmt.nix
@@ -1,6 +1,8 @@
 inputs:
 {
-  config =
+  options.nixos.system.binfmt = let inherit (inputs.lib) mkOption types; in mkOption
+    { type = types.nullOr (types.submodule {}); default = null; };
+  config = let inherit (inputs.config.nixos.system) binfmt; in inputs.lib.mkIf (binfmt != null)
   {
     programs.java = { enable = true; binfmt = true; };
     boot.binfmt.emulatedSystems = [ "aarch64-linux" "x86_64-windows" ];
