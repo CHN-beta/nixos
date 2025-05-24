0d45b6ffcadc9a0b77d21ea9fa9fe270a6a17bfb
chn <chn@chn.moe>
2025-05-24 18:56:39 +0800

    modules.system.envfs: only enable envfs on desktop and server

--- modules/system/envfs.nix
+++ modules/system/envfs.nix
@@ -1,6 +1,11 @@
 inputs:
 {
-  config =
+  options.nixos.system.envfs = let inherit (inputs.lib) mkOption types; in mkOption
+  {
+    type = types.nullOr (types.submodule {});
+    default = if builtins.elem inputs.config.nixos.model.type [ "desktop" "server" ] then {} else null;
+  };
+  config = let inherit (inputs.config.nixos.system) envfs; in inputs.lib.mkIf (envfs != null)
   {
     services.envfs.enable = true;
     environment.variables.ENVFS_RESOLVE_ALWAYS = "1";
