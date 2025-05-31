1a196c3eec7d0b2f0fdb1445ea71c894c90749e2
chn <chn@chn.moe>
2025-05-31 11:43:05 +0800

    format

--- modules/packages/mathematica.nix
+++ modules/packages/mathematica.nix
@@ -1,10 +1,7 @@
 inputs:
 {
   options.nixos.packages.mathematica = let inherit (inputs.lib) mkOption types; in mkOption
-  {
-    type = types.nullOr (types.submodule {});
-    default = null;
-  };
+    { type = types.nullOr (types.submodule {}); default = null; };
   config = let inherit (inputs.config.nixos.packages) mathematica; in inputs.lib.mkIf (mathematica != null)
     { nixos.packages.packages._packages = [ inputs.pkgs.mathematica ]; };
 }
