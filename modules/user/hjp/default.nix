1768853fba1f4c878f4d16142ca80d400a5d8ae6
chn <chn@chn.moe>
2025-06-01 14:30:38 +0800

    modules.user.hjp: fix

--- modules/user/hjp/default.nix
+++ modules/user/hjp/default.nix
@@ -2,7 +2,7 @@ inputs:
 {
   config = let inherit (inputs.config.nixos) user; in inputs.lib.mkIf (builtins.elem "hjp" user.users)
   {
-    home-manager.users.hjp.config.programs.zsh.initExtra =
+    home-manager.users.hjp.config.programs.zsh.initContent =
     ''
       export PATH=$PATH:/home/hjp/software/intel/oneapi/compiler/latest/bin
       export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/hjp/software/intel/oneapi/compiler/latest/lib
