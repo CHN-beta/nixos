d1fc2b0a1c94d2e4fd991acadb77b598142fa415
chn <chn@chn.moe>
2025-06-24 18:43:05 +0800

    devices.jykang: cleanup

--- devices/jykang.xmuhpc/default.nix
+++ devices/jykang.xmuhpc/default.nix
@@ -1,15 +1,12 @@
-# sudo nix build --store 'local?store=/data/gpfs01/jykang/.nix/store&real=/nix/store' .#jykang
-# sudo nix-store --store 'local?store=/data/gpfs01/jykang/.nix/store&real=/nix/store' -qR ./result | sudo xargs nix-store --store 'local?store=/data/gpfs01/jykang/.nix/store&real=/nix/store' --export > data.nar
-# cat data.nar  | nix-store --import
 { inputs, localLib }:
 let pkgs = import inputs.nixpkgs (localLib.buildNixpkgsConfig
 {
   inputs = { inherit (inputs.nixpkgs) lib; topInputs = inputs; };
-  nixpkgs = { march = null; cuda = null; nixRoot = "/data/gpfs01/jykang/.nix"; };
+  nixpkgs = { march = null; cuda = null; nixRoot = null; };
 });
 in (pkgs.symlinkJoin
 {
   name = "jykang";
   paths = with pkgs; [ hello iotop gnuplot localPackages.vaspkit ];
   postBuild = "echo ${inputs.self.rev or "dirty"} > $out/.version";
-}) // { passthru = { inherit pkgs; }; }
+}) // { inherit pkgs; }
