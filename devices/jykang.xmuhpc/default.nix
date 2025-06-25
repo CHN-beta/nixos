42080c0b9a5f329fe4cadbd9e7af97d36f225083
chn <chn@chn.moe>
2025-06-25 12:53:56 +0800

    devices.jykang: fix passthru

--- devices/jykang.xmuhpc/default.nix
+++ devices/jykang.xmuhpc/default.nix
@@ -7,9 +7,10 @@ let pkgs = import inputs.nixpkgs (localLib.buildNixpkgsConfig
   inputs = { inherit (inputs.nixpkgs) lib; topInputs = inputs; };
   nixpkgs = { march = null; cuda = null; nixRoot = "/data/gpfs01/jykang/.nix"; };
 });
-in (pkgs.symlinkJoin
+in pkgs.symlinkJoin
 {
   name = "jykang";
   paths = with pkgs; [ hello iotop gnuplot localPackages.vaspkit ];
   postBuild = "echo ${inputs.self.rev or "dirty"} > $out/.version";
-}) // { passthru = { inherit pkgs; }; }
+  passthru = { inherit pkgs; };
+}
