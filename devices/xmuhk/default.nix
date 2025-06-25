2b43d8498104edd684e45e493de30fcc36993558
chn <chn@chn.moe>
2025-06-25 10:44:48 +0800

    devices.xmuhk: use host singularity

--- devices/xmuhk/default.nix
+++ devices/xmuhk/default.nix
@@ -5,18 +5,13 @@ let
     inputs = { inherit (inputs.nixpkgs) lib; topInputs = inputs; };
     nixpkgs = { march = null; cuda = null; nixRoot = null; };
   });
-  singularity = pkgs.singularity.overrideAttrs (prev:
-  {
-    configureFlags = builtins.filter (x: x != "--without-libsubid") prev.configureFlags;
-    buildInputs = prev.buildInputs ++ [ pkgs.shadow ];
-  });
   lumericalLicenseManager = 
     let
       ip = "${pkgs.iproute2}/bin/ip";
       awk = "${pkgs.gawk}/bin/awk";
       sed = "${pkgs.gnused}/bin/sed";
       chmod = "${pkgs.coreutils}/bin/chmod";
-      sing = "${singularity}/bin/singularity";
+      sing = "/public/software/singularity/singularity-3.8.3/bin/singularity";
     in pkgs.writeShellScriptBin "lumericalLicenseManager"
     ''
       echo "Cleaning up..."
