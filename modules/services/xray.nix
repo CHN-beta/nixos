895e371ac9c0eb99670911830d4a14e3257e7cf7
chn <chn@chn.moe>
2025-06-26 10:00:33 +0800

    Revert "modules.services.xray: revert version"
    This reverts commit 13d571477b76603e47e1b9c646d6bec2b7087931.

--- modules/services/xray.nix
+++ modules/services/xray.nix
@@ -48,13 +48,7 @@ inputs:
       {
         services =
         {
-          xray =
-          {
-            enable = true;
-            # there is a bug in xray 25.05
-            package = inputs.pkgs.pkgs-2411.xray;
-            settingsFile = inputs.config.sops.templates."xray-client.json".path;
-          };
+          xray = { enable = true; settingsFile = inputs.config.sops.templates."xray-client.json".path; };
           dnsmasq =
           {
             enable = true;
