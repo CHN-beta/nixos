55a5085c237968df41fb028eca211ed2aaef24d3
chn <chn@chn.moe>
2025-06-30 20:42:43 +0800

    flake.dns: add xserverxmu

--- flake/dns/config/chn.moe.nix
+++ flake/dns/config/chn.moe.nix
@@ -4,7 +4,7 @@ let
   {
     autoroute = [ "api" "git" "grafana" "matrix" "peertube" "send" "synapse" "vikunja" "铜锣湾" ];
     nas = [ "initrd.nas" ];
-    office = [ "srv2-node0" ];
+    office = [ "srv2-node0" "xserverxmu" ];
     vps4 = [ "initrd.vps4" "xserver2.vps4" ];
     vps6 =
     [
