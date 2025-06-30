1147ec64b73cafc34ed5d6370282e2d5fe8a2337
chn <chn@chn.moe>
2025-06-30 09:18:53 +0800

    Revert "modules.packages.android-studio: remove"
    This reverts commit 9ef5d5f35d96ff4936e69cd7648e400c5c39a997.

---
+++ modules/packages/android-studio.nix
@@ -0,0 +1,9 @@
+inputs:
+{
+  options.nixos.packages.android-studio = let inherit (inputs.lib) mkOption types; in mkOption
+    { type = types.nullOr (types.submodule {}); default = null; };
+  config = let inherit (inputs.config.nixos.packages) android-studio; in inputs.lib.mkIf (android-studio != null)
+  {
+    nixos.packages.packages._packages = with inputs.pkgs; [ androidStudioPackages.stable.full ];
+  };
+}
