c65f295518315b882b34e0971b141ed7c0c8307f
chn <chn@chn.moe>
2025-07-04 09:49:24 +0800

    modules.user.aleksana: limit root access

--- modules/user/aleksana/default.nix
+++ modules/user/aleksana/default.nix
@@ -1,8 +1,5 @@
 inputs:
 {
   config = let inherit (inputs.config.nixos) user; in inputs.lib.mkIf (builtins.elem "aleksana" user.users)
-  {
-    users.users.aleksana.extraGroups = inputs.lib.intersectLists
-      [ "wheel" "wireshark" "libvirtd" ] (builtins.attrNames inputs.config.users.groups);
-  };
+    { users.users.aleksana.extraGroups = inputs.lib.mkIf (inputs.config.nixos.model.hostname == "srv3") [ "wheel" ]; };
 }
