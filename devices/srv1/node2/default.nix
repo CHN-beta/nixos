ca15905e1a4dbb96f1837df5be4ba9d4391f5d68
chn <chn@chn.moe>
2025-07-05 09:47:37 +0800

    devices.srv1.node2: disable kvm xray

--- devices/srv1/node2/default.nix
+++ devices/srv1/node2/default.nix
@@ -9,23 +9,13 @@ inputs:
         nixpkgs.march = "broadwell";
         network =
         {
-          static =
-          {
-            br0 = { ip = "192.168.1.12"; mask = 24; gateway = "192.168.1.1"; dns = "192.168.1.1"; };
-            eno2 = { ip = "192.168.178.3"; mask = 24; };
-          };
+          static.eno2 = { ip = "192.168.178.3"; mask = 24; gateway = "192.168.178.1"; dns = "192.168.178.1"; };
           trust = [ "eno2" ];
-          bridge.br0.interfaces = [ "eno1" ];
         };
         fileSystems.mount.btrfs."/dev/disk/by-partlabel/srv1-node2-nodatacow" =
           { "/nix/nodatacow" = "/nix/nodatacow"; "/nix/backups" = "/nix/backups"; };
       };
-      services =
-      {
-        xray.client = {};
-        beesd."/".threads = 4;
-        kvm.nodatacow = true;
-      };
+      services.beesd."/".threads = 4;
     };
   };
 }
