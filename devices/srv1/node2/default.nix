d9d7bef796f2d39315d3f6a214e7badf69c6868a
chn <chn@chn.moe>
2025-07-05 14:33:01 +0800

    Revert "devices.srv1.node2: disable kvm xray"
    This reverts commit ca15905e1a4dbb96f1837df5be4ba9d4391f5d68.

--- devices/srv1/node2/default.nix
+++ devices/srv1/node2/default.nix
@@ -9,13 +9,23 @@ inputs:
         nixpkgs.march = "broadwell";
         network =
         {
-          static.eno2 = { ip = "192.168.178.3"; mask = 24; gateway = "192.168.178.1"; dns = "192.168.178.1"; };
+          static =
+          {
+            br0 = { ip = "192.168.1.12"; mask = 24; gateway = "192.168.1.1"; dns = "192.168.1.1"; };
+            eno2 = { ip = "192.168.178.3"; mask = 24; };
+          };
           trust = [ "eno2" ];
+          bridge.br0.interfaces = [ "eno1" ];
         };
         fileSystems.mount.btrfs."/dev/disk/by-partlabel/srv1-node2-nodatacow" =
           { "/nix/nodatacow" = "/nix/nodatacow"; "/nix/backups" = "/nix/backups"; };
       };
-      services.beesd."/".threads = 4;
+      services =
+      {
+        xray.client = {};
+        beesd."/".threads = 4;
+        kvm.nodatacow = true;
+      };
     };
   };
 }
