a12ff043e11f5fc89efbbd6a2ba9b5dd7073dfb7
chn <chn@chn.moe>
2025-07-14 15:40:17 +0800

    devices.srv2-node0: enable bridge

--- devices/srv2/node0/default.nix
+++ devices/srv2/node0/default.nix
@@ -11,7 +11,8 @@ inputs:
         network =
         {
           static.eno2 = { ip = "192.168.178.1"; mask = 24; };
-          wireless.networks = [ "409的5G" ];
+          wireless = { networks = [ "409的5G" ]; fourAddr = true; };
+          bridge.br0.interfaces = [ "eno1" "wlp134s0" ];
           masquerade = [ "eno2" ];
           trust = [ "eno2" ];
         };
