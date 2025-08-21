fa396bd0ed94419fae48d67aa6353ad131df16fe
chn <chn@chn.moe>
2025-08-21 21:28:12 +0800

    modules.packages.chromium: only enable on desktop

--- modules/packages/chromium.nix
+++ modules/packages/chromium.nix
@@ -3,7 +3,7 @@ inputs:
   options.nixos.packages.chromium = let inherit (inputs.lib) mkOption types; in mkOption
   {
     type = types.nullOr (types.submodule {});
-    default = if builtins.elem inputs.config.nixos.model.type [ "desktop" "server" ] then {} else null;
+    default = if inputs.config.nixos.model.type == "desktop" then {} else null;
   };
   config = let inherit (inputs.config.nixos.packages) chromium; in inputs.lib.mkIf (chromium != null)
   {
