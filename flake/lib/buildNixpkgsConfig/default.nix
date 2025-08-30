dac39597cc372f21066ecf27ca02c63b9b437916
chn <chn@chn.moe>
2025-08-30 10:27:33 +0800

    flake.lib.buildNixpkgsConfig: fix alderlake build

--- flake/lib/buildNixpkgsConfig/default.nix
+++ flake/lib/buildNixpkgsConfig/default.nix
@@ -144,6 +144,8 @@ in platformConfig //
       })
       // (inputs.lib.optionalAttrs (nixpkgs.march == "silvermont")
         { c-blosc = prev.c-blosc.overrideAttrs { doCheck = false; }; })
+      // (inputs.lib.optionalAttrs (nixpkgs.march == "alderlake")
+        { x265 = prev.x265.override { inherit (final.genericPackages) stdenv; }; })
       // (inputs.lib.optionalAttrs (nixpkgs.arch or null == "aarch64") { nix = final.nixVersions.nix_2_29; })
   )];
 }
