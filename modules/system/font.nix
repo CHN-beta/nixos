40d4dbbaedc1be09f43e090ac55c20e93507bfbb
chn <chn@chn.moe>
2025-09-01 17:51:04 +0800

    modules.system.font: add fonts

--- modules/system/font.nix
+++ modules/system/font.nix
@@ -10,6 +10,8 @@ inputs:
         noto-fonts source-han-sans source-han-serif source-code-pro hack-font jetbrains-mono hack-font inter
         noto-fonts-color-emoji roboto sarasa-gothic source-han-mono wqy_microhei wqy_zenhei noto-fonts-cjk-sans
         noto-fonts-emoji corefonts vistafonts vistafonts-chs dejavu_fonts nerd-fonts.fira-code
+        # needed by typst may template
+        lxgw-wenkai libertinus
       ];
       fontconfig.defaultFonts =
       {
