fb857db9f4a0a36b6a58b6e4729269fb6a0978d2
chn <chn@chn.moe>
2025-09-16 08:49:45 +0800

    Revert "devices.pc: switch to cachyos kernel"
    This reverts commit 4705912140a0c7b5e94309a3ae2e000a0c58bc71.

--- devices/pc/default.nix
+++ devices/pc/default.nix
@@ -30,7 +30,6 @@ inputs:
         ];
         nixpkgs.march = "znver4";
         sysctl.laptop-mode = 5;
-        kernel.variant = "cachyos";
       };
       hardware.gpu.type = "amd";
       services =
