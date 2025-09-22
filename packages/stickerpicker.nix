aacb42d6925a1de8eab76074d26fa73275c69b84
chn <chn@chn.moe>
2025-09-22 09:56:13 +0800

    packages.stickerpicker: fix build

--- packages/stickerpicker.nix
+++ packages/stickerpicker.nix
@@ -1,6 +1,9 @@
-{ src, buildPythonApplication, aiohttp, yarl, pillow, telethon, cryptg, python-magic }: buildPythonApplication
+{ src, buildPythonApplication, aiohttp, yarl, pillow, telethon, cryptg, python-magic, setuptools }:
+buildPythonApplication
 {
   name = "stickerpicker";
   inherit src;
   propagatedBuildInputs = [ aiohttp yarl pillow telethon cryptg python-magic ];
+  pyproject = true;
+  build-system = [ setuptools ];
 }
