cc92fdf9c4423e921d16d31c8aaa6057a4b0e93e
chn <chn@chn.moe>
2025-09-24 14:56:11 +0800

    modules.packages.git: add config

--- modules/packages/git.nix
+++ modules/packages/git.nix
@@ -16,6 +16,7 @@ inputs:
         lfs.ssh.automultiplex = false; # 避免 lfs 一直要求触摸 yubikey
         receive.denyCurrentBranch = "warn"; # 允许 push 到非 bare 的仓库
         merge.ours.driver = true; # 允许 .gitattributes 中设置的 merge=ours 生效
+        advice.addIgnoredFile = false; # 关闭 add 忽略文件时的提示
       };
     };
   };
