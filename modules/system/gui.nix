a0dac3e24b506219fe61f26bdd84bb0f2f910f9b
chn <chn@chn.moe>
2025-09-25 15:47:40 +0800

    Reapply "module.system.gui: use kwallet in niri"
    This reverts commit e28519b2f4c81dd51ea8cf283e9b736c6ace583f.

--- modules/system/gui.nix
+++ modules/system/gui.nix
@@ -74,7 +74,15 @@ inputs:
       {
         config.programs.dankMaterialShell = { enable = true; enableKeybinds = true; enableSystemd = true; };
       })];
-      services.gnome.gcr-ssh-agent.enable = false;
+      # niri module will auto enable this, disable it to avoid conflict with system ssh-agent and kwallet
+      services.gnome = { gcr-ssh-agent.enable = false; gnome-keyring.enable = inputs.lib.mkForce false; };
+    })
+    # niri setup kwallet
+    (inputs.lib.mkIf (gui.implementation == "niri")
+    {
+      nixos.packages.packages._packages = with inputs.pkgs.kdePackages; [ kwallet kwalletmanager kwallet-pam ];
+      xdg.portal.extraPortals = [ inputs.pkgs.kdePackages.kwallet ];
+      security.pam.services.login.kwallet = { enable = true; package = inputs.pkgs.kdePackages.kwallet-pam; };
     })
   ];
 }
