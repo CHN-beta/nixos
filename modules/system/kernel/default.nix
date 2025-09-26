7674dae93fcb5f573ea0348574bea59bbf5cdb75
chn <chn@chn.moe>
2025-09-26 19:54:46 +0800

    modules.system.kernel: fix cachyos

--- modules/system/kernel/default.nix
+++ modules/system/kernel/default.nix
@@ -60,7 +60,7 @@ inputs:
         xanmod-lts = inputs.pkgs.linuxPackages_xanmod;
         xanmod-latest = inputs.pkgs.linuxPackages_xanmod_latest;
         xanmod-unstable = inputs.pkgs.pkgs-unstable.linuxPackages_xanmod_latest;
-        cachyos = inputs.pkgs.linuxPackages_cachyos;
+        cachyos = inputs.pkgs.linuxPackages_cachyos-gcc;
       }.${kernel.variant};
       kernelPatches =
         let patches =
