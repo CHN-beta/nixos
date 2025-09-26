3967974068a7982efabaceb15e3f226437140412
chn <chn@chn.moe>
2025-09-26 17:18:06 +0800

    packages.pybinding: init

--- flake.nix
+++ flake.nix
@@ -64,6 +64,7 @@
     phono3py = { url = "github:phonopy/phono3py"; flake = false; };
     sticker = { url = "git+https://git.chn.moe/chn/sticker.git?lfs=1"; flake = false; };
     speedtest = { url = "github:librespeed/speedtest"; flake = false; };
+    pybinding = { url = "git+https://github.com/dean0x7d/pybinding?submodules=1"; flake = false; };
   };
 
   outputs = inputs: let localLib = import ./flake/lib inputs.nixpkgs.lib; in
---
+++ packages/pybinding/default.nix
@@ -0,0 +1,12 @@
+{ src, buildPythonPackage, setuptools, cmake, buildProxy, distutils, numpy, scipy, matplotlib, pytest }:
+buildPythonPackage
+{
+  name = "pybinding";
+  inherit src;
+  pyproject = true;
+  build-system = [ setuptools cmake distutils ];
+  dependencies = [ numpy scipy matplotlib pytest ];
+  dontUseCmakeConfigure = true;
+  preBuild = ''source ${buildProxy}'';
+  patches = [ ./fix.patch ];
+}
