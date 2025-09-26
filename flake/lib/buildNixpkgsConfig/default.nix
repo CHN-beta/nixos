9348c13940a16e6c87159ea24d3e3a09f7f74cfd
chn <chn@chn.moe>
2025-09-27 00:25:50 +0800

    lib.buildNixpkgsConfig: fix sphinx build

--- flake/lib/buildNixpkgsConfig/default.nix
+++ flake/lib/buildNixpkgsConfig/default.nix
@@ -80,7 +80,21 @@ in platformConfig //
           {
             pkgs-2305 = "nixpkgs-2305";
             pkgs-2311 = "nixpkgs-2311";
-            pkgs-2411 = "nixpkgs-2411";
+            pkgs-2411 =
+            {
+              source = "nixpkgs-2411";
+              overlays =
+              [
+                (final: prev: inputs.lib.optionalAttrs (nixpkgs.march != null)
+                {
+                  pythonPackagesExtensions = prev.pythonPackagesExtensions or [] ++ [(final: prev:
+                  {
+                    sphinx = prev.sphinx.overridePythonAttrs (prev:
+                      { disabledTests = prev.disabledTests or [] ++ [ "test_xml_warnings" ]; });
+                  })];
+                })
+              ];
+            };
             # pkgs-unstable =
             # {
             #   source = "nixpkgs-unstable";
