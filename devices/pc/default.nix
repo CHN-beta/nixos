c233fde9f23d5375a05ff368031f9dcbc3f4a59e
chn <chn@chn.moe>
2025-09-29 17:17:50 +0800

    Reapply "devices.pc: switch to cachyos kernel"
    This reverts commit d40f1fbcab3bafb6dc8d8649bb28c5745723617c.

--- devices/pc/default.nix
+++ devices/pc/default.nix
@@ -34,7 +34,7 @@ inputs:
         ];
         nixpkgs = { march = "znver5"; rocm = true; };
         sysctl.laptop-mode = 5;
-        kernel.variant = "xanmod-latest";
+        kernel.variant = "cachyos";
       };
       hardware = { gpu.type = "amd"; asus = {};};
       services =
