7dba94af81e3b3c1f5db2fc293ebb9d5a819d006
chn <chn@chn.moe>
2025-09-29 17:20:22 +0800

    Revert "devices.pc: enable rocm support"
    This reverts commit db977df437af7fc4b4656a1b522d30b56bd50e19.

--- devices/pc/default.nix
+++ devices/pc/default.nix
@@ -32,7 +32,7 @@ inputs:
           # SAHF FXSR XSAVE RDRND LZCNT HLE PREFETCHW SGX PCONFIG
           "icelake-server"
         ];
-        nixpkgs = { march = "znver5"; rocm = true; };
+        nixpkgs = { march = "znver5"; rocm = false; };
         sysctl.laptop-mode = 5;
         kernel.variant = "cachyos";
       };
