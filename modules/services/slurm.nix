c0f5d6314dd33f3e64d283c9804f724bbe072604
chn <chn@chn.moe>
2025-10-12 14:09:42 +0800

    modules.services.slurm: add root to telegram users

--- modules/services/slurm.nix
+++ modules/services/slurm.nix
@@ -233,7 +233,7 @@ inputs:
         secrets = { "slurm/db" = { owner = "slurm"; key = "mariadb/slurm"; }; }
           // builtins.listToAttrs (builtins.map
             (n: inputs.lib.nameValuePair "telegram/${n}" {})
-            [ "token" "user/chn" "user/hjp" ]);
+            [ "token" "user/chn" "user/hjp" "user/root" ]);
         templates."info.yaml" =
         {
           owner = "slurm";
