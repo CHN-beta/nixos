c425c11c541dfef503426004b3554bc2e7ae5312
chn <chn@chn.moe>
2025-10-20 23:16:16 +0800

    Revert "modules.system.kernel: add amdgpu patch"
    This reverts commit 84c11d9223f33541e6d69c9ca5a15658fa22569b.

--- devices/pc/default.nix
+++ devices/pc/default.nix
@@ -34,7 +34,7 @@ inputs:
         ];
         nixpkgs = { march = "znver5"; rocm = true; };
         sysctl.laptop-mode = 5;
-        kernel = { variant = "cachyos"; patches = [ "amdgpu" ]; };
+        kernel.variant = "cachyos";
       };
       hardware = { gpu.type = "amd"; asus = {};};
       services =
