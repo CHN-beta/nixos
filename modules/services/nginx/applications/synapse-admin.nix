e900590e5152d06bf4a61896eddb3816bb26510e
chn <chn@chn.moe>
2025-10-26 09:56:21 +0800

    modules.services.nginx.applications.synapse-admin: fix

--- modules/services/nginx/applications/synapse-admin.nix
+++ modules/services/nginx/applications/synapse-admin.nix
@@ -1,26 +1,11 @@
 inputs:
 {
-  options.nixos.services.nginx.applications.synapse-admin.instances =
-    let inherit (inputs.lib) mkOption types; in mkOption
-    {
-      type = types.attrsOf (types.submodule (submoduleInputs: { options =
-        { hostname = mkOption { type = types.nonEmptyStr; default = submoduleInputs.config._module.args.name; }; };}));
-      default = {};
-    };
-  config =
-    let
-      inherit (inputs.config.nixos.services.nginx.applications.synapse-admin) instances;
-      inherit (inputs.localLib) attrsToList;
-      inherit (builtins) map listToAttrs;
-    in
-    {
-      nixos.services.nginx.https = listToAttrs (map
-        (site: with site.value;
-        {
-          name = hostname;
-          value.location."/".static =
-            { root = "${inputs.pkgs.synapse-admin}"; index = [ "index.html" ]; };
-        })
-        (attrsToList instances));
-    };
+  options.nixos.services.nginx.applications.synapse-admin.instances = let inherit (inputs.lib) mkOption types; in
+    mkOption { type = types.attrsOf (types.submodule (submoduleInputs: {})); default = {}; };
+  config = let inherit (inputs.config.nixos.services.nginx.applications.synapse-admin) instances; in
+  {
+    nixos.services.nginx.https = builtins.mapAttrs
+      (n: v: { location."/".static = { root = "${inputs.pkgs.synapse-admin-etkecc}"; index = [ "index.html" ]; }; })
+      instances;
+  };
 }
