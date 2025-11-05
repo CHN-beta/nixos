a2fd753ff1710fd72017d92cbe6bb615f4491a77
chn <chn@chn.moe>
2025-11-05 23:06:59 +0800

    modules.services.ollama: fix pull

--- modules/services/ollama.nix
+++ modules/services/ollama.nix
@@ -4,7 +4,12 @@ inputs:
     { type = types.nullOr (types.submodule {}); default = null; };
   config = let inherit (inputs.config.nixos.services) ollama; in inputs.lib.mkIf (ollama != null)
   {
-    services.ollama = { enable = true; host = "0.0.0.0"; };
+    services.ollama =
+    {
+      enable = true;
+      host = "0.0.0.0";
+      environmentVariables = { OLLAMA_REGISTRY_MAXSTREAMS = "2"; OLLAMA_EXPERIMENT= "client2"; };
+    };
     nixos.packages.packages._packages = [ inputs.pkgs.oterm ];
   };
 }
