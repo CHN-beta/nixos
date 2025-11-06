43ffea5e3b348b3113c100be8ceed876cafb9d9b
chn <chn@chn.moe>
2025-11-06 21:44:40 +0800

    modules.user.lilydjwg: add ssh key

--- modules/user/lilydjwg.nix
+++ modules/user/lilydjwg.nix
@@ -2,10 +2,17 @@ inputs:
 {
   config = let inherit (inputs.config.nixos) user; in inputs.lib.mkIf (builtins.elem "lilydjwg" user.users)
   {
-    users.users = inputs.lib.mkIf (inputs.config.nixos.model.hostname == "pc")
-    {
-      lilydjwg.extraGroups = [ "wheel" ];
-      root.openssh.authorizedKeys.keys = [(builtins.readFile ./keys/lilydjwg)];
-    };
+    users.users = inputs.lib.mkMerge
+    [
+      {
+        lilydjwg.openssh.authorizedKeys.keys =
+          [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA0P/hglDmmb77qkTpAEwxx6KH+3MI9sn/nb4fNafI8V root@wikis" ];
+      }
+      (inputs.lib.mkIf (inputs.config.nixos.model.hostname == "pc")
+      {
+        lilydjwg.extraGroups = [ "wheel" ];
+        root.openssh.authorizedKeys.keys = [(builtins.readFile ./keys/lilydjwg)];
+      })
+    ];
   };
 }
