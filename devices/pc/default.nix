51e9786246e14680e1e2f231e45c0e63bdb3cc59
chn <chn@chn.moe>
2025-11-07 19:40:20 +0800

    Revert "devices.pc: switch to rc kernel"
    This reverts commit b43f8a16e77bda071d4e92ca212e708b027563b0.

--- devices/pc/default.nix
+++ devices/pc/default.nix
@@ -34,7 +34,7 @@ inputs:
         ];
         nixpkgs = { march = "znver5"; rocm = true; };
         sysctl.laptop-mode = 5;
-        kernel.variant = "cachyos-rc";
+        kernel.variant = "cachyos";
       };
       hardware = { gpu.type = "amd"; asus = {};};
       services =
