b43f8a16e77bda071d4e92ca212e708b027563b0
chn <chn@chn.moe>
2025-11-07 17:07:09 +0800

    devices.pc: switch to rc kernel

--- devices/pc/default.nix
+++ devices/pc/default.nix
@@ -34,7 +34,7 @@ inputs:
         ];
         nixpkgs = { march = "znver5"; rocm = true; };
         sysctl.laptop-mode = 5;
-        kernel.variant = "cachyos";
+        kernel.variant = "cachyos-rc";
       };
       hardware = { gpu.type = "amd"; asus = {};};
       services =
