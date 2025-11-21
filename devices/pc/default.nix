26c09a35bdb3b25c44d6271d834e60658cd20861
chn <chn@chn.moe>
2025-11-21 13:31:43 +0800

    devices.pc: add xanmod variant

--- devices/pc/default.nix
+++ devices/pc/default.nix
@@ -102,6 +102,10 @@ inputs:
     '';
     # 解决有时蓝牙不能使用的问题
     boot.kernelParams = [ "mt7925e.disable_aspm=1" ];
-    specialisation.niri.configuration.nixos.system.gui.implementation = "niri";
+    specialisation =
+    {
+      niri.configuration.nixos.system.gui.implementation = "niri";
+      xanmod.configuration.nixos.system.kernel.variant = inputs.lib.mkForce "xanmod-lts";
+    };
   };
 }
