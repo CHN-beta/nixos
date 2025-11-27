ef592ca01468cf606fbd43d45790245a108fb00d
chn <chn@chn.moe>
2025-11-27 08:42:45 +0800

    Revert "devices.nas: disable store ro protect"
    This reverts commit 840eb7c09c234fcb2dfdf8a952449c95c6e17fe4.

--- devices/nas/default.nix
+++ devices/nas/default.nix
@@ -73,6 +73,5 @@ inputs:
     };
     systemd.tmpfiles.rules =
       [ "w /sys/class/powercap/intel-rapl/intel-rapl:0/constraint_0_power_limit_uw - - - - 10000000" ];
-    boot.nixStoreMountOpts = [ "nodev" "nosuid" ];
   };
 }
