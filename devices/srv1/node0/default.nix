4e2354c2021e8cad672178bdb956f0fcf5c0c7bb
chn <chn@chn.moe>
2025-11-27 16:49:27 +0800

    modules.services.xrdp: drop, since plasma will not support it

--- devices/srv1/node0/default.nix
+++ devices/srv1/node0/default.nix
@@ -23,7 +23,6 @@ inputs:
       {
         sshd.motd = true;
         beesd."/" = { hashTableSizeMB = 128; threads = 4; };
-        xrdp = { enable = true; hostname = [ "srv1.chn.moe" ]; };
         samba = { hostsAllowed = ""; shares = { home.path = "/home"; root.path = "/"; }; };
       };
       packages =
