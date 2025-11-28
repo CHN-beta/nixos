2cf730b48f1dc111c81ac540f00b0820be6c990e
chn <chn@chn.moe>
2025-11-28 10:04:33 +0800

    devices.pc: use lts kernel as default

--- devices/pc/default.nix
+++ devices/pc/default.nix
@@ -50,7 +50,7 @@ inputs:
         ];
         nixpkgs = { march = "znver5"; rocm = true; };
         sysctl.laptop-mode = 5;
-        kernel.variant = "cachyos";
+        kernel.patches = [ "btrfs" ];
       };
       hardware = { gpu.type = "amd"; asus = {};};
       services =
@@ -121,7 +121,5 @@ inputs:
     '';
     # 解决有时蓝牙不能使用的问题
     boot.kernelParams = [ "mt7925e.disable_aspm=1" ];
-    specialisation.linux-lts.configuration.nixos.system.kernel =
-      { variant = inputs.lib.mkForce "xanmod-lts"; patches = [ "btrfs" ]; };   
   };
 }
