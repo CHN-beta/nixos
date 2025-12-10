0d2aff16a8cf42f032f81365e5d9cff88f78acf7
chn <chn@chn.moe>
2025-12-10 11:47:51 +0800

    devices.pc: enable waydroid

--- devices/pc/default.nix
+++ devices/pc/default.nix
@@ -92,6 +92,7 @@ inputs:
         mariadb.mountFrom = "nodatacow";
         open-webui.ollamaHost = "127.0.0.1";
         howdy = {};
+        waydroid = {};
       };
       bugs = [ "amdpstate" ];
       packages = { mathematica = {}; vasp = {}; };
