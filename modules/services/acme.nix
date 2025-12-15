57efd0fb9454b4dabab3f0149c8a11c151938d23
Haonan Chen <chn@chn.moe>
2025-12-15 10:10:11 +0800

    Revert "modules.services.acme: use google dns"
    This reverts commit 397d8bb7674d314eba66633a05880bdb4f21abfe.

--- modules/services/acme.nix
+++ modules/services/acme.nix
@@ -26,7 +26,7 @@ inputs:
       {
         email = "chn@chn.moe";
         dnsProvider = "cloudflare";
-        dnsResolver = "8.8.8.8";
+        dnsResolver = "1.1.1.1";
       };
       certs = builtins.listToAttrs (builtins.map
         (cert:
