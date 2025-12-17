c507e8d69d78584fb4add67c7ce6f25b19a9f2a5
Haonan Chen <chn@chn.moe>
2025-12-17 15:56:22 +0800

    Revert "modules.services.postgresql: enable jit"
    This reverts commit 9e66c845c14282e08559e31c592ae01c1ae560f3.

--- modules/services/postgresql.nix
+++ modules/services/postgresql.nix
@@ -23,7 +23,7 @@ inputs:
       postgresql =
       {
         enable = true;
-        package = inputs.pkgs.postgresql_17_jit;
+        package = inputs.pkgs.postgresql_17;
         extensions = ps: with ps; [ pgroonga ];
         enableTCPIP = true;
         authentication = "host all all 0.0.0.0/0 md5";
