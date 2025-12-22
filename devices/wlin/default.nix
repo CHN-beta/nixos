37a5069c3a91a05108a51dccc0b2db0521cb9d75
Haonan Chen <chn@chn.moe>
2025-12-22 19:02:33 +0800

    devices.wlin: add archive

--- devices/wlin/default.nix
+++ devices/wlin/default.nix
@@ -5,10 +5,11 @@ let
     inputs = { inherit (inputs.nixpkgs) lib; topInputs = inputs; };
     nixpkgs = { march = "haswell"; nixRoot = "/data/gpfs01/wlin/.nix"; nixos = false; };
   });
-in pkgs.symlinkJoin
-{
-  name = "wlin";
-  paths = with pkgs; [ gnuplot localPackages.vaspkit pv ];
-  postBuild = "echo ${inputs.self.rev or "dirty"} > $out/.version";
-  passthru = { inherit pkgs; };
-}
+  wlin = pkgs.symlinkJoin
+  {
+    name = "wlin";
+    paths = with pkgs; [ gnuplot localPackages.vaspkit pv ];
+    postBuild = "echo ${inputs.self.rev or "dirty"} > $out/.version";
+    passthru = { inherit pkgs; archive = pkgs.closureInfo { rootPaths = [ wlin.drvPath ]; }; };
+  };
+in wlin
