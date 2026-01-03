3e3e62838b2b8c7e03c7107da4a0e2e17d7afc2f
Haonan Chen <chn@chn.moe>
2026-01-03 23:04:29 +0800

    devices.pc: switch to xanmod kernel

--- devices/pc/default.nix
+++ devices/pc/default.nix
@@ -50,7 +50,7 @@ inputs:
         ];
         nixpkgs = { march = "znver5"; rocm = true; };
         sysctl.laptop-mode = 5;
-        kernel = { variant = "cachyos"; patches = [ "btrfs" ]; };
+        kernel = { variant = "xanmod-latest"; patches = [ "btrfs" ]; };
       };
       hardware = { gpu.type = "amd"; asus = {};};
       services =
