3b2ed6d850c673278f671d48e9ecf8eae577a5c8
Haonan Chen <chn@chn.moe>
2026-01-06 12:20:25 +0800

    modules.system.systemd: handle lid switch

--- modules/system/systemd.nix
+++ modules/system/systemd.nix
@@ -3,11 +3,7 @@ inputs:
   config =
   {
     # only preserve the last 7 days of logs
-    services =
-    {
-      journald.extraConfig = "MaxRetentionSec=7d";
-      logind.settings.Login.HandleLidSwitch = "ignore";
-    };
+    services.journald.extraConfig = "MaxRetentionSec=7d";
     systemd =
     {
       settings.Manager =
