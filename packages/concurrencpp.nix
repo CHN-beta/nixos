361b6004552c35a52f2285016e6b7008e6a1dae8
Haonan Chen <chn@chn.moe>
2026-03-02 19:54:22 +0800

    packages.concurrencpp: fix windows build

--- packages/concurrencpp.nix
+++ packages/concurrencpp.nix
@@ -1,6 +1,7 @@
-{ stdenv, cmake, src }: stdenv.mkDerivation
+{ stdenv, cmake, src, windows, lib }: stdenv.mkDerivation
 {
   name = "concurrencpp";
   inherit src;
+  buildInputs = lib.optionals stdenv.hostPlatform.isMinGW [ windows.pthreads ];
   nativeBuildInputs = [ cmake ];
 }
