42aa638b79408982f5e48f350577cc90afdb34af
Haonan Chen <chn@chn.moe>
2026-03-13 19:05:08 +0800

    modules.packages.minimal: add xray

--- modules/packages/minimal.nix
+++ modules/packages/minimal.nix
@@ -33,7 +33,7 @@ inputs:
         apacheHttpd openssl ssh-to-age gnupg age sops pam_u2f yubico-piv-tool libfido2
         # networking
         ipset iptables iproute2 dig nettools traceroute tcping-go whois tcpdump nmap inetutils wireguard-tools openvpn
-        parted
+        parted xray
         # nix tools
         nix-output-monitor nix-tree ssh-to-age nix-inspect
         # development
