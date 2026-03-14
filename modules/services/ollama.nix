3940fb2445a948deb6e6b195db48536dce98ec5f
Haonan Chen <chn@chn.moe>
2026-03-14 13:25:32 +0800

    modules.services.ollama: fix memory usage

--- modules/services/ollama.nix
+++ modules/services/ollama.nix
@@ -7,7 +7,20 @@
     {
       enable = true;
       host = "0.0.0.0";
-      environmentVariables = { OLLAMA_REGISTRY_MAXSTREAMS = "2"; OLLAMA_EXPERIMENT= "client2"; };
+      environmentVariables =
+      {
+        # fix model pull failed
+        OLLAMA_REGISTRY_MAXSTREAMS = "2";
+        OLLAMA_EXPERIMENT = "client2";
+        # minimize context memory usage
+        OLLAMA_KV_CACHE_TYPE = "q4_0";
+        OLLAMA_FLASH_ATTENTION = "1";
+        OLLAMA_CONTEXT_LENGTH = "32768";
+        # disable GTT usage
+        HSA_ENABLE_SDMA = "0";
+        # limit ollama CPU threads
+        OLLAMA_NUM_THREADS = "1";
+      };
       package = pkgs.pkgs-unstable.ollama;
     };
     nixos.packages.packages._packages = [ pkgs.oterm ];
