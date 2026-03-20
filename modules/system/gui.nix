9dfc219c401bc2e94ebdce4cf7204b35d605ed22
Haonan Chen <chn@chn.moe>
2026-03-20 13:58:52 +0800

    modules.system.gui: try fix qt icon

--- modules/system/gui.nix
+++ modules/system/gui.nix
@@ -28,6 +28,7 @@
         GTK_USE_PORTAL = "1";
         # let electron use gnome keyring https://github.com/electron/electron/issues/39789#issuecomment-3433810585
         GNOME_DESKTOP_SESSION_ID = "this-is-deprecated";
+        QT_QPA_PLATFORMTHEME = "gtk3";
       };
       persistence."/nix/persistent".directories =
         [{ directory = "/var/cache/tuigreet"; user = "greeter"; group = "greeter"; mode = "0700"; }];
@@ -44,7 +45,7 @@
       ];
     };
     xdg.portal.extraPortals = (builtins.map (p: pkgs."xdg-desktop-portal-${p}") [ "gtk" "wlr" "gnome" ]);
-    qt = { enable = true; platformTheme = "qt5ct"; };
+    qt.enable = true;
     gtk.iconCache.enable = true;
     i18n.inputMethod =
     {
