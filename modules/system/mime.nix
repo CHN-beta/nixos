764e8a6dc6c5435bfa8379de7d5b17fa0f9e5756
Haonan Chen <chn@chn.moe>
2026-03-26 11:33:01 +0800

    modules.system.mime: set firefox as default browser

--- modules/system/mime.nix
+++ modules/system/mime.nix
@@ -1,6 +1,11 @@
 {
   config =
   {
-    xdg.mime.addedAssociations."application/pdf" = "org.kde.okular.desktop";
+    xdg.mime.addedAssociations =
+    {
+      "application/pdf" = "org.kde.okular.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+    };
   };
 }
