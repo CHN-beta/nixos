4fea555213c463edfd4943f17edd8b47fcdfb9cd
Haonan Chen <chn@chn.moe>
2026-03-28 14:51:24 +0800

    devices.srv2-node1: loose gpu power limit

--- devices/srv2/node1/default.nix
+++ devices/srv2/node1/default.nix
@@ -25,7 +25,7 @@
       {
         wantedBy = [ "multi-user.target" ];
         path = [ config.hardware.nvidia.package ];
-        script = "nvidia-smi -pl 200";
+        script = "nvidia-smi -pl 300";
         serviceConfig.Type = "oneshot";
       };
     };
