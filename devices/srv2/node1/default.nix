8b7a7f010924e6cd60b679e7149ef3ffda190a4f
Haonan Chen <chn@chn.moe>
2026-03-31 12:25:02 +0800

    devices.srv2-node1: enable cpu turbo

--- devices/srv2/node1/default.nix
+++ devices/srv2/node1/default.nix
@@ -18,16 +18,12 @@
         healthcheck = {};
       };
     };
-    systemd =
+    systemd.services.nvidia-power-limit =
     {
-      tmpfiles.rules = [ "w /sys/devices/system/cpu/intel_pstate/no_turbo - - - - 1" ];
-      services.nvidia-power-limit =
-      {
-        wantedBy = [ "multi-user.target" ];
-        path = [ config.hardware.nvidia.package ];
-        script = "nvidia-smi -pl 300";
-        serviceConfig.Type = "oneshot";
-      };
+      wantedBy = [ "multi-user.target" ];
+      path = [ config.hardware.nvidia.package ];
+      script = "nvidia-smi -pl 300";
+      serviceConfig.Type = "oneshot";
     };
   };
 }
