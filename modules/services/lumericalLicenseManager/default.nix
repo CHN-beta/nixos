bc6a22152e5ab1b6f6bf738461d2555300e25ff7
Haonan Chen <chn@chn.moe>
2026-04-07 13:39:59 +0800

    modules.services.lumericalLicenseManager: mordenize

--- modules/services/lumericalLicenseManager/default.nix
+++ modules/services/lumericalLicenseManager/default.nix
@@ -1,29 +1,28 @@
-inputs:
+{ lib, config, topInputs, pkgs, ... }:
 {
-  options.nixos.services.lumericalLicenseManager = let inherit (inputs.lib) mkOption types; in mkOption
+  options.nixos.services.lumericalLicenseManager = lib.mkOption
   {
-    type = types.nullOr (types.submodule { options =
+    type = lib.types.nullOr (lib.types.submodule { options =
     {
-      macAddress = mkOption { type = types.str; };
-      autoStart = mkOption { type = types.bool; default = true; };
+      macAddress = lib.mkOption { type = lib.types.str; };
+      autoStart = lib.mkOption { type = lib.types.bool; default = true; };
     };});
     default = null;
   };
-  config = let inherit (inputs.config.nixos.services) lumericalLicenseManager;
-    in inputs.lib.mkIf (lumericalLicenseManager != null)
+  config = let inherit (config.nixos.services) lumericalLicenseManager; in lib.mkIf (lumericalLicenseManager != null)
   {
     virtualisation.oci-containers.containers.lumericalLicenseManager =
     {
-      inherit (inputs.topInputs.self.src.lumerical.licenseManager) image imageFile;
+      inherit (topInputs.self.src.lumerical.licenseManager) image imageFile;
       extraOptions = [ "--network=host" ];
       volumes =
         let
           macAddress = builtins.replaceStrings [ ":" ] [ "" ] lumericalLicenseManager.macAddress;
-          license = inputs.pkgs.localPackages.lumerical.license.override { inherit macAddress; };
+          license = pkgs.localPackages.lumerical.license.override { inherit macAddress; };
         in [ "${license}:/home/ansys_inc/shared_files/licensing/license_files/ansyslmd.lic" ];
     };
     nixos.services.podman = {};
     systemd.services.podman-lumericalLicenseManager.wantedBy =
-      inputs.lib.mkIf (!lumericalLicenseManager.autoStart) (inputs.lib.mkForce []);
+      lib.mkIf (!lumericalLicenseManager.autoStart) (lib.mkForce []);
   };
 }
