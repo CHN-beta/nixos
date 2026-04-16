ee60e24cb50a986a404202e52ee0cdc358c7f6ae
Haonan Chen <chn@chn.moe>
2026-04-17 00:21:06 +0800

    modules.packages.direnv: cleanup

--- modules/packages/direnv.nix
+++ modules/packages/direnv.nix
@@ -1,13 +1,6 @@
 { lib, config, ... }:
 {
-  options.nixos.packages.direnv = lib.mkOption
-  {
-    type = lib.types.nullOr (lib.types.submodule {});
-    default =
-      if (config.nixos.model.type == "desktop") && (config.nixos.model.arch == "x86_64") then {}
-      else null;
-  };
-  config = let inherit (config.nixos.packages) direnv; in lib.mkIf (direnv != null)
+  config = lib.mkIf ((config.nixos.model.type == "desktop") && (config.nixos.model.arch == "x86_64"))
   {
     programs.direnv = { enable = true; nix-direnv.enable = true; };
     nixos.user.sharedModules = [{ config.programs.direnv.enable = true; }];
