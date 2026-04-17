b5ad2cf462e63b95e24d4c7b30a1ac5f19c9bcd5
Haonan Chen <chn@chn.moe>
2026-04-17 09:00:16 +0800

    modules.services.lumericalLicenseManager: remove autoStart option

--- modules/services/lumericalLicenseManager/default.nix
+++ modules/services/lumericalLicenseManager/default.nix
@@ -5,7 +5,6 @@
     type = lib.types.nullOr (lib.types.submodule { options =
     {
       macAddress = lib.mkOption { type = lib.types.str; };
-      autoStart = lib.mkOption { type = lib.types.bool; default = true; };
     };});
     default = null;
   };
@@ -22,7 +21,5 @@
         in [ "${license}:/home/ansys_inc/shared_files/licensing/license_files/ansyslmd.lic" ];
     };
     nixos.services.podman = {};
-    systemd.services.podman-lumericalLicenseManager.wantedBy =
-      lib.mkIf (!lumericalLicenseManager.autoStart) (lib.mkForce []);
   };
 }
