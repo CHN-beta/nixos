ae10c73deb7cf14c3fba2d98cadb0e24f74785bf
Haonan Chen <chn@chn.moe>
2026-04-20 22:35:04 +0800

    flake.nixos: refactory

--- flake/nixos.nix
+++ flake/nixos.nix
@@ -3,31 +3,25 @@ let
   inherit (inputs.nixpkgs) lib;
   singles = [ "nas" "pc" "vps4" "vps6" "vps9" "pe" ];
   cluster = { srv1 = 3; srv2 = 3; };
-  deviceModules = builtins.listToAttrs
-  (
-    (builtins.map
-      (n: { name = n; value = [ { config.nixos.model.hostname = n; } ../devices/${n} ]; })
-      singles)
-    ++ (builtins.concatLists (builtins.map
-      (cluster: builtins.map
-        (node:
-        {
-          name = "${cluster.name}-${node}";
-          value =
+  deviceModules =
+    [
+      (singles |> lib.flip lib.genAttrs (n: [ { config.nixos.model.hostname = n; } ../devices/${n} ]))
+      (cluster
+        |> lib.mapAttrsToList (n: v: lib.genList (i: "node${builtins.toString i}") v
+          |> lib.flip lib.genAttrs' (node: lib.nameValuePair "${n}-${node}"
           [
-            { config.nixos.model.cluster = { clusterName = cluster.name; nodeName = node; }; }
-            ../devices/${cluster.name}
-            ../devices/${cluster.name}/${node}
-          ];
-        })
-        (builtins.genList (n: "node${builtins.toString n}") cluster.value))
-      (lib.attrsToList cluster)))
-  );
-in builtins.mapAttrs
-  (n: v: lib.nixosSystem
-  {
-    system = null;
-    specialArgs = { flakeInputs = inputs; inherit localLib; };
-    modules = (localLib.mkModules v) ++ [ inputs.self.nixosModules.default ];
-  })
-  deviceModules
+            { config.nixos.model.cluster = { clusterName = n; nodeName = node; }; }
+            ../devices/${n}
+            ../devices/${n}/${node}
+          ]))
+        |> lib.mergeAttrsList)
+    ]
+    |> lib.mergeAttrsList;
+in deviceModules
+  |> builtins.mapAttrs
+    (n: v: lib.nixosSystem
+    {
+      system = null;
+      specialArgs = { flakeInputs = inputs; inherit localLib; };
+      modules = (localLib.mkModules v) ++ [ inputs.self.nixosModules.default ];
+    })
