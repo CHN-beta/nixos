3de1d957efa368165e43529710cc3c8c78b786ec
Haonan Chen <chn@chn.moe>
2026-04-25 18:16:47 +0800

    lib.buildNixpkgsConfig: fix boost

--- packages/debug-kernel310-hang/src/main.cpp
+++ packages/debug-kernel310-hang/src/main.cpp
@@ -7,14 +7,15 @@ int main()
 {
   namespace bp = boost::process;
   boost::asio::io_context context;
-  boost::filesystem::path actual_program = "/data/gpfs01/jykang/linwei/chn/software/hpcstat/bin/ssh-add";
+  boost::filesystem::path actual_program = bp::environment::find_executable("echo");
   auto env = bp::environment::current();
   auto stdout_pipe = std::make_unique<boost::asio::readable_pipe>(context);
   std::string stdout_string;
   bp::process_stdio stdio{ .in = {}, .out = *stdout_pipe, .err = {} };
   auto proc = bp::process
-    (context, actual_program, { "-l" }, std::move(stdio), std::move(env));
+    (context, actual_program, { "hhh" }, std::move(stdio), std::move(env));
   proc.wait();
-  boost::asio::read(*stdout_pipe, boost::asio::dynamic_buffer(stdout_string));
+  boost::system::error_code ec;
+  boost::asio::read(*stdout_pipe, boost::asio::dynamic_buffer(stdout_string), ec);
   std::cout << std::format("{} {}\n", proc.exit_code(), stdout_string);
 }
