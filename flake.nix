1f511559d03222cfc7a09c4d9662f6d03513afc2
Haonan Chen <chn@chn.moe>
2026-05-12 20:33:34 +0800

    flake/dev -> devShells

--- flake.nix
+++ flake.nix
@@ -82,7 +82,7 @@
     overlay.default = import ./overlays { localLib = lib; flakeInputs = inputs; };
     nixosModules.default.imports = lib.mkModules [ ./nixosModules ];
     config.dns = inputs.self.packages.x86_64-linux.dns-push.meta.config;
-    devShells.x86_64-linux = import ./flake/dev.nix { inherit inputs; };
+    devShells.x86_64-linux = import ./devShells.nix { inherit inputs; };
     src = import ./flake/src.nix { inherit inputs; };
     apps.x86_64-linux = import ./flake/apps.nix { inherit inputs; };
   };
