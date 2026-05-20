8f7cdae633e37537249f6f0a74888da6b85b2006
Haonan Chen <chn@chn.moe>
2026-05-20 18:29:51 +0800

    apps.deploy: optimize

--- apps.nix
+++ apps.nix
@@ -1,6 +1,6 @@
 self:
 let
-  inherit (self.packages.x86_64-linux) dns-push pkgs lib;
+  inherit (self.packages.x86_64-linux) pkgs lib;
   build = self.nixosConfigurations
     |> builtins.attrNames
     |> builtins.map (name:
@@ -10,33 +10,39 @@ let
       '')
     |> builtins.concatStringsSep "\n"
     |> pkgs.writeShellScript "build";
-  deployKdl = self.nixosConfigurations
-    |> builtins.attrNames
-    |> lib.subtractLists [ "pe" ]
-    |> builtins.map (host:
-      ''
-        tab name="${host}" {
-          pane command="nixos-rebuild" {
-            args "switch" "--flake" ".#${host}" "--target-host" "root@ts.${host}" "--option" "store" "/nix/tf"
-          }
-        }
-      '')
-    |> (hosts: pkgs.writeText "deploy.kdl"
-      ''
-        layout {
-          default_tab_template {
-            pane size=1 borderless=true {
-              plugin location="zellij:tab-bar"
+
+  deploy =
+    let
+      doNotDeploy = [ "pe" ];
+      doNotUseTailscale = [ "vps4" "vps6" "vps9" ];
+      deployKdl = self.nixosConfigurations
+        |> lib.attrNames
+        |> lib.subtractLists doNotDeploy
+        |> lib.map (host:
+          let connect = if lib.elem host doNotUseTailscale then host else "ts.${host}"; in
+          ''
+            tab name="${host}" {
+              pane command="nixos-rebuild" {
+                args "switch" "--flake" ".#${host}" "--target-host" "root@${connect}" "--option" "store" "/nix/tf"
+              }
             }
-            children
-            pane size=2 borderless=true {
-              plugin location="zellij:status-bar"
+          '')
+        |> (hosts: pkgs.writeText "deploy.kdl"
+          ''
+            layout {
+              default_tab_template {
+                pane size=1 borderless=true {
+                  plugin location="zellij:tab-bar"
+                }
+                children
+                pane size=2 borderless=true {
+                  plugin location="zellij:status-bar"
+                }
+              }
+              ${lib.concatStringsSep "\n" hosts}
             }
-          }
-          ${lib.concatStringsSep "\n" hosts}
-        }
-      '');
-    deploy = pkgs.writeShellScript "deploy" "zellij --layout ${deployKdl}";
+          '');
+    in pkgs.writeShellScript "deploy" "zellij --layout ${deployKdl}";
 in
-  { inherit dns-push build deploy; }
+  { inherit (self.packages.x86_64-linux) dns-push; inherit build deploy; }
   |> builtins.mapAttrs (_: v: { type = "app"; program = "${v}"; })
