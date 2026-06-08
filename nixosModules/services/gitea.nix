d15922e08a7cca7b41874063f8cb7c1eed1ffaae
Haonan Chen <chn@chn.moe>
2026-06-08 10:51:37 +0800

    nixosModules.services.gitea: allow git

--- nixosModules/services/gitea.nix
+++ nixosModules/services/gitea.nix
@@ -75,6 +75,11 @@
           [
             { import = "(data)/meta/default-config.yaml"; }
             {
+              name = "allow-git";
+              action = "ALLOW";
+              expression.any = [ ''userAgent.contains("git/")'' ''userAgent.contains("Nix/")'' ];
+            }
+            {
               name = "challenge-all";
               path_regex = ".*";
               action = "CHALLENGE";
