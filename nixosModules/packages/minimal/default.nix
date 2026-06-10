e9f86e13201c52cb87b1f1fd5668295f66c70b26
Haonan Chen <chn@chn.moe>
2026-06-10 18:48:35 +0800

    nixosModules/packages/minimal: zellij enable catppuccin

--- nixosModules/packages/minimal/default.nix
+++ nixosModules/packages/minimal/default.nix
@@ -190,13 +190,16 @@
       environment.systemPackages = [ pkgs.zellij ];
       nixos.user.sharedModules = [
         {
-          config.programs.zellij = {
-            enable = true;
-            settings = {
-              scroll_buffer_size = 100000000;
-              show_startup_tips = false;
-              show_release_notes = false;
+          config = {
+            programs.zellij = {
+              enable = true;
+              settings = {
+                scroll_buffer_size = 100000000;
+                show_startup_tips = false;
+                show_release_notes = false;
+              };
             };
+            catppuccin.zellij.enable = true;
           };
         }
       ];
