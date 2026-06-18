de1490735786bbb445c0d6c848847989b07dbf79
Haonan Chen <chn@chn.moe>
2026-06-18 17:44:18 +0800

    overlay/packages/blog-buildproxy: fix mermaid

--- overlay/packages/blog-buildproxy.nix
+++ overlay/packages/blog-buildproxy.nix
@@ -1,15 +1,26 @@
-{ fetchurl }:
-[
+{ fetchurl }: [
   {
     url = "https://cdn.jsdelivr.net/npm/flexsearch@0.8.143/dist/flexsearch.bundle.min.js";
     file = fetchurl {
       url = "https://cdn.jsdelivr.net/npm/flexsearch@0.8.143/dist/flexsearch.bundle.min.js";
-      sha256 = "0k3g87h84s667m7zphlsaqzvkdka4rszq5pw66cvngjpi8d98gj3";
+      hash = "sha256-Qz6UGopXPruZMfwW/HUmara5P1aawvtPPcZoguBBb0w=";
     };
     status_code = 200;
     headers = {
       "content-type" = "application/javascript; charset=utf-8";
-      "content-length" = "46087";
+      "content-length" = "15422";
+    };
+  }
+  {
+    url = "https://cdn.jsdelivr.net/npm/mermaid@latest/dist/mermaid.min.js";
+    file = fetchurl {
+      url = "https://cdn.jsdelivr.net/npm/mermaid@latest/dist/mermaid.min.js";
+      hash = "sha256-cBN+d7snO7LvlyuG6LBADMqL5TyyW/xFkRoYbcmGZd4=";
+    };
+    status_code = 200;
+    headers = {
+      "content-type" = "application/javascript; charset=utf-8";
+      "content-length" = "914605";
     };
   }
 ]
