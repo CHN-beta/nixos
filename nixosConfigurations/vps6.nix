9179abf2962aaa77f3e9fb6398262f7c601a6be1
Haonan Chen <chn@chn.moe>
2026-07-03 11:29:59 +0800

    nixosConfigurations/vps6: set ipv6 tunnel

--- nixosConfigurations/vps6.nix
+++ nixosConfigurations/vps6.nix
@@ -48,6 +48,33 @@
         vikunja = { };
       };
     };
+    systemd.network = {
+      netdevs."10-ipv6net" = {
+        netdevConfig = {
+          Name = "ipv6net";
+          Kind = "sit";
+        };
+        tunnelConfig = {
+          Local = pkgs.localPkgs.getAddress "vps6";
+          Remote = "45.32.66.87";
+          TTL = 255;
+        };
+      };
+      networks = {
+        "10-ipv6net" = {
+          matchConfig.Name = "ipv6net";
+          address = [ "2607:8700:5500:2255::2/64" ];
+          routes = [
+            {
+              Gateway = "2607:8700:5500:2255::1";
+              Destination = "::/0";
+            }
+          ];
+          linkConfig.RequiredForOnline = "routable";
+        };
+        "10-ens18".networkConfig.Tunnel = "ipv6net";
+      };
+    };
     networking.nftables.tables.forward = {
       family = "inet";
       content =
