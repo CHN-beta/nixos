b51e38d20ef89a76fdf8c62a22718b761218a78e
Haonan Chen <chn@chn.moe>
2026-07-06 09:32:30 +0800

    lib/buildNixpkgsConfig: fix elfutils

--- lib/buildNixpkgsConfig/default.nix
+++ lib/buildNixpkgsConfig/default.nix
@@ -446,13 +446,13 @@ let
           #   patches = prev.patches or [ ] ++ [ ./sysprof.patch ];
           # });
           # gnupg = prev.gnupg.override { enableMinimal = true; };
-          # elfutils = prev.elfutils.overrideAttrs (prev: {
-          #   env = prev.env or { } // {
-          #     NIX_CFLAGS_COMPILE = "-Wno-error=unused-but-set-variable";
-          #   };
-          #   doCheck = false;
-          #   doInstallCheck = false;
-          # });
+          elfutils = prev.elfutils.overrideAttrs (prev: {
+            env = prev.env or { } // {
+              NIX_CFLAGS_COMPILE = "-Wno-error=unused-but-set-variable";
+            };
+            doCheck = false;
+            doInstallCheck = false;
+          });
           # go = prev.go.overrideAttrs (prev: {
           #   env = prev.env or { } // {
           #     CGO_ENABLED = 0;
