33bb3c3345c4b71357ee63e6bcabc90815af6c95
Haonan Chen <chn@chn.moe>
2026-07-06 09:29:57 +0800

    lib/buildNixpkgsConfig: disable systemd

--- lib/buildNixpkgsConfig/default.nix
+++ lib/buildNixpkgsConfig/default.nix
@@ -396,18 +396,24 @@ let
           # # for matplotlib
           # enableTk = false;
           # vtk = null;
-          # # systemd does not working
-          # systemd = null;
-          # systemdMinimal = null;
-          # systemdLibs = null;
-          # udevCheckHook = null;
-          # bubblewrap = null;
-          # enableUdev = false;
-          # enableSystemd = false;
-          # withLogind = false;
-          # systemdSupport = false;
-          # udevSupport = false;
-          # withSystemd = false;
+          # systemd = prev.systemd.overrideAttrs (old: {
+          #   patches = old.patches or [ ] ++ [ ./systemd-kernel310.patch ];
+          # });
+          # systemd use too much new kernel features
+          systemd = null;
+          systemdMinimal = null;
+          systemdLibs = null;
+          enableSystemd = false;
+          systemdSupport = false;
+          withSystemd = false;
+          # udev depend on systemd
+          enableUdev = false;
+          withLogind = false;
+          udevSupport = false;
+          udevCheckHook = null;
+          # bluez depend on udev
+          bluez = null;
+          libgudev = prev.libgudev.overrideAttrs (prev: { doCheck = false; });
           # # per package fixes
           audit = prev.audit.overrideAttrs (old: {
             patches = old.patches or [ ] ++ [ ./audit-calipso.patch ];
