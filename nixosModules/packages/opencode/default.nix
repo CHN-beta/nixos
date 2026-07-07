084e984641b7d75fdb4b3cc050d8bff0f08fff6c
Haonan Chen <chn@chn.moe>
2026-07-07 17:25:44 +0800

    nixosModules/packages/opencode: add opencode-notifier

--- nixosModules/packages/opencode/default.nix
+++ nixosModules/packages/opencode/default.nix
@@ -2,12 +2,17 @@
   lib,
   config,
   self,
+  pkgs,
   ...
 }:
 {
   config = lib.mkIf (config.nixos.model.variant == "desktop") {
     environment = {
-      systemPackages = [ self.inputs.llm-agents.packages.x86_64-linux.opencode ];
+      systemPackages = [
+        self.inputs.llm-agents.packages.x86_64-linux.opencode
+        # needed by opencode-notifier
+        pkgs.libnotify
+      ];
       persistence."/nix/persistent".users.chn.directories = [ ".cache/opencode" ];
     };
     nixos.user.sharedModules = [
@@ -19,7 +24,10 @@
               package = self.inputs.llm-agents.packages.x86_64-linux.opencode;
               enableMcpIntegration = true;
               settings = {
-                plugin = [ "opencode-antigravity-auth@1.6.0" ];
+                plugin = [
+                  "opencode-antigravity-auth@1.6.0"
+                  "@mohak34/opencode-notifier@0.2.8"
+                ];
                 # copy from https://github.com/NoeFabris/opencode-antigravity-auth#models
                 provider.google = (builtins.fromJSON (builtins.readFile ./google.json)).provider.google;
               };
