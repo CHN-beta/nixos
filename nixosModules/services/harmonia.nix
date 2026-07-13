0ded45390f71d332dcec6121e0398f44a31ed9f9
Haonan Chen <chn@chn.moe>
2026-07-13 20:16:02 +0800

    nixosModules/services/harmonia: listen on 5001 port

--- nixosModules/services/harmonia.nix
+++ nixosModules/services/harmonia.nix
@@ -1,48 +1,47 @@
-inputs: {
-  options.nixos.services.harmonia =
-    let
-      inherit (inputs.lib) mkOption types;
-    in
-    mkOption {
-      type = types.nullOr (
-        types.submodule {
-          options = {
-            hostname = mkOption {
-              type = types.nonEmptyStr;
-              default = "nix-store.chn.moe";
-            };
-            store = mkOption {
-              type = types.nullOr types.nonEmptyStr;
-              default = null;
-            };
+{ lib, config, ... }: {
+  options.nixos.services.harmonia = lib.mkOption {
+    type = lib.types.nullOr (
+      lib.types.submodule {
+        options = {
+          hostname = lib.mkOption {
+            type = lib.types.nonEmptyStr;
+            default = "nix-store.chn.moe";
           };
-        }
-      );
-      default = null;
-    };
+          store = lib.mkOption {
+            type = lib.types.nullOr lib.types.nonEmptyStr;
+            default = null;
+          };
+        };
+      }
+    );
+    default = null;
+  };
   config =
     let
-      inherit (inputs.config.nixos.services) harmonia;
+      inherit (config.nixos.services) harmonia;
     in
-    inputs.lib.mkIf (harmonia != null) {
+    lib.mkIf (harmonia != null) {
       services.harmonia-dev = {
         cache = {
           enable = true;
-          signKeyPaths = [ inputs.config.nixos.system.sops.secrets."store/signingKey".path ];
-          settings = inputs.lib.mkIf (harmonia.store != null) {
-            virtual_nix_store = "/nix/store";
-            real_nix_store = "${harmonia.store}/nix/store";
-            enable_compression = true;
-          };
+          signKeyPaths = [ config.nixos.system.sops.secrets."store/signingKey".path ];
+          settings = lib.mkMerge [
+            { bind = "127.0.0.1:5001"; }
+            (lib.mkIf (harmonia.store != null) {
+              virtual_nix_store = "/nix/store";
+              real_nix_store = "${harmonia.store}/nix/store";
+              enable_compression = true;
+            })
+          ];
         };
         daemon = {
           enable = true;
-          dbPath = inputs.lib.mkIf (harmonia.store != null) "${harmonia.store}/nix/var/nix/db/db.sqlite";
+          dbPath = lib.mkIf (harmonia.store != null) "${harmonia.store}/nix/var/nix/db/db.sqlite";
         };
       };
       nixos = {
         system.sops.secrets."store/signingKey" = { };
-        services.nginx.https.${harmonia.hostname}.location."/".proxy.upstream = "http://127.0.0.1:5000";
+        services.nginx.https.${harmonia.hostname}.location."/".proxy.upstream = "http://127.0.0.1:5001";
       };
     };
 }
