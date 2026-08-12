935f0411492ac99eb04ae5f1d1c97f681998d60e
Haonan Chen <chn@chn.moe>
2026-08-12 12:32:09 +0800

    Reapply "nixosConfigurations/pc: enable virtualbox"
    This reverts commit 81bbfd7d91f4011a2433b1391c1817a3e7a0e83a.

--- lib/buildNixpkgsConfig/default.nix
+++ lib/buildNixpkgsConfig/default.nix
@@ -206,6 +206,9 @@ let
         btop = prev.btop.overrideAttrs (prev: {
           patches = prev.patches or [ ] ++ [ ./btop.patch ];
         });
+        virtualbox = prev.virtualbox.overrideAttrs (prev: {
+          patches = prev.patches or [ ] ++ [ ./virtualbox.patch ];
+        });
         prrte = prev.prrte.overrideAttrs (prev: {
           configureFlags = prev.configureFlags or [ ] ++ [ "--with-lsf" ];
           buildInputs = prev.buildInputs or [ ] ++ [
