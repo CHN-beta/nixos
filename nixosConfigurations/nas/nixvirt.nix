2571677226c35e4c736b132efc736f1abfdc7cc9
Haonan Chen <chn@chn.moe>
2026-09-09 15:08:10 +0800

    nixosConfigurations/nas/nixvirt: add new nodes

--- nixosConfigurations/nas/nixvirt.nix
+++ nixosConfigurations/nas/nixvirt.nix
@@ -1,12 +1,34 @@
 { self, ... }: {
-  config.nixos.services.nixvirt.instance.ddml = {
-    owner = "chn";
-    storage = {
-      mountFrom = "ssd";
-      iso = "${self.src.iso.nixos}";
+  config.nixos.services.nixvirt.instance = {
+    ddml = {
+      owner = "chn";
+      storage = {
+        mountFrom = "ssd";
+        iso = "${self.src.iso.nixos}";
+      };
+      memory.sizeMB = 4096;
+      cpu.count = 4;
+      network.address = 2;
     };
-    memory.sizeMB = 8192;
-    cpu.count = 4;
-    network.address = 2;
+    ddml-n1 = {
+      owner = "chn";
+      storage = {
+        mountFrom = "ssd";
+        iso = "${self.src.iso.nixos}";
+      };
+      memory.sizeMB = 4096;
+      cpu.count = 4;
+      network.address = 3;
+    };
+    ddml-n2 = {
+      owner = "chn";
+      storage = {
+        mountFrom = "ssd";
+        iso = "${self.src.iso.nixos}";
+      };
+      memory.sizeMB = 4096;
+      cpu.count = 4;
+      network.address = 4;
+    };
   };
 }
