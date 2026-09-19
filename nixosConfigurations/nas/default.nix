0e9455a3e34736ff0a10237e5d70c4e347f26dfd
Haonan Chen <chn@chn.moe>
2026-09-19 18:50:51 +0800

    nixosConfigurations/nas: fix if name

--- nixosConfigurations/nas/default.nix
+++ nixosConfigurations/nas/default.nix
@@ -60,7 +60,7 @@
       services = {
         sshd = { };
         xray.client.coredns = {
-          extraInterfaces = [ "enp3s0.20" ];
+          extraInterfaces = [ "enp4s0.20" ];
           hosts."git.chn.moe" = "127.0.0.1";
         };
         beesd = {
@@ -117,7 +117,7 @@
           persistent = "/nix/persistent";
           ssd = "/nix/ssd";
         };
-        pppoe.interface = "enp2s0";
+        pppoe.interface = "enp3s0";
       };
       packages = {
         opencode = { };
